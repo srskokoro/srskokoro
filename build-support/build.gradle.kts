3c09581d4d843310beb4e50d6aab8ff32f65eab7
netaneru <netaneru3@gmail.com>
2022-12-11 10:45:01 +0900

    Reorganized

--- build-support/build.gradle.kts
+++ build-support/build.gradle.kts
@@ -7,10 +7,6 @@ repositories {
 	mavenCentral()
 }
 
-dependencies {
-	compileOnly(kotlin("gradle-plugin", "1.7.10"))
-}
-
 gradlePlugin {
 	plugins {
 		create("build-support") {
@@ -18,4 +14,8 @@ gradlePlugin {
 			implementationClass = "BuildSupport"
 		}
 	}
+}
+
+dependencies {
+	compileOnly(kotlin("gradle-plugin", "1.7.10"))
 }
