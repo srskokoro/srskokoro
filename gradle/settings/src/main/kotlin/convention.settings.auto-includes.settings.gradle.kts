0469e607d1f9c763185275136116bd4d4f4e2f87
netaneru <netaneru3@gmail.com>
2023-01-25 16:04:29 +0800

    Auto-include our custom plugins instead

--- gradle/settings/src/main/kotlin/convention.settings.auto-includes.settings.gradle.kts
+++ gradle/settings/src/main/kotlin/convention.settings.auto-includes.settings.gradle.kts
@@ -1,3 +1,16 @@
+@file:Suppress("UnstableApiUsage")
+
+pluginManagement {
+	// Include our own custom plugins
+	if (File(rootDir, "gradle/plugins").exists()) {
+		includeBuild("gradle/plugins")
+	}
+	// If not the main build, it might be located next to the current build
+	if (File(rootDir, "../plugins").exists()) {
+		includeBuild("../plugins")
+	}
+}
+
 // Include all subfolders that contain a 'build.gradle.kts' as subprojects
 rootDir.let { rootDir ->
 	rootDir.list()
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,6 @@
 
 pluginManagement {
 	includeBuild("gradle/settings")
-	includeBuild("gradle/plugins")
 }
 plugins {
 	id("convention.settings")
