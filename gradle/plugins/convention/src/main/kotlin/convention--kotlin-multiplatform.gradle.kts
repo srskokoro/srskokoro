c4e8e0d4022fad8ddbe50ce505630506055e7520
netaneru <netaneru3@gmail.com>
2023-01-25 16:46:18 +0800

    Clarity

--- gradle/plugins/convention/src/main/kotlin/convention--kotlin-multiplatform.gradle.kts
+++ gradle/plugins/convention/src/main/kotlin/convention--kotlin-multiplatform.gradle.kts
@@ -7,7 +7,12 @@ plugins {
 // Make sure kotlin multiplatform plugin is applied first; throws otherwise.
 plugins.getPlugin("org.jetbrains.kotlin.multiplatform")
 
+// Adds extensions to conveniently set dependencies at the top level. See,
+// - https://kotlinlang.org/docs/multiplatform-add-dependencies.html
+// - https://kotlinlang.org/docs/gradle-configure-project.html#set-dependencies-at-top-level
+//
 // The kotlin multiplatform plugin doesn't (yet) do this for us :P
+//
 dependencies.extensions.let { exts ->
 	// NOTE: Extensions added at configuration time doesn't (yet) generate accessors. Which is why we must do
 	// this here :P -- See, https://docs.gradle.org/current/userguide/kotlin_dsl.html#kotdsl:accessor_applicability
