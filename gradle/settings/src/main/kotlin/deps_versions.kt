2cda7d3f6ab07f31fae1f9990383506755053703
netaneru <netaneru3@gmail.com>
2023-01-29 02:13:28 +0800

    As extension functions instead
    …so that they're indicated specially (and distinctly) in the IDE (e.g.,
    colored & italicized by IntelliJ IDEA and Android Studio).

--- gradle/settings/src/main/kotlin/deps_versions.kt
+++ gradle/settings/src/main/kotlin/deps_versions.kt
@@ -1,40 +1,40 @@
-@Suppress("ClassName", "MemberVisibilityCanBePrivate")
+@Suppress("ClassName")
 internal object deps_versions {
 	val plugins = mutableMapOf<String, String>()
 	val pluginGroups = mutableMapOf<String, String>()
 
 	val modules = mutableMapOf<Pair<String, String>, String>()
 	val moduleGroups = mutableMapOf<String, String>()
+}
 
-	fun plugin(id: String, version: String) {
-		plugins[id] = version
+internal fun deps_versions.plugin(id: String, version: String) {
+	plugins[id] = version
+}
+
+internal fun deps_versions.pluginGroup(idNs: String, version: String) {
+	pluginGroups[idNs] = version
+}
+
+internal fun deps_versions.module(moduleNotation: String, version: String) {
+	val groupDelimiterIdx = moduleNotation.indexOf(':')
+	require(groupDelimiterIdx >= 0) {
+		"Supplied `String` module notation \"$moduleNotation\" is invalid. " +
+			"Example notations: \"org.gradle:gradle-core\", \"org.mockito:mockito-core\""
 	}
 
-	fun pluginGroup(idNs: String, version: String) {
-		pluginGroups[idNs] = version
-	}
+	return module(
+		group = moduleNotation.substring(0, groupDelimiterIdx),
+		name = moduleNotation.substring(groupDelimiterIdx + 1),
+		version = version,
+	)
+}
 
-	fun module(moduleNotation: String, version: String) {
-		val groupDelimiterIdx = moduleNotation.indexOf(':')
-		require(groupDelimiterIdx >= 0) {
-			"Supplied `String` module notation \"$moduleNotation\" is invalid. " +
-				"Example notations: \"org.gradle:gradle-core\", \"org.mockito:mockito-core\""
-		}
+internal fun deps_versions.module(moduleNotation: Pair<String, String>, version: String) {
+	modules[moduleNotation] = version
+}
 
-		return module(
-			group = moduleNotation.substring(0, groupDelimiterIdx),
-			name = moduleNotation.substring(groupDelimiterIdx + 1),
-			version = version,
-		)
-	}
+internal fun deps_versions.module(group: String, name: String, version: String) = module(group to name, version)
 
-	fun module(moduleNotation: Pair<String, String>, version: String) {
-		modules[moduleNotation] = version
-	}
-
-	fun module(group: String, name: String, version: String) = module(group to name, version)
-
-	fun moduleGroup(group: String, version: String) {
-		moduleGroups[group] = version
-	}
+internal fun deps_versions.moduleGroup(group: String, version: String) {
+	moduleGroups[group] = version
 }
