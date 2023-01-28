823df88bc28a7f3e75753b454c3179f72a8dc6de
netaneru <netaneru3@gmail.com>
2023-01-29 03:00:47 +0800

    A simpler & optimized implementation for creating dependecy bundles

--- gradle/dependencies/bundles.kt
+++ gradle/dependencies/bundles.kt
@@ -1,9 +1,10 @@
-@file:Suppress()
+@Suppress("ClassName")
+abstract class deps_bundles internal constructor() : deps_bundles_base() {
 
-val deps_bundles.testCommon
-	get() = init {
+	val testCommon = bundle {
 		module("org.jetbrains.kotlin:kotlin-test")
 		module("org.jetbrains.kotlinx:kotlinx-coroutines-test")
 		module("io.kotest:kotest-assertions-core")
 		module("io.kotest:kotest-property")
 	}
+}
---
+++ gradle/settings/src/main/kotlin/deps_bundles_base.kt
@@ -0,0 +1,6 @@
+@Suppress("ClassName")
+abstract class deps_bundles_base internal constructor() {
+
+	internal inline fun bundle(init: deps_bundle_spec.() -> Unit) =
+		deps_bundle(deps_bundle_spec().apply(init))
+}
