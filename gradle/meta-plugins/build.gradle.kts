7ec48cc935f1e570813bec8dc96cc399d561eb81
netaneru <netaneru3@gmail.com>
2023-01-29 05:25:44 +0800

    Necessary to avoid warning in console

---
+++ gradle/meta-plugins/build.gradle.kts
@@ -0,0 +1,5 @@
+plugins {
+	// Necessary to avoid the plugin to be loaded multiple times in each
+	// subproject's classloader -- https://youtrack.jetbrains.com/issue/KT-46200
+	`kotlin-dsl` apply false
+}
