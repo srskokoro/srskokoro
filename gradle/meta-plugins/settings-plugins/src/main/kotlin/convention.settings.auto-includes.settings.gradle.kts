524955003ff564f3c9815ce12f4476a919a54c9a
netaneru <netaneru3@gmail.com>
2023-02-13 07:52:46 +0800

    Shared the main build's 'gradle.properties' instead

--- gradle/meta-plugins/settings-plugins/src/main/kotlin/convention.settings.auto-includes.settings.gradle.kts
+++ gradle/meta-plugins/settings-plugins/src/main/kotlin/convention.settings.auto-includes.settings.gradle.kts
@@ -1,13 +1,36 @@
 @file:Suppress("UnstableApiUsage")
 
+import java.util.*
+
 pluginManagement {
 	// Include our own custom plugins
 	"gradle/plugins".let { dir ->
 		for (parent in arrayOf("", "../", "../../")) {
 			val target = "$parent$dir"
 			if (File(rootDir, "$target/settings.gradle.kts").exists()) {
+				// Special handling if we're the main build
+				if (parent == "") {
+					// Share the main build's `gradle.properties`
+					val propSrc = File(rootDir, "gradle.properties")
+					val propDst = File(rootDir, "$target/gradle.properties")
+					val propSrcLastMod = propSrc.lastModified()
+					if (propSrcLastMod >= propDst.lastModified()) {
+						val prop = Properties()
+						propSrc.inputStream().use {
+							prop.load(it)
+						}
+						if (propDst.exists() && !propDst.delete()) throw FileAlreadyExistsException(
+							file = propDst, reason = "Failed to delete the destination file."
+						)
+						propDst.outputStream().use {
+							prop.store(it, "Auto-generated. DO NOT MODIFY.")
+						}
+						if (propSrcLastMod >= propDst.lastModified())
+							propDst.setLastModified(propSrcLastMod + 1)
+					}
+				}
 				includeBuild(target)
-				break
+				break // Done!
 			}
 		}
 	}
