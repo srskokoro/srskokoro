c136930dd75698575c2b7d8ffeacbcfb8491fcfc
netaneru <netaneru3@gmail.com>
2023-02-18 07:19:47 +0800

    Better implementation

--- gradle/meta-plugins/settings-plugins/src/main/kotlin/Settings_shareGradleProperties.kt
+++ gradle/meta-plugins/settings-plugins/src/main/kotlin/Settings_shareGradleProperties.kt
@@ -14,19 +14,20 @@ import java.util.*
  * directory).
  */
 fun Settings.shareGradleProperties(projectDir: String) {
-	val propSrc = File(rootDir, "gradle.properties")
-	val propDst = File(rootDir, "$projectDir/gradle.properties")
+	val propSrcFile = File(rootDir, "gradle.properties")
+	val propDstFile = File(rootDir, "$projectDir/gradle.properties")
+	val propDstPath = propDstFile.toPath()
 
-	if (propDst.exists()) {
-		Files.readAttributes(propDst.toPath(), BasicFileAttributes::class.java).let { propDstAttr ->
+	if (propDstFile.isFile) {
+		Files.readAttributes(propDstPath, BasicFileAttributes::class.java).let { propDstAttr ->
 			val propDstModMs = propDstAttr.lastModifiedTime().toMillis()
-			if (propDstModMs > propSrc.lastModified() && propDstModMs == propDstAttr.creationTime().toMillis()) {
+			if (propDstModMs > propSrcFile.lastModified() && propDstModMs == propDstAttr.creationTime().toMillis()) {
 				return@shareGradleProperties // It's likely up-to-date
 			}
 		}
 
-		if (!propDst.delete()) throw FileAlreadyExistsException(
-			file = propDst, reason = "Failed to delete the destination file."
+		if (!propDstFile.delete()) throw FileAlreadyExistsException(
+			file = propDstFile, reason = "Failed to delete the destination file."
 		)
 	}
 
@@ -35,14 +36,14 @@ fun Settings.shareGradleProperties(projectDir: String) {
 	// (also known as Latin-1).
 	// - See, https://github.com/gradle/gradle/issues/13741#issuecomment-658177619
 	// - See also, https://en.wikipedia.org/wiki/.properties
-	propSrc.inputStream().use {
+	propSrcFile.inputStream().use {
 		prop.load(it)
 	}
-	propDst.outputStream().use {
+	propDstFile.outputStream().use {
 		prop.store(it, "Auto-generated. DO NOT MODIFY.")
 	}
 
-	Files.getFileAttributeView(propDst.toPath(), BasicFileAttributeView::class.java).let { propDstAttrView ->
+	Files.getFileAttributeView(propDstPath, BasicFileAttributeView::class.java).let { propDstAttrView ->
 		// Sets the creation time to be the same as the last modification time.
 		val lastModifiedTime = propDstAttrView.readAttributes().lastModifiedTime()
 		propDstAttrView.setTimes(null, null, /* createTime = */ lastModifiedTime)
@@ -60,5 +61,5 @@ fun Settings.shareGradleProperties(projectDir: String) {
 		}
 	}
 
-	println("Auto-generated 'gradle.properties' file: ${propDst.absolutePath}")
+	println("Auto-generated 'gradle.properties' file: ${propDstFile.absolutePath}")
 }
