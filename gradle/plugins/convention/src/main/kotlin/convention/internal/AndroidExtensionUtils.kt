4d72cc06bd623d62a08ae447e4503eb47d8e9557
netaneru <netaneru3@gmail.com>
2023-02-19 07:21:17 +0800

    Better name

--- gradle/plugins/convention/src/main/kotlin/convention/internal/AndroidExtensionUtils.kt
+++ gradle/plugins/convention/src/main/kotlin/convention/internal/AndroidExtensionUtils.kt
@@ -4,14 +4,14 @@ import org.gradle.api.Action
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.withType
 
-internal inline fun Project.whenAndroidPlugin(crossinline block: () -> Unit) {
+internal inline fun Project.ifAndroidProject(crossinline block: () -> Unit) {
 	plugins.withType<com.android.build.gradle.BasePlugin> {
 		block()
 	}
 }
 
 internal fun Project.withAndroid(configure: Action<com.android.build.api.dsl.CommonExtension<*, *, *, *>>) {
-	whenAndroidPlugin {
+	ifAndroidProject {
 		extensions.configure("android", configure)
 	}
 }
