5a9d2532fd486d45e2dc5e81dd065324c7f7c464
netaneru <netaneru3@gmail.com>
2023-02-19 08:04:00 +0800

    Convenience for future setup

--- gradle/plugins/convention/src/main/kotlin/convention/internal/AndroidExtensionUtils.kt
+++ gradle/plugins/convention/src/main/kotlin/convention/internal/AndroidExtensionUtils.kt
@@ -5,6 +5,8 @@ import org.gradle.api.Project
 import org.gradle.kotlin.dsl.withType
 
 internal typealias AndroidExtension = com.android.build.api.dsl.CommonExtension<*, *, *, *>
+internal typealias AndroidAppExtension = com.android.build.api.dsl.ApplicationExtension
+internal typealias AndroidLibExtension = com.android.build.api.dsl.LibraryExtension
 
 internal inline fun Project.ifAndroidProject(crossinline block: () -> Unit) {
 	plugins.withType<com.android.build.gradle.BasePlugin> {
@@ -12,8 +14,17 @@ internal inline fun Project.ifAndroidProject(crossinline block: () -> Unit) {
 	}
 }
 
-internal fun Project.withAndroid(configure: Action<AndroidExtension>) {
+@JvmName("withAndroid_")
+internal fun Project.withAndroid(configure: Action<out AndroidExtension>) {
 	ifAndroidProject {
 		extensions.configure("android", configure)
 	}
 }
+
+/**
+ * @param T any subtype of [AndroidExtension] (e.g. [AndroidAppExtension] or
+ * [AndroidLibExtension]).
+ * @see AndroidAppExtension
+ * @see AndroidLibExtension
+ */
+internal fun <T : AndroidExtension> Project.withAndroid(configure: Action<T>) = withAndroid(configure)
