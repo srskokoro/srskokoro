0e148c0c5f1cba07ebcdf69ebb07b8a13a16aec8
netaneru <netaneru3@gmail.com>
2023-02-25 14:47:22 +0800

    Merged the two code blocks

--- gradle/plugins/convention/src/main/kotlin/convention.compose.jvm.app.gradle.kts
+++ gradle/plugins/convention/src/main/kotlin/convention.compose.jvm.app.gradle.kts
@@ -12,28 +12,27 @@ dependencies {
 	implementation(compose.desktop.currentOs)
 }
 
-compose.desktop.application {
-	nativeDistributions {
+compose.desktop.application.let { app ->
+	app.nativeDistributions {
 		appResourcesRootDir.set(file("res"))
 	}
-}
 
-// TODO Remove eventually. See also, https://github.com/JetBrains/compose-jb/pull/2515
-// Workaround as it seems that configuring the JVM toolchain doesn't
-// automatically set `compose.desktop.application.javaHome` still.
-afterEvaluate {
-	val app = compose.desktop.application
-	if (app.javaHome == System.getProperty("java.home")) {
-		@Suppress("UsePropertyAccessSyntax")
-		val launcher = javaToolchains.launcherFor(java.toolchain).getOrNull()
-		if (launcher != null) {
-			app.javaHome = launcher.metadata.installationPath.asFile.absolutePath
+	// TODO Remove eventually. See also, https://github.com/JetBrains/compose-jb/pull/2515
+	// Workaround as it seems that configuring the JVM toolchain doesn't
+	// automatically set `compose.desktop.application.javaHome` still.
+	afterEvaluate {
+		if (app.javaHome == System.getProperty("java.home")) {
+			@Suppress("UsePropertyAccessSyntax")
+			val launcher = javaToolchains.launcherFor(java.toolchain).getOrNull()
+			if (launcher != null) {
+				app.javaHome = launcher.metadata.installationPath.asFile.absolutePath
+			} else {
+				logger.warn("Warning: JVM toolchain was not configured.")
+			}
 		} else {
-			logger.warn("Warning: JVM toolchain was not configured.")
+			// May give us an indication of whether this workaround is still needed.
+			logger.quiet("Custom `javaHome` set for `compose.desktop.application`:")
+			logger.quiet("  ${app.javaHome}")
 		}
-	} else {
-		// May give us an indication of whether this workaround is still needed.
-		logger.quiet("Custom `javaHome` set for `compose.desktop.application`:")
-		logger.quiet("  ${app.javaHome}")
 	}
 }
