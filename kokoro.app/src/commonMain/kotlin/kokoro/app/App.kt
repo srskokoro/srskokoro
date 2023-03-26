19a90c58093a58af76b7674f53a39e253674f543
netaneru <netaneru3@gmail.com>
2023-03-26 17:37:59 +0800

    Documented for clarity on expected behavior

--- kokoro.app/src/commonMain/kotlin/kokoro/app/App.kt
+++ kokoro.app/src/commonMain/kotlin/kokoro/app/App.kt
@@ -8,5 +8,10 @@ val APP_CACHE_SCHEMA_VERSION_NAME = APP_CACHE_SCHEMA_VERSION.toString().padStart
 
 expect object App {
 
+	/**
+	 * The primary directory for storing cache data. All cache data should
+	 * usually go under this directory. This directory is always a subdirectory
+	 * of the cache directory root.
+	 */
 	@JvmStatic val cacheData: File
 }
