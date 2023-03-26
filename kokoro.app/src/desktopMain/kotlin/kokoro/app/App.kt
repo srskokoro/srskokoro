974cdc1775f1ab3c285ef4e6ba03a377ff31f5bf
netaneru <netaneru3@gmail.com>
2023-03-26 17:44:08 +0800

    Reduced ambiguity

--- kokoro.app/src/desktopMain/kotlin/kokoro/app/App.kt
+++ kokoro.app/src/desktopMain/kotlin/kokoro/app/App.kt
@@ -7,7 +7,7 @@ import kotlin.io.path.createDirectories
 @Suppress("NewApi")
 actual object App {
 
-	private const val APP_CACHE_DIR_NAME = "SRSKokoroAppCache"
+	private const val APP_CACHE_DIR_ROOT_NAME = "SRSKokoroAppCache"
 	private val platformDirs = AppDirsFactory.getInstance()
 
 	private var _cacheMain: File? = null
@@ -15,7 +15,7 @@ actual object App {
 	@JvmStatic
 	actual val cacheMain: File
 		get() = _cacheMain ?: File(
-			platformDirs.getUserDataDir(APP_CACHE_DIR_NAME, null, null, /* roaming = */ false),
+			platformDirs.getUserDataDir(APP_CACHE_DIR_ROOT_NAME, null, null, /* roaming = */ false),
 			APP_CACHE_SCHEMA_VERSION_NAME
 		).also {
 			it.toPath().createDirectories()
