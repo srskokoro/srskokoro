7e723a6c8740c042717fe21597147f6f6e81399e
netaneru <netaneru3@gmail.com>
2023-03-29 04:05:18 +0800

    Removed as we're not even sure how that works

--- gradle/meta-plugins/deps-plugins/src/main/kotlin/conv/deps/internal/util/Utils.kt
+++ gradle/meta-plugins/deps-plugins/src/main/kotlin/conv/deps/internal/util/Utils.kt
@@ -6,7 +6,7 @@ internal inline fun indexOfModuleGroupDelimiter(isDependencyNotation: Boolean, n
 	require(groupDelimiterIdx >= 0) {
 		"Supplied `String` module notation \"$notation\" is invalid. " +
 			if (!isDependencyNotation) "Example notations: \"org.gradle:gradle-core\", \"org.mockito:mockito-core\""
-			else "Example notations: \"org.gradle:gradle-core:2.2\", \"org.mockito:mockito-core:1.9.5:javadoc\""
+			else "Example notations: \"org.gradle:gradle-core:2.2\", \"org.mockito:mockito-core:1.9.5\""
 	}
 	return groupDelimiterIdx
 }
