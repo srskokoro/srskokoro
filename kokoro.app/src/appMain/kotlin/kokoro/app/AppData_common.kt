feb9731b7f030c17a80bc7a13e8e68f0fb095981
netaneru <netaneru3@gmail.com>
2023-04-08 10:24:58 +0800

    Looks better

--- kokoro.app/src/appMain/kotlin/kokoro/app/AppData_common.kt
+++ kokoro.app/src/appMain/kotlin/kokoro/app/AppData_common.kt
@@ -7,15 +7,13 @@ internal object AppData_common {
 
 	@JvmStatic
 	fun ensureCacheMain(dirRoot: Path, fs: FileSystem): Path {
-		return (dirRoot / APP_DATA_SCHEMA_VERSION_NAME).also {
-			fs.apply {
-				createDirectories(it)
-				if (metadataOrNull(it)?.isDirectory != true) {
-					// Let the following throw its own exception (so that we
-					// don't have to throw our own customized one).
-					createDirectory(it, mustCreate = true)
-				}
-			}
+		val it = dirRoot / APP_DATA_SCHEMA_VERSION_NAME
+		fs.createDirectories(it)
+		if (fs.metadataOrNull(it)?.isDirectory != true) {
+			// Let the following throw its own exception (so that we
+			// don't have to throw our own customized one).
+			fs.createDirectory(it, mustCreate = true)
 		}
+		return it
 	}
 }
