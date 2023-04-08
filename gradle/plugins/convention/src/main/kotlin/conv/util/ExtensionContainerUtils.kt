abf6f3f907d036924dc509ed30c4809acf4b24df
netaneru <netaneru3@gmail.com>
2023-04-09 03:06:09 +0800

    Actually unnecessary :P

--- gradle/plugins/convention/src/main/kotlin/conv/util/ExtensionContainerUtils.kt
+++ gradle/plugins/convention/src/main/kotlin/conv/util/ExtensionContainerUtils.kt
@@ -5,8 +5,6 @@ import org.gradle.api.plugins.ExtensionAware
 import org.gradle.api.plugins.ExtensionContainer
 import org.gradle.kotlin.dsl.add
 
-inline fun <reified T> typeOf(@Suppress("UNUSED_PARAMETER") it: T) = org.gradle.kotlin.dsl.typeOf<T>()
-
 inline infix fun <reified T : Named> T.asExtensionIn(extendable: ExtensionAware) = asExtensionIn(extendable.extensions)
 
 inline infix fun <reified T : Named> T.asExtensionIn(extensions: ExtensionContainer): T {
