6c3018d03acd4bfa4920650976e0b730b08300bc
netaneru <netaneru3@gmail.com>
2023-04-10 05:13:31 +0800

    Reorganized

--- kokoro.app/src/desktopMain/kotlin/kokoro/app/AppDataPlatformImpl.kt
+++ kokoro.app/src/desktopMain/kotlin/kokoro/app/AppDataPlatformImpl.kt
@@ -6,6 +6,13 @@ import net.harawata.appdirs.AppDirsFactory.getInstance as getAppDirsFactory
 @Suppress("NOTHING_TO_INLINE")
 actual object AppDataPlatformImpl {
 
+	private const val APP_DATA_DIR_NAME = "SRSKokoro"
+
+	private fun getPlatformAppDir(roaming: Boolean = false) = getAppDirsFactory()
+		.getUserDataDir(APP_DATA_DIR_NAME, null, null, /* roaming = */ roaming)
+
+	// --
+
 	@JvmStatic actual fun forRoamingRoot(): String =
 		getOverrides().run { roamingRoot ?: defaultRoot } ?: getPlatformAppDir(roaming = true)
 
@@ -17,11 +24,4 @@ actual object AppDataPlatformImpl {
 
 	@JvmStatic actual fun forCacheRoot(): String =
 		getOverrides().run { cacheRoot ?: defaultRoot } ?: getPlatformAppDir()
-
-	// --
-
-	private const val APP_DATA_DIR_NAME = "SRSKokoro"
-
-	private fun getPlatformAppDir(roaming: Boolean = false) = getAppDirsFactory()
-		.getUserDataDir(APP_DATA_DIR_NAME, null, null, /* roaming = */ roaming)
 }
