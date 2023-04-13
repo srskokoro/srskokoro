db766c9ea74204a9f8c2c4d567818ac8e3369b2a
netaneru <netaneru3@gmail.com>
2023-04-14 03:18:15 +0800

    Lock file offsets with docs

--- kokoro.app/src/desktopMain/kotlin/main.kt
+++ kokoro.app/src/desktopMain/kotlin/main.kt
@@ -1,2 +1,23 @@
-﻿fun main(args: Array<out String>) {
+﻿/**
+ * An offset to a lock byte that can be locked on in the lock file.
+ *
+ * This lock byte is what a process should lock on whenever it needs to update
+ * the application instance count, add more application instances, designate a
+ * particular process as the master instance, etc.
+ *
+ * @see MASTER_INSTANCE_LOCK_BYTE
+ */
+private const val INSTANCE_CHANGE_LOCK_BYTE = 0L
+
+/**
+ * An offset to a lock byte that can be locked on in the lock file.
+ *
+ * This lock byte is what a process should lock on in order to designate itself
+ * as the master instance of the application's single-program process model.
+ *
+ * This lock must only be acquired and/or released while locking on [INSTANCE_CHANGE_LOCK_BYTE].
+ */
+private const val MASTER_INSTANCE_LOCK_BYTE = 1L
+
+fun main(args: Array<out String>) {
 }
