4ef09ac906f8a594c11247735ea9688b26f5ff56
netaneru <netaneru3@gmail.com>
2023-04-16 14:52:59 +0800

    Why not? :P

--- gradle/conventions/conv.gmazzo.buildconfig.gradle.kts
+++ gradle/conventions/conv.gmazzo.buildconfig.gradle.kts
@@ -4,5 +4,7 @@ plugins {
 
 // Automatically generate build config files on "gradle sync" (via IntelliJ IDEA
 // or Android Studio) -- https://twitter.com/Sellmair/status/1619308362881187840
-tasks.maybeCreate("prepareKotlinIdeaImport")
-	.dependsOn(tasks.generateBuildConfig)
+tasks.let {
+	it.maybeCreate("prepareKotlinIdeaImport")
+		.dependsOn(it.generateBuildConfig)
+}
