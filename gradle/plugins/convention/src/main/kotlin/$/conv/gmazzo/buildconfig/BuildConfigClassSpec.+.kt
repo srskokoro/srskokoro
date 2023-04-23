3e5d61b397bf69016c637541835cff907372918a
netaneru <netaneru3@gmail.com>
2023-04-23 11:23:08 +0800

    Added more convenience functions

--- gradle/plugins/convention/src/main/kotlin/$/conv/gmazzo/buildconfig/BuildConfigClassSpec.+.kt
+++ gradle/plugins/convention/src/main/kotlin/$/conv/gmazzo/buildconfig/BuildConfigClassSpec.+.kt
@@ -6,3 +6,16 @@ inline infix fun BuildConfigClassSpec.inPackage(packageName: String) = packageNa
 
 inline fun BuildConfigClassSpec.internalObject(name: String) =
 	className(name).useKotlinOutput { internalVisibility = true }
+
+inline fun BuildConfigClassSpec.publicObject(name: String) =
+	className(name).useKotlinOutput()
+
+inline fun BuildConfigClassSpec.internalTopLevel(name: String? = null) = run {
+	className.set(name)
+	useKotlinOutput { topLevelConstants = true; internalVisibility = true }
+}
+
+inline fun BuildConfigClassSpec.publicTopLevel(name: String? = null) = run {
+	className.set(name)
+	useKotlinOutput { topLevelConstants = true }
+}
