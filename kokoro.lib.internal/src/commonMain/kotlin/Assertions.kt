051241fbeb24bae32c28193580c5f977a8173ce7
netaneru <netaneru3@gmail.com>
2023-04-24 14:58:08 +0800

    Added contracts to our `assert` implementation

--- kokoro.lib.internal/src/commonMain/kotlin/Assertions.kt
+++ kokoro.lib.internal/src/commonMain/kotlin/Assertions.kt
@@ -1,10 +1,19 @@
 package kokoro.internal
 
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
+
 @PublishedApi
 internal const val ASSERTIONS_ENABLED = !IS_RELEASING
 
+@OptIn(ExperimentalContracts::class)
 // See, https://youtrack.jetbrains.com/issue/KT-22292
 inline fun assert(lazyMessage: () -> Any = { "Assertion failed!" }, lazyCheck: () -> Boolean) {
+	contract {
+		callsInPlace(lazyMessage, InvocationKind.AT_MOST_ONCE)
+		callsInPlace(lazyCheck, InvocationKind.AT_MOST_ONCE)
+	}
 	if (ASSERTIONS_ENABLED && lazyCheck()) {
 		throw AssertionError(lazyMessage())
 	}
