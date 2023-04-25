c727a2dddca379a5036a0c247a390d90be674efc
netaneru <netaneru3@gmail.com>
2023-04-25 19:22:39 +0800

    Try-catch no longer necessary

--- kokoro.app/src/desktopMain/kotlin/main.kt
+++ kokoro.app/src/desktopMain/kotlin/main.kt
@@ -459,33 +459,27 @@ private class AppRelay(sockDir: String) {
 // --
 
 private fun generateInetPortFile(target: NioPath, boundServer: ServerSocketChannel) {
-	try {
-		// The following cast throws NPE if the server is not bound
-		val address = boundServer.localAddress as InetSocketAddress
-		val port = address.port.toShort()
+	val address = boundServer.localAddress // May throw `ClosedChannelException`
+		as InetSocketAddress // Cast throws NPE if the server is not bound
+	val port = address.port.toShort()
 
-		val bb = ByteBuffer.allocate(2)
-		bb.putShort(port)
-		bb.rewind()
+	val bb = ByteBuffer.allocate(2)
+	bb.putShort(port)
+	bb.rewind()
 
-		// Output to a temporary file first
-		val tmp = NioPath.of("$target.tmp")
+	// Output to a temporary file first
+	val tmp = NioPath.of("$target.tmp")
 
-		// Needs `DSYNC` here since otherwise, file writes can be delayed by the
-		// OS (even when properly closed) and we have to do a rename/move
-		// operation later to atomically publish our changes. See also,
-		// https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/nio/file/package-summary.html#integrity
-		FileChannel.open(tmp, DSYNC, CREATE, WRITE, TRUNCATE_EXISTING).use {
-			it.write(bb)
-		}
-
-		// Atomically publish our changes via a rename/move operation
-		Files.move(tmp, target, ATOMIC_MOVE, REPLACE_EXISTING)
-		// ^ Same as in `okio.NioSystemFileSystem.atomicMove()`
-	} catch (ex: Throwable) {
-		boundServer.closeInCatch(ex)
-		throw ex
+	// Needs `DSYNC` here since otherwise, file writes can be delayed by the OS
+	// (even when properly closed) and we have to do a rename/move operation
+	// later to atomically publish our changes. See also, https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/nio/file/package-summary.html#integrity
+	FileChannel.open(tmp, DSYNC, CREATE, WRITE, TRUNCATE_EXISTING).use {
+		it.write(bb)
 	}
+
+	// Atomically publish our changes via a rename/move operation
+	Files.move(tmp, target, ATOMIC_MOVE, REPLACE_EXISTING)
+	// ^ Same as in `okio.NioSystemFileSystem.atomicMove()`
 }
 
 private fun readInetPortFile(target: NioPath, client: SocketChannel): Int {
