6f29248ab52e0163971a7f19519d87dbba911f91
netaneru <netaneru3@gmail.com>
2023-04-28 12:12:28 +0800

    Assertion utilities for requiring not null

--- kokoro.lib.internal/src/commonMain/kotlin/kokoro/internal/Assertions.kt
+++ kokoro.lib.internal/src/commonMain/kotlin/kokoro/internal/Assertions.kt
@@ -1,5 +1,8 @@
 package kokoro.internal
 
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.contract
+
 @PublishedApi
 internal const val ASSERTIONS_ENABLED = !IS_RELEASING
 
@@ -7,5 +10,31 @@ internal const val ASSERTIONS_ENABLED = !IS_RELEASING
 inline fun assert(lazyMessage: () -> Any = { "Assertion failed!" }, lazyCheck: () -> Boolean) {
 	if (ASSERTIONS_ENABLED && !lazyCheck()) {
 		throw AssertionError(lazyMessage())
+	}
+}
+
+/**
+ * Throws an [AssertionError] if the [value] is null.
+ */
+@OptIn(ExperimentalContracts::class)
+inline fun <T : Any> assertNotNull(value: T?) {
+	contract {
+		returns() implies (value != null)
+	}
+	assertNotNull(value) { "Required value was null." }
+}
+
+/**
+ * Throws an [AssertionError] with the result of calling [lazyMessage] if the
+ * [value] is null.
+ */
+@OptIn(ExperimentalContracts::class)
+inline fun <T : Any> assertNotNull(value: T?, lazyMessage: () -> Any) {
+	contract {
+		returns() implies (value != null)
+	}
+	if (ASSERTIONS_ENABLED && value == null) {
+		val message = lazyMessage()
+		throw AssertionError(message)
 	}
 }
