ed11528eae6be79545af5571b4b6b576c6bcf275
netaneru <netaneru3@gmail.com>
2023-05-06 03:20:21 +0800

    Counterpart with call argument names

--- kokoro.lib.internal/src/jvmishMain/kotlin/kokoro/internal/StackTraceElement.kt
+++ kokoro.lib.internal/src/jvmishMain/kotlin/kokoro/internal/StackTraceElement.kt
@@ -1,5 +1,18 @@
 package kokoro.internal
 
+@Suppress("NOTHING_TO_INLINE")
+inline fun StackTraceElement(
+	declaringClass: String,
+	methodName: String,
+	fileName: String?,
+	lineNumber: Int,
+) = java.lang.StackTraceElement(
+	/* declaringClass = */ declaringClass,
+	/* methodName = */ methodName,
+	/* fileName = */ fileName,
+	/* lineNumber = */ lineNumber,
+)
+
 @Suppress("RemoveRedundantQualifierName")
 expect inline fun StackTraceElement(
 	classLoaderName: String?,
