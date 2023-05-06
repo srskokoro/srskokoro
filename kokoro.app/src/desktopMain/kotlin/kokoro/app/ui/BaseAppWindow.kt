38d219fa7de29423a8b7ec5025fc07c93e081b54
netaneru <netaneru3@gmail.com>
2023-05-06 21:36:25 +0800

    Can actually be optimized & simplified

--- kokoro.app/src/desktopMain/kotlin/kokoro/app/ui/BaseAppWindow.kt
+++ kokoro.app/src/desktopMain/kotlin/kokoro/app/ui/BaseAppWindow.kt
@@ -3,20 +3,14 @@ package kokoro.app.ui
 import kokoro.internal.ui.assertThreadSwing
 import java.awt.GraphicsConfiguration
 import java.awt.event.WindowEvent
-import java.awt.event.WindowListener
 import java.lang.ref.WeakReference
 import javax.swing.JFrame
 
-open class BaseAppWindow : JFrame, WindowListener {
+open class BaseAppWindow : JFrame {
 	constructor() : super()
 	constructor(gc: GraphicsConfiguration?) : super(gc)
 	constructor(title: String?) : super(title)
 	constructor(title: String?, gc: GraphicsConfiguration?) : super(title, gc)
-
-	init {
-		@Suppress("LeakingThis")
-		super.addWindowListener(this)
-	}
 
 	companion object {
 		/** @see javax.swing.JOptionPane.getRootFrame */
@@ -32,23 +26,11 @@ open class BaseAppWindow : JFrame, WindowListener {
 	@Suppress("LeakingThis")
 	private val ref = WeakReference(this)
 
-	//region `WindowListener` implementation
+	override fun processWindowEvent(e: WindowEvent) {
+		super.processWindowEvent(e)
 
-	override fun windowOpened(e: WindowEvent) {}
-
-	override fun windowClosing(e: WindowEvent) {}
-
-	override fun windowClosed(e: WindowEvent) {}
-
-	override fun windowIconified(e: WindowEvent) {}
-
-	override fun windowDeiconified(e: WindowEvent) {}
-
-	override fun windowActivated(e: WindowEvent) {
-		lastActiveRef = ref
+		if (e.id == WindowEvent.WINDOW_ACTIVATED) {
+			lastActiveRef = ref
+		}
 	}
-
-	override fun windowDeactivated(e: WindowEvent) {}
-
-	//endregion
 }
