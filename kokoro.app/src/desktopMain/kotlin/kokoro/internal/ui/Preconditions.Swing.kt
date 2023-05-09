4b3b3276b0d760b60a37abaecdff1a5af4576b64
netaneru <netaneru3@gmail.com>
2023-05-10 04:35:37 +0800

    Utility to ensure thread is Swing EDT

--- kokoro.app/src/desktopMain/kotlin/kokoro/internal/ui/Preconditions.Swing.kt
+++ kokoro.app/src/desktopMain/kotlin/kokoro/internal/ui/Preconditions.Swing.kt
@@ -1,9 +1,15 @@
 package kokoro.internal.ui
 
 import kokoro.internal.assert
+import java.awt.EventQueue
 import javax.swing.SwingUtilities
 
 @Suppress("NOTHING_TO_INLINE")
 inline fun assertThreadSwing() = assert({
 	"Swing EDT expected! Actual thread: ${Thread.currentThread().name}"
 }) { SwingUtilities.isEventDispatchThread() }
+
+@Suppress("NOTHING_TO_INLINE")
+inline fun checkThreadSwing() = check(lazyMessage = {
+	"Swing EDT expected! Actual thread: ${Thread.currentThread().name}"
+}, value = EventQueue.isDispatchThread())
