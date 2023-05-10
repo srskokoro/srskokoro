bfd9ca544e6600079e41da6df682d81f7e41cb6c
netaneru <netaneru3@gmail.com>
2023-05-10 11:45:24 +0800

    Alt., chain lazy text result to the button factory

--- kokoro.app/src/appMain/kotlin/kokoro/app/ui/AlertChoice.kt
+++ kokoro.app/src/appMain/kotlin/kokoro/app/ui/AlertChoice.kt
@@ -19,3 +19,11 @@ inline fun AlertChoice(
 	override fun getButton(locale: Locale) = lazyButton(locale)
 	override fun getText(locale: Locale) = lazyText(locale)
 }
+
+inline fun AlertChoice2(
+	crossinline lazyButton: AlertChoice.(text: Any, Locale) -> Any,
+	crossinline lazyText: AlertChoice.(Locale) -> Any,
+) = object : AlertChoice() {
+	override fun getButton(locale: Locale) = lazyButton(getText(locale), locale)
+	override fun getText(locale: Locale) = lazyText(locale)
+}
