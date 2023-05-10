2d429683672c41f3ac8e4320595ab7f1cd6eecc1
netaneru <netaneru3@gmail.com>
2023-05-10 11:28:06 +0800

    Ability to get a button component instead

--- kokoro.app/src/appMain/kotlin/kokoro/app/ui/AlertChoice.kt
+++ kokoro.app/src/appMain/kotlin/kokoro/app/ui/AlertChoice.kt
@@ -4,12 +4,18 @@ import kokoro.app.i18n.Locale
 
 abstract class AlertChoice {
 
+	abstract fun getButton(locale: Locale): Any
+
 	abstract fun getLabel(locale: Locale): Any
 
 	override fun toString(): String =
 		getLabel(Locale.ROOT).toString()
 }
 
-inline fun AlertChoice(crossinline lazyLabel: AlertChoice.(locale: Locale) -> Any) = object : AlertChoice() {
+inline fun AlertChoice(
+	crossinline lazyButton: AlertChoice.(Locale) -> Any = { getLabel(it) },
+	crossinline lazyLabel: AlertChoice.(Locale) -> Any,
+) = object : AlertChoice() {
+	override fun getButton(locale: Locale) = lazyButton(locale)
 	override fun getLabel(locale: Locale) = lazyLabel(locale)
 }
