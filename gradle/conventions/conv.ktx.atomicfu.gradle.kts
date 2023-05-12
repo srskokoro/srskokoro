7c747fa652217d8756130e358db8b02b6cb3da25
netaneru <netaneru3@gmail.com>
2023-05-13 02:43:32 +0800

    AtomicFU setup

--- gradle/conventions/conv.ktx.atomicfu.gradle.kts
+++ gradle/conventions/conv.ktx.atomicfu.gradle.kts
@@ -1,2 +1,14 @@
-apply(plugin = "kotlinx-atomicfu")
-// TODO Configure if necessary
+plugins {
+	id("kotlinx-atomicfu")
+}
+
+check(
+	providers.gradleProperty("kotlin.js.compiler").get().let { it == "ir" || it == "both" } &&
+		providers.gradleProperty("kotlinx.atomicfu.enableJvmIrTransformation").get() == "true" &&
+		providers.gradleProperty("kotlinx.atomicfu.enableJsIrTransformation").get() == "true"
+) {
+	"""
+	Proper setup of IR transformation mode is required.
+	- See, https://github.com/Kotlin/kotlinx-atomicfu/blob/version-0.20.2/README.md#atomicfu-compiler-plugin
+	""".trimIndent()
+}
