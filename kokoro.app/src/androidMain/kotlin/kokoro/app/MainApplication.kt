522311087307cea6046dd20d67d68ce90557c96e
netaneru <netaneru3@gmail.com>
2023-05-13 06:18:22 +0800

    Ensured `Locale.getDefault()` is up-to-date

--- kokoro.app/src/androidMain/kotlin/kokoro/app/MainApplication.kt
+++ kokoro.app/src/androidMain/kotlin/kokoro/app/MainApplication.kt
@@ -1,6 +1,9 @@
 package kokoro.app
 
 import android.app.Application
+import android.content.res.Configuration
+import android.os.Build
+import java.util.Locale
 import kokoro.app.MainApplication.Singleton.instance as mainApplication
 
 class MainApplication : Application() {
@@ -21,5 +24,26 @@ class MainApplication : Application() {
 			getOrNull() ?: throw UninitializedPropertyAccessException(
 				"Main application was not initialized."
 			)
+	}
+
+	override fun onConfigurationChanged(newConfig: Configuration) {
+		super.onConfigurationChanged(newConfig)
+
+		// Update the JVM default `Locale` manually as it may not automatically
+		// be done for us, at least for some devices -- e.g., in the emulator,
+		// it seems to be automatically updated. See also,
+		// - https://medium.com/@hectorricardomendez/how-to-get-the-current-locale-in-android-fc12d8be6242
+		// - https://stackoverflow.com/a/21844639
+		// - https://developer.android.com/guide/topics/resources/app-languages
+		//
+		val locale = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+			newConfig.locales.get(0)
+		} else {
+			@Suppress("DEPRECATION")
+			newConfig.locale
+		}
+		if (Locale.getDefault() != locale) {
+			Locale.setDefault(locale)
+		}
 	}
 }
