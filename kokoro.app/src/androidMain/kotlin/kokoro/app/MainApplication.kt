7fed4e5d560ec49740eb37b0aec781c099058130
netaneru <netaneru3@gmail.com>
2023-05-14 01:37:29 +0800

    Better

--- kokoro.app/src/androidMain/kotlin/kokoro/app/MainApplication.kt
+++ kokoro.app/src/androidMain/kotlin/kokoro/app/MainApplication.kt
@@ -30,7 +30,7 @@ class MainApplication : Application() {
 		super.onConfigurationChanged(newConfig)
 
 		// Update the JVM default `Locale` manually as it may not automatically
-		// be done for us, at least for some devices -- e.g., in the emulator,
+		// be updated for us, at least on some devices -- e.g., in the emulator,
 		// it seems to be automatically updated. See also,
 		// - https://medium.com/@hectorricardomendez/how-to-get-the-current-locale-in-android-fc12d8be6242
 		// - https://stackoverflow.com/a/21844639
