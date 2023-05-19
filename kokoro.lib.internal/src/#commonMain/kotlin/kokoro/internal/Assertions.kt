93720696a6cf739ecca9c04b719c5112413418cd
netaneru <netaneru3@gmail.com>
2023-05-20 01:01:44 +0800

    Better as a doc

--- kokoro.lib.internal/src/#commonMain/kotlin/kokoro/internal/Assertions.kt
+++ kokoro.lib.internal/src/#commonMain/kotlin/kokoro/internal/Assertions.kt
@@ -6,7 +6,7 @@ import kotlin.contracts.contract
 @PublishedApi
 internal const val ASSERTIONS_ENABLED = !IS_RELEASING
 
-// See, https://youtrack.jetbrains.com/issue/KT-22292
+/** See, [KT-22292](https://youtrack.jetbrains.com/issue/KT-22292) */
 inline fun assert(lazyMessage: () -> Any = { "Assertion failed!" }, lazyCheck: () -> Boolean) {
 	if (ASSERTIONS_ENABLED && !lazyCheck()) {
 		throw AssertionError(lazyMessage())
