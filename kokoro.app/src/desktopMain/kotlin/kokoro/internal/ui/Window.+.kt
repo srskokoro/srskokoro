7c2041846afff3c4fb09551f027c571ff486247c
netaneru <netaneru3@gmail.com>
2023-05-20 01:55:53 +0800

    Split into separate files

--- kokoro.app/src/desktopMain/kotlin/kokoro/internal/ui/Window.+.kt
+++ kokoro.app/src/desktopMain/kotlin/kokoro/internal/ui/Window.+.kt
@@ -1,28 +1,7 @@
 package kokoro.internal.ui
 
-import java.awt.Component
 import java.awt.Dimension
 import java.awt.Window
-import kotlin.contracts.ExperimentalContracts
-import kotlin.contracts.InvocationKind
-import kotlin.contracts.contract
-
-@Suppress("NOTHING_TO_INLINE")
-inline fun <C : Component> C.ifVisible(): C? = when {
-	isVisible -> this
-	else -> null
-}
-
-@OptIn(ExperimentalContracts::class)
-inline fun <C : Component, R> C.ifVisible(block: (component: C) -> R): R? {
-	contract {
-		callsInPlace(block, InvocationKind.AT_MOST_ONCE)
-	}
-	return when {
-		isVisible -> block(this)
-		else -> null
-	}
-}
 
 /**
  * Similar to [Window.pack]`()` but without causing the `Window` to be validated
