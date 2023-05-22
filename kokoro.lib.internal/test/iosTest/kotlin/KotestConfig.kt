f1a5d77a26798d7c500790265cc2860da91d3858
netaneru <netaneru3@gmail.com>
2023-05-22 09:19:03 +0800

    Kotest: properly set up parallelism on iOS

--- kokoro.lib.internal/test/iosTest/kotlin/KotestConfig.kt
+++ kokoro.lib.internal/test/iosTest/kotlin/KotestConfig.kt
@@ -1,2 +1,7 @@
 
-actual class KotestConfig : BaseKotestConfig()
+actual class KotestConfig : BaseKotestConfig() {
+	override val parallelism
+		@OptIn(ExperimentalStdlibApi::class)
+		get() = Platform.getAvailableProcessors() // WARNING: May throw `IllegalStateException`
+			.coerceAtLeast(2) // See, https://github.com/Kotlin/kotlinx.coroutines/pull/3366/files
+}
