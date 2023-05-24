56bfe8a1aae97911abfa3131c31ba4e99d4a81f4
netaneru <netaneru3@gmail.com>
2023-05-24 12:04:36 +0800

    Fix: new version of build config plugin has issues with `lintAnalyze*` task

--- gradle/conventions/conv.gmazzo.buildconfig.gradle.kts
+++ gradle/conventions/conv.gmazzo.buildconfig.gradle.kts
@@ -1,3 +1,4 @@
+import com.android.build.gradle.internal.lint.AndroidLintAnalysisTask
 import com.github.gmazzo.gradle.plugins.BuildConfigTask
 import conv.util.*
 
@@ -12,4 +13,11 @@ buildConfig.let {
 
 // Automatically generate build config files on "gradle sync" (via IntelliJ IDEA
 // or Android Studio) -- https://twitter.com/Sellmair/status/1619308362881187840
-tasks.maybeCreate("prepareKotlinIdeaImport").dependsOn(tasks.withType<BuildConfigTask>())
+internal val buildConfigTasks = tasks.withType<BuildConfigTask>()
+tasks.maybeCreate("prepareKotlinIdeaImport").dependsOn(buildConfigTasks)
+
+// Fixes an issue on Android where a `lintAnalyze*` task would use the output of
+// of a `BuildConfigTask` and Gradle would complain since there's neither an
+// explicit nor implicit dependency between the said tasks.
+// TODO Determine the actual root cause and report it :P
+tasks.withType<AndroidLintAnalysisTask>().configureEach { dependsOn(buildConfigTasks) }
