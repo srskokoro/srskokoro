f60f38952e16347f4e7cd1eddc738b5721c76c46
netaneru <netaneru3@gmail.com>
2023-06-12 18:07:29 +0800

    Should restrict only for JVM

--- gradle/plugins/convention/src/main/kotlin/conv/internal/setup/KotlinJvmCompileSetup.kt
+++ gradle/plugins/convention/src/main/kotlin/conv/internal/setup/KotlinJvmCompileSetup.kt
@@ -1,8 +1,8 @@
 package conv.internal.setup
 
-import org.jetbrains.kotlin.gradle.dsl.KotlinCommonCompilerOptions
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmCompilerOptions
 
-internal fun setUp(compilerOptions: KotlinCommonCompilerOptions) = with(compilerOptions) {
+internal fun setUp(compilerOptions: KotlinJvmCompilerOptions) = with(compilerOptions) {
 	// Output Java 8 default methods in interfaces -- https://kotlinlang.org/docs/java-to-kotlin-interop.html#default-methods-in-interfaces
 	freeCompilerArgs.add("-Xjvm-default=all")
 }
