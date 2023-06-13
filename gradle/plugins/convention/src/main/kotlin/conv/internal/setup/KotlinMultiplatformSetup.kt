28b466fb36b545ea81d02b07019790b44703e3c0
netaneru <netaneru3@gmail.com>
2023-06-14 04:14:21 +0800

    Assert that our common "assets" won't become java-style "resources" on Android

--- gradle/plugins/convention/src/main/kotlin/conv/internal/setup/KotlinMultiplatformSetup.kt
+++ gradle/plugins/convention/src/main/kotlin/conv/internal/setup/KotlinMultiplatformSetup.kt
@@ -5,11 +5,15 @@ import assets
 import getAndroidAssets
 import org.gradle.api.Project
 import org.gradle.api.file.Directory
+import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.file.SourceDirectorySet
 import org.gradle.api.plugins.ExtensionAware
 import org.gradle.api.provider.Provider
+import org.gradle.api.provider.ValueSource
+import org.gradle.api.provider.ValueSourceParameters
 import org.gradle.api.tasks.TaskProvider
 import org.gradle.kotlin.dsl.add
+import org.gradle.kotlin.dsl.named
 import org.gradle.kotlin.dsl.withType
 import org.gradle.language.jvm.tasks.ProcessResources
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
@@ -20,6 +24,8 @@ import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinJvmAndroidCompilation
 import org.jetbrains.kotlin.gradle.plugin.sources.android.androidSourceSetInfoOrNull
 import org.jetbrains.kotlin.gradle.targets.jvm.KotlinJvmTarget
 import org.jetbrains.kotlin.gradle.utils.ObservableSet
+import java.io.File
+import java.util.UUID
 import java.util.concurrent.Callable
 
 internal fun Project.setUp(kotlin: KotlinMultiplatformExtension) {
@@ -39,10 +45,21 @@ internal fun Project.setUp(kotlin: KotlinMultiplatformExtension) {
  * - [Why Is ClassLoader.getResourceAsStream So Slow in Android? - nimbledroid : r/androiddev | Reddit](https://www.reddit.com/r/androiddev/comments/4dmflo/why_is_classloadergetresourceasstream_so_slow_in/)
  */
 private fun Project.setUpAssetsDir(kotlin: KotlinMultiplatformExtension) {
+	// The following sets up a dummy directory as an additional "resources"
+	// directory of `commonMain`. This dummy directory contains a dummy file,
+	// which we'll later exclude on JVM targets, but we'll throw if found on
+	// Android targets. The purpose of this setup is to assert that our common
+	// "assets" won't become Java-style "resources" on Android.
+	val dummyName = setUpConvAssetsDummy(kotlin)
+
 	val kotlinTargets = kotlin.targets
 	kotlinTargets.withType<KotlinJvmTarget> {
 		compilations.all {
-			initAssetsAsResources(allKotlinSourceSets, this.project)
+			val project = this.project
+			initAssetsAsResources(allKotlinSourceSets, project)
+			project.tasks.named<@Suppress("UnstableApiUsage") ProcessResources>(processResourcesTaskName) {
+				exclude { it.name == dummyName && it.relativePath.segments.size <= 1 }
+			}
 		}
 	}
 	ifAndroidProject {
@@ -67,6 +84,17 @@ private fun Project.setUpAssetsDir(kotlin: KotlinMultiplatformExtension) {
 
 				mergeAssetsTask.configure { dependsOn(task) }
 			})
+		}
+		tasks.withType<com.android.build.gradle.internal.tasks.ProcessJavaResTask>().configureEach {
+			eachFile {
+				if (name == dummyName && relativePath.segments.size <= 1) throw AssertionError(
+					"""
+					Common "assets" may have been included as Java-style "resources" on Android.
+					This should not happen unless the Kotlin plugin now causes that to happen.
+					Revision of build logic is thus necessary.
+					""".trimIndent()
+				)
+			}
 		}
 	}
 }
@@ -108,3 +136,66 @@ private fun KotlinJvmAndroidCompilation.initConvAssetsProcessingTask(project: Pr
 	}
 	return task to outputDir
 }
+
+private fun Project.setUpConvAssetsDummy(kotlin: KotlinMultiplatformExtension): String {
+	val dummyDirProvider = layout.buildDirectory.dir("generated/convAssetsDummy")
+
+	getSourceSets(kotlin).getByName("commonMain")
+		.resources.srcDir(dummyDirProvider)
+
+	@Suppress("UnstableApiUsage")
+	return providers.of(ConvAssetsDummyNameValueSource::class.java) {
+		parameters.dummyDirProperty.set(dummyDirProvider)
+	}.get()
+}
+
+@Suppress("UnstableApiUsage")
+internal abstract class ConvAssetsDummyNameValueSource :
+	ValueSource<String, ConvAssetsDummyNameValueSource.Parameters> {
+
+	interface Parameters : ValueSourceParameters {
+		val dummyDirProperty: DirectoryProperty
+	}
+
+	override fun obtain(): String {
+		val dummyDir = parameters.dummyDirProperty.get().asFile
+
+		if (dummyDir.exists()) {
+			dummyDir.list()?.let { names ->
+				var dummyName: String? = null
+				for (it in names) if (it.startsWith(DUMMY_NAME_PREFIX)) {
+					if (dummyName == null) dummyName = it // First match found.
+					else return@let // More than one entry matches. Skip below.
+				}
+				if (dummyName != null && dummyName.length == DUMMY_NAME_LENGTH) {
+					return dummyName
+				}
+			}
+			check(dummyDir.deleteRecursively()) { "Directory deletion failed: $dummyDir" }
+		}
+		dummyDir.mkdirs()
+
+		val dummyName = UUID.randomUUID().let {
+			DUMMY_NAME_PREFIX +
+				it.mostSignificantBits.toBase36Padded() +
+				it.leastSignificantBits.toBase36Padded()
+		}
+		if (dummyName.length != DUMMY_NAME_LENGTH)
+			throw AssertionError("Unexpected")
+
+		val dummyFile = File(dummyDir, dummyName)
+		check(dummyFile.createNewFile()) { "File creation failed: $dummyFile" }
+
+		return dummyName
+	}
+}
+
+private fun Long.toBase36Padded(): String =
+	toULong().toString(36).uppercase().padStart(BASE_36_PADDED_LENGTH, '0')
+
+private const val BASE_36_PADDED_LENGTH = 13
+
+private const val DUMMY_NAME_PREFIX = ".dummy#"
+private const val DUMMY_NAME_PREFIX_LENGTH = DUMMY_NAME_PREFIX.length
+
+private const val DUMMY_NAME_LENGTH = DUMMY_NAME_PREFIX_LENGTH + BASE_36_PADDED_LENGTH * 2
