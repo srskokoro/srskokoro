44afed8274033cbfaad2451fc4e75fe1f684c4a5
netaneru <netaneru3@gmail.com>
2023-06-17 18:31:06 +0800

    JCEF installation directory setup & runtime access

--- kokoro.app.desktop/build.gradle.kts
+++ kokoro.app.desktop/build.gradle.kts
@@ -7,6 +7,28 @@ application {
 	mainClass.set("main.MainKt")
 }
 
+afterEvaluate {
+	tasks.withType<JavaExec>().configureEach {
+		// Clear value inherited from `gradlew`; let it be null.
+		environment.remove("APP_HOME")
+
+		val jcefInstallTask = project.jcef.installTask.get()
+		dependsOn(jcefInstallTask)
+		systemProperty("jcef.bundle", jcefInstallTask.outputDir.get()
+			.asFile.let { File(it, "jcef") })
+	}
+}
+
+distributions {
+	main {
+		contents {
+			from(jcef.installTask) {
+				include("jcef/**")
+			}
+		}
+	}
+}
+
 dependencies {
 	deps.bundles.testExtras *= {
 		testImplementation(it)
---
+++ kokoro.app/src/desktopMain/kotlin/kokoro/jcef/JcefSetup.kt
@@ -0,0 +1,17 @@
+package kokoro.jcef
+
+import java.io.File
+
+object JcefSetup {
+	val bundleDir = System.getProperty("jcef.bundle")?.let { File(it) } ?: File(
+		System.getenv("APP_HOME") ?: error(ERROR_JCEF_BUNDLE_DIR_NOT_SET),
+		"jcef",
+	)
+}
+
+private const val ERROR_JCEF_BUNDLE_DIR_NOT_SET = """
+JCEF bundle location not set.
+Must either set system property "jcef.bundle" (pointing to the JCEF install
+directory or bundle), or set up environment variable "APP_HOME" (where
+"APP_HOME/jcef" is the JCEF install directory).
+"""
