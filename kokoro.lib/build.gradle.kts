9fe8167ac2905aba1d22b3a655310fb024a28e33
netaneru <netaneru3@gmail.com>
2023-06-25 03:08:05 +0800

    Prep: `kokoro` library project

---
+++ kokoro.lib/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+	id("kokoro.conv.kt.mpp.lib")
+	id("conv.version")
+}
+
+kotlin {
+	/**
+	 * See 'build.targets.txt' to declare build targets, then use
+	 * `targets.<targetName>` here to configure them further.
+	 *
+	 * See also [conv.internal.setup.setUpTargetsExtensions] in the convention
+	 * plugins.
+	 */
+	@Suppress("UNUSED_VARIABLE") val eat_comment: Nothing
+
+	targets.desktop {
+		// TODO Uncomment eventually to allow `.java` sources -- https://youtrack.jetbrains.com/issue/KT-30878
+		//withJava()
+	}
+}
+
+val NAMESPACE = extra["kokoro.ns"] as String
+
+android {
+	namespace = NAMESPACE
+}
+
+dependencies {
+	deps.bundles.testExtras *= {
+		commonTestImplementation(it)
+	}
+
+	commonMainImplementation(project(":kokoro.lib.internal"))
+}
