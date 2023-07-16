8b094ef81890cacd771da310d22374075be69fe8
netaneru <netaneru3@gmail.com>
2023-07-16 14:27:50 +0800

    Abandoned in favor of 715251ac
    - Reverts 380c5c69

--- gradle/plugins/convention/src/main/kotlin/conv/internal/setup/KotlinSetup.kt
+++ gradle/plugins/convention/src/main/kotlin/conv/internal/setup/KotlinSetup.kt
@@ -71,18 +71,10 @@ private fun SourceDirectorySet.setUpAltSrcDirs(
 		srcDir("src" + File.separatorChar + "#" + subPath)
 		srcDir("src" + File.separatorChar + "+" + subPath)
 
-		srcDir("~src" + File.separatorChar + subPath)
-		srcDir("~src" + File.separatorChar + "#" + subPath)
-		srcDir("~src" + File.separatorChar + "+" + subPath)
-
 		if (isTestSourceSet != 0) {
 			srcDir("test" + File.separatorChar + subPath)
 			srcDir("test" + File.separatorChar + "#" + subPath)
 			srcDir("test" + File.separatorChar + "+" + subPath)
-
-			srcDir("~test" + File.separatorChar + subPath)
-			srcDir("~test" + File.separatorChar + "#" + subPath)
-			srcDir("~test" + File.separatorChar + "+" + subPath)
 
 			if (isTestSourceSet == 1 && subPath.startsWith(TEST_PLUS_FILE_SEP)) {
 				srcDir(subPath)
