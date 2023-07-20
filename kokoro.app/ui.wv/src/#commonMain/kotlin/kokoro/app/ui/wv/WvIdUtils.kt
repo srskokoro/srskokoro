8d2b00e65184edf42d593dab7461f5fc5b83e006
netaneru <netaneru3@gmail.com>
2023-07-21 01:03:06 +0800

    Encoding for element IDs in WebView DOM

---
+++ kokoro.app/ui.wv/src/#commonMain/kotlin/kokoro/app/ui/wv/WvIdUtils.kt
@@ -0,0 +1,32 @@
+package kokoro.app.ui.wv
+
+import kokoro.internal.SPECIAL_USE_IN_TESTS_DEPRECATION
+
+@Deprecated(SPECIAL_USE_IN_TESTS_DEPRECATION)
+internal val wvIdEncodeMap = charArrayOf(
+	'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+	'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+	'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+	'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+	'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '$', '_',
+)
+
+private const val SLOT_BITS = 5
+internal const val MAX_SLOT = (1 shl SLOT_BITS) - 1 // 31
+
+internal fun appendWvElemId(dst: StringBuilder, elemId: Int) {
+	var rem = elemId ushr SLOT_BITS
+	val slot = elemId and MAX_SLOT
+
+	// TODO Benchmark whether filling a `CharArray` first then appending would
+	//  be more performant than appending each `Char` individually as below.
+
+	@Suppress("DEPRECATION")
+	val ecm = wvIdEncodeMap
+	dst.append(ecm[slot])
+
+	do {
+		dst.append(ecm[rem and 63])
+		rem = rem ushr 6
+	} while (rem != 0)
+}
---
+++ kokoro.app/ui.wv/test/#commonTest/kotlin/kokoro/app/ui/wv/WvIdUtils_spec.kt
@@ -0,0 +1,100 @@
+package kokoro.app.ui.wv
+
+import assert
+import io.kotest.assertions.assertSoftly
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.ints.shouldBeExactly
+import io.kotest.matchers.ints.shouldBeLessThan
+import io.kotest.matchers.string.shouldHaveMaxLength
+import io.kotest.matchers.string.shouldHaveMinLength
+import io.kotest.property.Arb
+import io.kotest.property.Exhaustive
+import io.kotest.property.arbitrary.int
+import io.kotest.property.checkAll
+import io.kotest.property.exhaustive.ints
+import io.kotest.property.forAll
+
+class WvIdUtils_spec : FunSpec({
+	test("The encoding map is exactly $wvIdEncodeMap_size in size") {
+		@Suppress("DEPRECATION") val ecm = wvIdEncodeMap
+		ecm.size.shouldBeExactly(wvIdEncodeMap_size)
+	}
+	test("All characters in the encoding map are ASCII characters") {
+		checkAll(Exhaustive.ints(0 until wvIdEncodeMap_size)) { i ->
+			@Suppress("DEPRECATION")
+			val c = wvIdEncodeMap[i]
+			c.code.shouldBeLessThan(ASCII_TABLE_SIZE)
+		}
+	}
+	test("The first characters of the encoding map, up to $MAX_SLOT inclusive, are valid starting characters in a dot notation property access in JS") {
+		forAll(Exhaustive.ints(0..MAX_SLOT)) { i ->
+			@Suppress("DEPRECATION")
+			val c = wvIdEncodeMap[i]
+
+			// Quote:
+			// > An identifier must start with `$`, `_`, or any character in the
+			// > Unicode categories “Uppercase letter (Lu)”, “Lowercase letter
+			// > (Ll)”, “Titlecase letter (Lt)”, “Modifier letter (Lm)”, “Other
+			// > letter (Lo)”, or “Letter number (Nl)”.
+			// -- from, https://stackoverflow.com/a/9337047
+			//
+			// NOTE: Although JS allows `$` and `_`, we're reserving them for
+			// special use and should not be allowed here.
+			c.isLetter()
+		}
+	}
+	test("Output of `appendWvElemId` is as expected") {
+		checkAll(Arb.int()) { elemId ->
+			val elemIdStr = buildString {
+				appendWvElemId(this, elemId)
+			}
+			assertSoftly {
+				elemIdStr
+					.shouldHaveMinLength(2)
+					.shouldHaveMaxLength(6)
+			}
+			parseWvElemId(elemIdStr)
+				.shouldBeExactly(elemId)
+		}
+	}
+})
+
+private const val wvIdEncodeMap_size = 64
+
+private const val ASCII_TABLE_SIZE = 128
+
+private val wvIdDecodeMap = ByteArray(ASCII_TABLE_SIZE) { -1 }.also { dcm ->
+	@Suppress("DEPRECATION") val ecm = wvIdEncodeMap
+	for ((i, c) in ecm.withIndex()) {
+		val cc = c.code
+		if (cc < ASCII_TABLE_SIZE) {
+			dcm[cc] = i.toByte()
+		}
+	}
+}
+
+private fun parseWvElemId(src: String): Int {
+	val n = src.length
+	assert { n in 2..6 }
+
+	val dcm = wvIdDecodeMap
+	var i = 0
+
+	var res = src[i++].code.let { cc ->
+		assert { cc <= 127 }
+		val r = dcm[cc and 127].toInt()
+		assert { r <= 31 }
+		r and 31
+	}
+
+	var sh = -1
+	do {
+		val cc = src[i++].code
+		assert { cc <= 127 }
+		val r = dcm[cc and 127].toInt()
+		sh += 6
+		res = res or (r shl sh)
+	} while (i < n)
+
+	return res
+}
