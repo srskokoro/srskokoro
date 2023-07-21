d0f5eb1b6ee8a4e96971cd62d6704f6477c7e8b6
netaneru <netaneru3@gmail.com>
2023-07-21 20:42:43 +0800

    Exposed as `public` static fields instead

--- kokoro.app/src/androidMain/kotlin/kokoro/app/AppAssets.kt
+++ kokoro.app/src/androidMain/kotlin/kokoro/app/AppAssets.kt
@@ -1,15 +1,16 @@
 package kokoro.app
 
+import android.content.res.AssetManager
 import okio.Source
 import okio.source
 import java.io.InputStream
 
-actual object AppAssets
+actual object AppAssets {
+	// NOTE: If necessary, make this `volatile` and update on every application
+	// configuration change -- see, `Application.onConfigurationChanged()`
+	@JvmField val manager: AssetManager = MainApplication.get().assets
+}
 
-// NOTE: If necessary, make this `volatile` and update on every application
-// configuration change -- see, `Application.onConfigurationChanged()`
-private val assets = MainApplication.get().assets
+actual fun AppAssets.open(path: String): Source = manager.open(path).source()
 
-actual fun AppAssets.open(path: String): Source = assets.open(path).source()
-
-actual fun AppAssets.openStream(path: String): InputStream = assets.open(path)
+actual fun AppAssets.openStream(path: String): InputStream = manager.open(path)
--- kokoro.app/src/desktopMain/kotlin/kokoro/app/AppAssets.kt
+++ kokoro.app/src/desktopMain/kotlin/kokoro/app/AppAssets.kt
@@ -5,15 +5,15 @@ import okio.Source
 import okio.source
 import java.io.InputStream
 
-actual object AppAssets
-
-private val cl = AppAssets::class.java.classLoader
+actual object AppAssets {
+	@JvmField val resourceLoader: ClassLoader = AppAssets::class.java.classLoader
+}
 
 actual fun AppAssets.open(path: String): Source =
-	(cl.getResourceAsStream(path) ?: throw errorForMissingAsset(path))
+	(resourceLoader.getResourceAsStream(path) ?: throw errorForMissingAsset(path))
 		.source()
 
 actual fun AppAssets.openStream(path: String): InputStream =
-	cl.getResourceAsStream(path) ?: throw errorForMissingAsset(path)
+	resourceLoader.getResourceAsStream(path) ?: throw errorForMissingAsset(path)
 
 private fun errorForMissingAsset(path: String) = FileNotFoundException("Asset file: $path")
