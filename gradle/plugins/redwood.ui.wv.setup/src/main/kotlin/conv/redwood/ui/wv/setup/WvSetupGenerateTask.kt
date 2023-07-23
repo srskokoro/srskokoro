b77b17d25b1387c7b652db2a3bc538119f0db108
netaneru <netaneru3@gmail.com>
2023-07-24 01:27:25 +0800

    Implemented the JS minification with source map output

--- gradle/plugins/redwood.ui.wv.setup/src/main/kotlin/conv/redwood/ui/wv/setup/WvSetupGenerateTask.kt
+++ gradle/plugins/redwood.ui.wv.setup/src/main/kotlin/conv/redwood/ui/wv/setup/WvSetupGenerateTask.kt
@@ -1,5 +1,9 @@
 package conv.redwood.ui.wv.setup
 
+import com.google.javascript.jscomp.Compiler
+import com.google.javascript.jscomp.CompilerOptions
+import com.google.javascript.jscomp.SourceFile
+import com.google.javascript.jscomp.SourceMap
 import org.gradle.api.DefaultTask
 import org.gradle.api.file.ArchiveOperations
 import org.gradle.api.file.ConfigurableFileCollection
@@ -14,6 +18,7 @@ import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
+import java.io.File
 import javax.inject.Inject
 
 @CacheableTask
@@ -65,7 +70,41 @@ abstract class WvSetupGenerateTask @Inject constructor(
 		val wvSetupJsFile = wvSetupBuilder.resolveOutputFile(assetsOutput.get(), "${WvSetupBuilder.JS_BASE_NAME}.js")
 		wvSetupJsFile.writeText(wvSetupJs)
 
+		val wvSetupJsSourceFile = SourceFile.fromCode(wvSetupJsFile.path, wvSetupJs)
 		val wvSetupJsDir = wvSetupJsFile.parentFile
-		// TODO Generate minified version of JS, along with a source map
+
+		val wvSetupMinJsFilename = "${WvSetupBuilder.JS_BASE_NAME}.min.js"
+		val wvSetupMinJsFile = File(wvSetupJsDir, wvSetupMinJsFilename)
+		val wvSetupMinJsMapFile = File(wvSetupJsDir, "${WvSetupBuilder.JS_BASE_NAME}.min.js.map")
+
+		val jsCompilerOptions = CompilerOptions()
+		jsCompilerOptions.sourceMapOutputPath = wvSetupMinJsMapFile.absolutePath
+		jsCompilerOptions.sourceMapLocationMappings = listOf(SourceMap.LocationMapping { loc ->
+			// The original JS source should be relative to the output source map file
+			loc.substringAfterLast('/').takeIf { it.isNotEmpty() }
+		})
+
+		// TODO Properly set up the compiler options
+
+		val jsCompiler = Compiler()
+		val result = jsCompiler.compile(
+			listOf(), // TODO Don't know what this is for just yet
+			listOf(wvSetupJsSourceFile),
+			jsCompilerOptions,
+		)
+
+		if (result.success) {
+			val wvSetupMinJs = jsCompiler.toSource()
+
+			// Output the minified JS
+			wvSetupMinJsFile.writeText(wvSetupMinJs) // NOTE: Truncates if file already exists.
+
+			// Output the source map for the minified JS
+			wvSetupMinJsMapFile.writeText(buildString {
+				result.sourceMap.appendTo(this, wvSetupMinJsFilename)
+			})
+		} else {
+			// TODO Report errors and warnings
+		}
 	}
 }
