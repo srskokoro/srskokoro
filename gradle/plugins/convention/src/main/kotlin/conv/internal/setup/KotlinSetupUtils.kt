ff8c9fe76e07fe0af8763f969f8999ea409304b6
netaneru <netaneru3@gmail.com>
2023-07-23 20:23:43 +0800

    Exposed as a utility for other projects to use

--- gradle/plugins/convention/src/main/kotlin/conv/internal/setup/KotlinSetupUtils.kt
+++ gradle/plugins/convention/src/main/kotlin/conv/internal/setup/KotlinSetupUtils.kt
@@ -7,7 +7,7 @@ import org.gradle.api.plugins.ExtensionContainer
 import org.jetbrains.kotlin.gradle.dsl.KotlinProjectExtension
 import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 
-internal fun getSourceSets(kotlin: KotlinProjectExtension): NamedDomainObjectContainer<KotlinSourceSet> {
+fun getSourceSets(kotlin: KotlinProjectExtension): NamedDomainObjectContainer<KotlinSourceSet> {
 	// It's more efficient to get it this way, since `kotlin.sourceSets`
 	// allocates a new object every time (as of Kotlin Gradle Plugin 1.8.0)
 	// - Also throws if our assumption (that it's an extension) is incorrect.
