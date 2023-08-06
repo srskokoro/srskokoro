57668524f513c6e5a8900dce0420c3dac6eba806
netaneru <netaneru3@gmail.com>
2023-08-06 23:19:43 +0800

    Convenience for processing elements before the move or removal

--- kokoro.lib.internal/src/+appMain/kotlin/kokoro/internal/collections/MutableList.+.kt
+++ kokoro.lib.internal/src/+appMain/kotlin/kokoro/internal/collections/MutableList.+.kt
@@ -1,5 +1,27 @@
 package kokoro.internal.collections
 
+// NOTE:
+// - The receiver is specifically `ArrayList` for guaranteed `RandomAccess`.
+// - The function param is marked `crossinline` to prevent non-local returns.
+inline fun <T> ArrayList<T>.move(fromIndex: Int, toIndex: Int, count: Int, crossinline action: (T) -> Unit) {
+	for (i in fromIndex until fromIndex + count) {
+		action(get(i))
+	}
+	move(fromIndex, toIndex, count)
+}
+
+// NOTE:
+// - The receiver is specifically `ArrayList` for guaranteed `RandomAccess`.
+// - The function param is marked `crossinline` to prevent non-local returns.
+inline fun <T> ArrayList<T>.remove(index: Int, count: Int, crossinline action: (T) -> Unit) {
+	for (i in index until index + count) {
+		action(get(i))
+	}
+	remove(index, count)
+}
+
+// --
+
 // From, https://github.com/cashapp/redwood/blob/0.5.0/build-support/src/main/resources/app/cash/redwood/buildsupport/composeHelpers.kt
 fun <T> MutableList<T>.move(fromIndex: Int, toIndex: Int, count: Int) {
 	val dest = if (fromIndex > toIndex) toIndex else toIndex - count
