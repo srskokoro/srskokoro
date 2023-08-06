fc17661e7dcecc5ecc8b5d682825fdcb4f7430a4
netaneru <netaneru3@gmail.com>
2023-08-06 18:50:42 +0800

    Initial implementation of `Widget.Children`

---
+++ kokoro.app/ui.wv/src/#commonMain/kotlin/kokoro/app/ui/wv/widget/WvWidgetChildren.kt
@@ -0,0 +1,51 @@
+package kokoro.app.ui.wv.widget
+
+import app.cash.redwood.widget.Widget
+import kotlin.jvm.JvmField
+
+open class WvWidgetChildren(@JvmField val parent: WvWidget) : Widget.Children<WvWidget> {
+
+	override fun insert(index: Int, widget: Widget<WvWidget>) {
+		val parent = parent
+		val cmd = parent.binder.bindingCommand
+
+		cmd.append("I$(")
+		cmd.append(parent._widgetId)
+
+		cmd.append(','); cmd.append(index)
+
+		cmd.append(',')
+		cmd.append((widget as WvWidget)._widgetId)
+
+		cmd.append(")\n")
+	}
+
+	override fun move(fromIndex: Int, toIndex: Int, count: Int) {
+		val parent = parent
+		val cmd = parent.binder.bindingCommand
+
+		cmd.append("S$(")
+		cmd.append(parent._widgetId)
+
+		cmd.append(','); cmd.append(fromIndex)
+		cmd.append(','); cmd.append(toIndex)
+		cmd.append(','); cmd.append(count)
+
+		cmd.append(")\n")
+	}
+
+	override fun remove(index: Int, count: Int) {
+		val parent = parent
+		val cmd = parent.binder.bindingCommand
+
+		cmd.append("R$(")
+		cmd.append(parent._widgetId)
+
+		cmd.append(','); cmd.append(index)
+		cmd.append(','); cmd.append(count)
+
+		cmd.append(")\n")
+	}
+
+	override fun onModifierUpdated() = Unit
+}
