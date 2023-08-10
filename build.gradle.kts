482247803e0b4f61421fd91df567844b12082438
netaneru <netaneru3@gmail.com>
2023-08-10 21:25:21 +0800

    Temporary workaround for broken `-x` Gradle CLI option

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,19 @@ plugins {
 	id("com.louiscad.complete-kotlin")
 }
 
+// KLUDGE Can't yet exclude "testRelease" tasks via the `-x` CLI option.
+//  - Issue since Gradle 7.6: https://github.com/gradle/gradle/issues/24341
+//  - See also, https://github.com/gradle/gradle/pull/25214
+if (gradle.startParameter.taskNames.any { it == "check" || it == "test" }) {
+	allprojects(fun Project.() = afterEvaluate(fun Project.() = tasks.all(fun(task: Task) {
+		if (task.name.contains("testRelease", ignoreCase = true)) {
+			// The following attempts to simulate the `-x` Gradle CLI option
+			task.onlyIf { false } // Prevents task execution
+			task.setDependsOn(emptyList<Any?>()) // Clears task dependencies
+		}
+	})))
+}
+
 // NOTE: Only modify the `group` for direct subprojects of this project; let
 // Gradle automatically provide a unique `group` for subprojects of subprojects.
 // - See also, https://github.com/gradle/gradle/issues/847#issuecomment-1205001575
