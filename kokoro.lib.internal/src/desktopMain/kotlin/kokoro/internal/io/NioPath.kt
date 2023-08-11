8d149b9dcbe926d018f1c5396b0ddc7059886c86
netaneru <netaneru3@gmail.com>
2023-08-11 23:36:35 +0800

    Convenient documentation for quick reference

--- kokoro.lib.internal/src/desktopMain/kotlin/kokoro/internal/io/NioPath.kt
+++ kokoro.lib.internal/src/desktopMain/kotlin/kokoro/internal/io/NioPath.kt
@@ -5,8 +5,18 @@ import java.nio.file.Path
 
 typealias NioPath = Path
 
+/**
+ * @throws java.nio.file.InvalidPathException
+ *
+ * @see Path.of
+ */
 @Suppress("NOTHING_TO_INLINE")
 inline fun String.toNioPath(): Path = NioPath.of(this)
 
+/**
+ * @throws java.nio.file.InvalidPathException
+ *
+ * @see File.toPath
+ */
 @Suppress("NOTHING_TO_INLINE")
 inline fun File.toNioPath(): Path = toPath()
