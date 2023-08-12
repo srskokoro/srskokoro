8eab91da2e2f1f5ecee2b05acf5aa869af583e7c
netaneru <netaneru3@gmail.com>
2023-08-12 10:19:49 +0800

    Fix: `kotlin-js` gradle plugin is deprecated
    - The replacement is `kotlin-multiplatform` gradle plugin.
    - See, https://kotlinlang.org/docs/whatsnew19.html#deprecation-of-the-kotlin-js-gradle-plugin

--- gradle/conventions/conv.kt.js.gradle.kts
+++ gradle/conventions/conv.kt.js.gradle.kts
@@ -1,21 +1,7 @@
-import conv.internal.setup.*
-
 plugins {
-	id("conv.base")
-	kotlin("js")
-	id("io.kotest.multiplatform")
+	id("conv.kt.mpp")
 }
 
 kotlin {
 	js(IR)
-	setUp(this)
-}
-
-dependencies {
-	setUpTestFrameworkDeps_js {
-		testImplementation(it)
-	}
-	setUpTestCommonDeps {
-		testImplementation(it)
-	}
 }
