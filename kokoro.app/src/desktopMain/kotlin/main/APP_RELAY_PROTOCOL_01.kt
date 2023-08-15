cb3511462ff0c3fb05f4a5bb8fa18b128ceb9ef1
netaneru <netaneru3@gmail.com>
2023-08-15 09:25:35 +0800

    Extracted into a separate file to improve maintenance

---
+++ kokoro.app/src/desktopMain/kotlin/main/APP_RELAY_PROTOCOL_01.kt
@@ -0,0 +1,116 @@
+package main
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.swing.Swing
+import kotlinx.coroutines.withContext
+import okio.BufferedSource
+import okio.EOFException
+import java.io.IOException
+import java.nio.ByteBuffer
+import java.nio.channels.ClosedChannelException
+
+internal object APP_RELAY_PROTOCOL_01 {
+	const val ID = 1
+
+	fun shove(args: Array<out String>, buffer: okio.Buffer): Long {
+		if (ID > Byte.MAX_VALUE) throw AssertionError(
+			"Should be implemented as a varint at this point"
+		)
+		buffer.writeByte(ID)
+
+		// We'll pass the current working directory plus command arguments
+		val payloadCount = 1 + args.size
+		buffer.writeInt(payloadCount)
+
+		// Reserves a header for the UTF-8 lengths
+		val payloadUtf8LengthsOffset = buffer.size
+		var size = payloadUtf8LengthsOffset + Int.SIZE_BYTES * payloadCount
+
+		val unsafe = okio.Buffer.UnsafeCursor()
+		buffer.readAndWriteUnsafe(unsafe).use { u ->
+			// Needed to ensure a contiguous range of available bytes.
+			// - Throws if larger than the buffer's supported segment size.
+			u.expandBuffer(Math.toIntExact(size - payloadUtf8LengthsOffset))
+			// Resize into where we only plan to write our header
+			u.resizeBuffer(size)
+		}
+
+		val payloadUtf8Lengths = IntArray(payloadCount)
+		var i = 0
+
+		try {
+			val currentDir = System.getProperty("user.dir")
+			buffer.writeUtf8(currentDir).size.let { newSize ->
+				payloadUtf8Lengths[i] = Math.toIntExact(newSize - size)
+				size = newSize
+			}
+			for (arg in args) buffer.writeUtf8(arg).size.let { newSize ->
+				payloadUtf8Lengths[++i] = Math.toIntExact(newSize - size)
+				size = newSize
+			}
+		} catch (ex: ArithmeticException) {
+			throw IOException(
+				(if (i == 0) "Current working directory too long."
+				else "Command argument too long (at index ${i - 1}).") +
+					" Length in UTF-8 must be less than " + Short.MAX_VALUE +
+					" bytes.", ex)
+		}
+
+		// Fill the reserved header with the UTF-8 lengths
+		buffer.readAndWriteUnsafe(unsafe).use { u ->
+			u.seek(payloadUtf8LengthsOffset)
+			// Not sure if a loop would be more efficient. Anyway…
+			ByteBuffer.wrap(u.data, u.start, u.end - u.start)
+				.asIntBuffer().put(payloadUtf8Lengths)
+		}
+
+		return size
+	}
+
+	suspend fun consume(source: BufferedSource, dispatcher: AppDispatcher) {
+		val workingDir: String
+		val args: Array<String>
+
+		try {
+			val payloadCount = source.readInt()
+			if (payloadCount > 0) {
+				val argsSize = payloadCount - 1
+				@Suppress("UNCHECKED_CAST")
+				args = arrayOfNulls<String>(argsSize) as Array<String>
+
+				val workingDirLength = source.readInt()
+				val argsLengths = IntArray(argsSize)
+				repeat(argsSize) { i ->
+					argsLengths[i] = source.readInt()
+				}
+
+				workingDir = source.readUtf8(workingDirLength.toLong())
+				repeat(argsSize) { i ->
+					args[i] = source.readUtf8(argsLengths[i].toLong())
+				}
+			} else {
+				workingDir = ""
+				args = emptyArray()
+			}
+		} catch (ex: IOException) {
+			when (ex) {
+				is ClosedChannelException, is EOFException -> {
+					// Client disconnected early.
+					// Perhaps its process got killed.
+					// Anyway, abort.
+					throw AppDaemon.AbortClientConnection()
+				}
+				else -> throw ex
+			}
+		}
+
+		@Suppress("BlockingMethodInNonBlockingContext")
+		// Close the client connection early, as we might be about to run for a
+		// very long time. Doing so may throw -- let it!
+		source.close()
+
+		withContext(Dispatchers.Swing) {
+			dispatcher.dispatch(this, workingDir, args, isInit = false)
+		}
+	}
+}
--- kokoro.app/src/desktopMain/kotlin/main/main.kt
+++ kokoro.app/src/desktopMain/kotlin/main/main.kt
@@ -114,9 +114,6 @@ internal fun runSingleProcessModel(args: Array<out String>, dispatcher: AppDispa
 
 // --
 
-private const val APP_RELAY_PROTOCOL_01 = 0x01
-private const val APP_RELAY_PROTOCOL_DEFAULT = APP_RELAY_PROTOCOL_01
-
 private object ClientHandlingSwingScope : CoroutineScope {
 	override val coroutineContext = SupervisorJob() + Dispatchers.Swing + StackTraceModal
 }
@@ -214,7 +211,7 @@ internal class AppDaemon(
 							sendVersionCode(client)
 							val source = Channels.newInputStream(client).source().buffer()
 							when (val protocol = identifyRelayProtocol(source)) {
-								APP_RELAY_PROTOCOL_01 -> APP_RELAY_PROTOCOL_01_impl(source)
+								APP_RELAY_PROTOCOL_01.ID -> APP_RELAY_PROTOCOL_01.consume(source, dispatcher)
 								else -> throw UnsupportedOperationException(
 									"Unknown relay protocol: 0x${protocol.toString(16)} ($protocol)")
 							}
@@ -299,53 +296,6 @@ internal class AppDaemon(
 				}
 				else -> throw ex
 			}
-		}
-	}
-
-	private suspend fun APP_RELAY_PROTOCOL_01_impl(source: BufferedSource) {
-		val workingDir: String
-		val args: Array<String>
-
-		try {
-			val payloadCount = source.readInt()
-			if (payloadCount > 0) {
-				val argsSize = payloadCount - 1
-				@Suppress("UNCHECKED_CAST")
-				args = arrayOfNulls<String>(argsSize) as Array<String>
-
-				val workingDirLength = source.readInt()
-				val argsLengths = IntArray(argsSize)
-				repeat(argsSize) { i ->
-					argsLengths[i] = source.readInt()
-				}
-
-				workingDir = source.readUtf8(workingDirLength.toLong())
-				repeat(argsSize) { i ->
-					args[i] = source.readUtf8(argsLengths[i].toLong())
-				}
-			} else {
-				workingDir = ""
-				args = emptyArray()
-			}
-		} catch (ex: IOException) {
-			when (ex) {
-				is ClosedChannelException, is EOFException -> {
-					// Client disconnected early.
-					// Perhaps its process got killed.
-					// Anyway, abort.
-					throw AbortClientConnection()
-				}
-				else -> throw ex
-			}
-		}
-
-		@Suppress("BlockingMethodInNonBlockingContext")
-		// Close the client connection early, as we might be about to run for a
-		// very long time. Doing so may throw -- let it!
-		source.close()
-
-		withContext(Dispatchers.Swing) {
-			dispatcher.dispatch(this, workingDir, args, isInit = false)
 		}
 	}
 
@@ -466,57 +416,7 @@ internal class AppRelay(sockDir: String) {
 		val version = serverVersionCode
 		if (version == AppBuild.VERSION_CODE) try {
 			val buffer = okio.Buffer()
-			if (APP_RELAY_PROTOCOL_DEFAULT > Byte.MAX_VALUE) throw AssertionError(
-				"Should be implemented as a varint at this point"
-			)
-			buffer.writeByte(APP_RELAY_PROTOCOL_DEFAULT)
-
-			// We'll pass the current working directory plus command arguments
-			val payloadCount = 1 + args.size
-			buffer.writeInt(payloadCount)
-
-			// Reserves a header for the UTF-8 lengths
-			val payloadUtf8LengthsOffset = buffer.size
-			var size = payloadUtf8LengthsOffset + Int.SIZE_BYTES * payloadCount
-
-			val unsafe = okio.Buffer.UnsafeCursor()
-			buffer.readAndWriteUnsafe(unsafe).use { u ->
-				// Needed to ensure a contiguous range of available bytes.
-				// - Throws if larger than the buffer's supported segment size.
-				u.expandBuffer(Math.toIntExact(size - payloadUtf8LengthsOffset))
-				// Resize into where we only plan to write our header
-				u.resizeBuffer(size)
-			}
-
-			val payloadUtf8Lengths = IntArray(payloadCount)
-			var i = 0
-
-			try {
-				val currentDir = System.getProperty("user.dir")
-				buffer.writeUtf8(currentDir).size.let { newSize ->
-					payloadUtf8Lengths[i] = Math.toIntExact(newSize - size)
-					size = newSize
-				}
-				for (arg in args) buffer.writeUtf8(arg).size.let { newSize ->
-					payloadUtf8Lengths[++i] = Math.toIntExact(newSize - size)
-					size = newSize
-				}
-			} catch (ex: ArithmeticException) {
-				throw IOException(
-					(if (i == 0) "Current working directory too long."
-					else "Command argument too long (at index ${i - 1}).") +
-						" Length in UTF-8 must be less than " + Short.MAX_VALUE +
-						" bytes.", ex)
-			}
-
-			// Fill the reserved header with the UTF-8 lengths
-			buffer.readAndWriteUnsafe(unsafe).use { u ->
-				u.seek(payloadUtf8LengthsOffset)
-				// Not sure if a loop would be more efficient. Anyway…
-				ByteBuffer.wrap(u.data, u.start, u.end - u.start)
-					.asIntBuffer().put(payloadUtf8Lengths)
-			}
-
+			val size = APP_RELAY_PROTOCOL_01.shove(args, buffer)
 			val sink = Channels.newOutputStream(client).sink()
 			try {
 				sink.write(buffer, size) // Send it!
