fdc47af893d69b12e669a5a3b584e909441fdb98
netaneru <netaneru3@gmail.com>
2023-08-16 06:09:54 +0800

    Rearranged

--- gradle/meta-plugins/settings/src/main/kotlin/conv.settings.settings.gradle.kts
+++ gradle/meta-plugins/settings/src/main/kotlin/conv.settings.settings.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
 	id("conv.settings.base")
-	id("conv.settings.auto-includes.plugins")
 	id("conv.settings.auto-includes.deps")
+	id("conv.settings.auto-includes.plugins")
 	id("conv.settings.auto-includes.structure")
 }
