7d8c2eef777bb57fe5b4c568dbde68bca9198aad
netaneru <netaneru3@gmail.com>
2023-08-16 06:08:49 +0800

    Auto-inclusion of the "dependencies" project now also sets up "deps" and "repositories" settings plugins

--- gradle/meta-plugins/settings/src/main/kotlin/conv.settings.settings.gradle.kts
+++ gradle/meta-plugins/settings/src/main/kotlin/conv.settings.settings.gradle.kts
@@ -1,7 +1,5 @@
 plugins {
 	id("conv.settings.base")
-	id("conv.settings.deps")
-	id("conv.settings.repositories")
 	id("conv.settings.auto-includes.plugins")
 	id("conv.settings.auto-includes.deps")
 	id("conv.settings.auto-includes.structure")
