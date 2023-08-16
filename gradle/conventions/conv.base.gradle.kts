a55593cdb6cca315bd828671c6f4278e0a2aaf60
netaneru <netaneru3@gmail.com>
2023-08-16 17:12:09 +0800

    Don't include a version in the archive names, by default

--- gradle/conventions/conv.base.gradle.kts
+++ gradle/conventions/conv.base.gradle.kts
@@ -2,6 +2,12 @@ plugins {
 	base
 }
 
+tasks.withType<AbstractArchiveTask>().configureEach {
+	// By default, don't include the version in the names of output archives.
+	// - This clears the default behavior set by the `base` plugin.
+	archiveVersion.convention(null as String?)
+}
+
 configurations.all {
 	if (isCanBeResolved) {
 		// Fail on transitive upgrade/downgrade of direct dependency versions
