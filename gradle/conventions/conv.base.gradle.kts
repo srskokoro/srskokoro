deadc35e60d5046733f1e6849fdca7adf409b9ae
netaneru <netaneru3@gmail.com>
2023-08-17 14:59:15 +0800

    Ensured builds are reproducible

--- gradle/conventions/conv.base.gradle.kts
+++ gradle/conventions/conv.base.gradle.kts
@@ -6,6 +6,11 @@ tasks.withType<AbstractArchiveTask>().configureEach {
 	// By default, don't include the version in the names of output archives.
 	// - This clears the default behavior set by the `base` plugin.
 	archiveVersion.convention(null as String?)
+
+	// The following ensures that builds are reproducible.
+	// - See, https://docs.gradle.org/8.2.1/userguide/working_with_files.html#sec:reproducible_archives
+	isPreserveFileTimestamps = false
+	isReproducibleFileOrder = true
 }
 
 configurations.all {
