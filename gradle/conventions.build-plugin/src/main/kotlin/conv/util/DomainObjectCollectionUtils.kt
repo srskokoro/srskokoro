dc7c3863136a4fba8111e3f4c8dc84f720ebf275
netaneru <netaneru3@gmail.com>
2023-08-19 14:48:40 +0800

    Better + Optimized

--- gradle/conventions.build-plugin/src/main/kotlin/conv/util/DomainObjectCollectionUtils.kt
+++ gradle/conventions.build-plugin/src/main/kotlin/conv/util/DomainObjectCollectionUtils.kt
@@ -7,24 +7,24 @@ import kotlin.reflect.KClass
 /**
  * @see DomainObjectCollection.withType
  */
-inline fun <T : Any, D : DomainObjectCollection<in T>> D.onType(
-	type: KClass<T>,
+inline fun <reified T : Any, D : DomainObjectCollection<in T>> D.onType(
+	@Suppress("UNUSED_PARAMETER") type: KClass<T>,
 	crossinline configuration: T.() -> Unit,
 ): D {
-	withType(type.java) { configuration() }
+	all { if (this is T) configuration() }
 	return this
 }
 
 /**
  * @see DomainObjectCollection.withType
  */
-inline fun <T : Any, D : DomainObjectCollection<in T>> D.onType(
-	type: KClass<T>,
+inline fun <reified T : Any, D : DomainObjectCollection<in T>> D.onType(
+	@Suppress("UNUSED_PARAMETER") type: KClass<T>,
 	crossinline filter: (T) -> Boolean,
 	crossinline configuration: T.() -> Unit,
 ): D {
-	withType(type.java) {
-		if (filter(this)) {
+	all {
+		if (this is T && filter(this)) {
 			configuration()
 		}
 	}
