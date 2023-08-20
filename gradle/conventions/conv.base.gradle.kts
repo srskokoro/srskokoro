3eb8fd9d4e94bb5d5ca046e3748bbbf763f5fe0b
netaneru <netaneru3@gmail.com>
2023-08-20 11:51:47 +0800

    Better

--- gradle/conventions/conv.base.gradle.kts
+++ gradle/conventions/conv.base.gradle.kts
@@ -17,11 +17,11 @@ tasks.withType<AbstractArchiveTask>().configureEach {
 	isReproducibleFileOrder = true
 }
 
-prioritizedAfterEvaluate {
+prioritizedAfterEvaluate(fun Project.() {
 	// Don't proceed if we're either a direct child of the root project or the
 	// root project itself.
-	if (depth <= 1) return@prioritizedAfterEvaluate
-	val parent = parent ?: return@prioritizedAfterEvaluate
+	if (depth <= 1) return
+	val parent = parent ?: return
 
 	// NOTE: Blocks until the parent project is evaluated.
 	evaluationDependsOn(parent.path)
@@ -56,7 +56,7 @@ prioritizedAfterEvaluate {
 		if (android.namespace == null)
 			android.autoNamespaceOrNop(project)
 	}
-}
+})
 
 configurations.configureEach {
 	if (isCanBeResolved) {
