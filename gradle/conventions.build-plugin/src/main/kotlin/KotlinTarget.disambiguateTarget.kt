dbb17fcd746db52e5c43b2671684fa0c2f4c346a
netaneru <netaneru3@gmail.com>
2023-08-21 03:41:17 +0800

    Better clarity

--- gradle/conventions.build-plugin/src/main/kotlin/KotlinTarget.disambiguateTarget.kt
+++ gradle/conventions.build-plugin/src/main/kotlin/KotlinTarget.disambiguateTarget.kt
@@ -5,11 +5,7 @@ import org.jetbrains.kotlin.gradle.plugin.KotlinTarget
 /** @see KotlinTarget.disambiguateTarget */
 fun KotlinTarget.disambiguateTargetWithItsName() = disambiguateTarget(targetName)
 
-/**
- * Used for distinguishing several targets for one platform.
- *
- * See, “[Distinguish several targets for one platform | Set up targets for Kotlin Multiplatform | Kotlin Documentation](https://kotlinlang.org/docs/multiplatform-set-up-targets.html#distinguish-several-targets-for-one-platform)”
- */
+/** @see TARGET_DISAMBIGUATOR_ATTRIBUTE */
 fun KotlinTarget.disambiguateTarget(disambiguator: String) {
 	attributes.attribute(TARGET_DISAMBIGUATOR_ATTRIBUTE, disambiguator)
 }
--- gradle/conventions.build-plugin/src/main/kotlin/conv/attributes/TARGET_DISAMBIGUATOR_ATTRIBUTE.kt
+++ gradle/conventions.build-plugin/src/main/kotlin/conv/attributes/TARGET_DISAMBIGUATOR_ATTRIBUTE.kt
@@ -2,5 +2,10 @@ package conv.attributes
 
 import org.gradle.api.attributes.Attribute
 
+/**
+ * Used for distinguishing several targets for one platform.
+ *
+ * See, “[Distinguish several targets for one platform | Set up targets for Kotlin Multiplatform | Kotlin Documentation](https://kotlinlang.org/docs/multiplatform-set-up-targets.html#distinguish-several-targets-for-one-platform)”
+ */
 val TARGET_DISAMBIGUATOR_ATTRIBUTE: Attribute<String> =
 	Attribute.of("convention:conv.kt.mpp.targetDisambiguator", String::class.java)
