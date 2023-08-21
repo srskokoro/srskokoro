7266281bd2531c94f0bab4bbf5e8716b2e27b36a
netaneru <netaneru3@gmail.com>
2023-08-21 18:36:46 +0800

    Exposed for the other gradle plugin projects to use

--- gradle/conventions.build-plugin/src/main/kotlin/conv/internal/setup/_ExtensionsAccess.kt
+++ gradle/conventions.build-plugin/src/main/kotlin/conv/internal/setup/_ExtensionsAccess.kt
@@ -11,28 +11,28 @@ import org.jetbrains.kotlin.gradle.dsl.KotlinProjectExtension
 import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 
 
-internal val Project.baseExt
+val Project.baseExt
 	get() = extensions.getByName("base") as BasePluginExtension
 
-internal val Project.baseExtOrNull
+val Project.baseExtOrNull
 	get() = extensions.findByName("base") as? BasePluginExtension
 
-internal val Project.androidExt
+val Project.androidExt
 	get() = extensions.getByName("android") as AndroidExtension
 
-internal val Project.androidExtOrNull
+val Project.androidExtOrNull
 	get() = extensions.findByName("android") as? AndroidExtension
 
 /** @see kotlinMppExt */
-internal val Project.kotlinExt
+val Project.kotlinExt
 	get() = extensions.getByName("kotlin") as KotlinProjectExtension
 
 /** @see kotlinExt */
-internal val Project.kotlinMppExt
+val Project.kotlinMppExt
 	get() = extensions.getByName("kotlin") as KotlinMultiplatformExtension
 
 
-internal val Project.deps
+val Project.deps
 	// NOTE: Given that the extension below is set via the `settings` script, it
 	// will be null if Gradle simply evaluated a fake project in order to
 	// generate type-safe model accessors for precompiled script plugins to use.
@@ -41,13 +41,13 @@ internal val Project.deps
 	get() = extensions.findByName("deps") as DependencyVersions?
 
 
-internal val Project.sourceSets
+val Project.sourceSets
 	get() = extensions.getByName("sourceSets") as SourceSetContainer
 
-internal var Project.kotlinSourceSets
+var Project.kotlinSourceSets
 	get() = extensions.getByName("kotlinSourceSets").unsafeCast<NamedDomainObjectContainer<KotlinSourceSet>>()
-	set(value) = extensions.add("kotlinSourceSets", value)
+	internal set(value) = extensions.add("kotlinSourceSets", value)
 
-internal inline fun Project.kotlinSourceSets(
+inline fun Project.kotlinSourceSets(
 	configure: NamedDomainObjectContainer<KotlinSourceSet>.() -> Unit
 ) = configure(kotlinSourceSets)
