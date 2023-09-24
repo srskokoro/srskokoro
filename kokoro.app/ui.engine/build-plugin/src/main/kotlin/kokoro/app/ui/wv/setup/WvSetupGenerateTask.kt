1cf89147256a30abbb09921b68f1a4156119a6cb
netaneru <netaneru3@gmail.com>
2023-09-24 11:20:58 +0800

    Stubbed implementation more + Implemented removal

--- kokoro.app/ui.engine/build-plugin/src/main/kotlin/kokoro/app/ui/wv/setup/WvSetupGenerateTask.kt
+++ kokoro.app/ui.engine/build-plugin/src/main/kotlin/kokoro/app/ui/wv/setup/WvSetupGenerateTask.kt
@@ -1,9 +1,12 @@
 package kokoro.app.ui.wv.setup
 
+import conv.internal.support.removeLast
 import org.gradle.api.DefaultTask
 import org.gradle.api.file.ConfigurableFileCollection
 import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.file.DuplicateFileCopyingException
 import org.gradle.api.file.FileTree
+import org.gradle.api.file.FileType
 import org.gradle.api.model.ObjectFactory
 import org.gradle.api.tasks.CacheableTask
 import org.gradle.api.tasks.IgnoreEmptyDirectories
@@ -14,10 +17,14 @@ import org.gradle.api.tasks.PathSensitive
 import org.gradle.api.tasks.PathSensitivity
 import org.gradle.api.tasks.SkipWhenEmpty
 import org.gradle.api.tasks.TaskAction
+import org.gradle.work.ChangeType
+import org.gradle.work.FileChange
 import org.gradle.work.InputChanges
 import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 import wv
+import java.io.File
 import javax.inject.Inject
+import kotlin.io.path.deleteExisting
 
 @CacheableTask
 abstract class WvSetupGenerateTask @Inject constructor(
@@ -50,6 +57,61 @@ abstract class WvSetupGenerateTask @Inject constructor(
 
 	@TaskAction
 	fun execute(inputChanges: InputChanges) {
-		// TODO!
+		val forGeneration = LinkedHashSet<String>()
+
+		val outputDir = outputDir.get()
+		for (change in inputChanges.getFileChanges(inputFiles)) {
+			if (change.fileType == FileType.DIRECTORY) continue
+
+			val entry = change.normalizedPath
+			// TODO Optimize case checks
+			if (entry.endsWith(".const.wv.js")) {
+				val target = outputDir.file("${entry.removeLast(2)}kt").asFile
+				if (handleForGeneration(target, change, forGeneration)) {
+					generateForConstWvJs(target, change)
+				}
+			} else if (entry.endsWith(".templ.wv.js")) {
+				val target = outputDir.file("${entry.removeLast(2)}kt").asFile
+				if (handleForGeneration(target, change, forGeneration)) {
+					generateForTemplWvJs(target, change)
+				}
+			} else if (entry.endsWith(".wv.lst")) {
+				val target = outputDir.file("${entry}.kt").asFile
+				if (handleForGeneration(target, change, forGeneration)) {
+					generateForWvLst(target, change)
+				}
+			} else {
+				throw AssertionError("Unknown input file format: ${change.file}")
+			}
+		}
 	}
 }
+
+private fun handleForGeneration(target: File, change: FileChange, forGeneration: MutableSet<String>): Boolean {
+	if (change.changeType == ChangeType.REMOVED) {
+		if (change.normalizedPath !in forGeneration && !target.delete()) {
+			// Let the following throw a pretty error message
+			target.toPath().deleteExisting()
+		}
+		return false // Done.
+	}
+	if (!forGeneration.add(change.normalizedPath)) {
+		throw E_DuplicateInput(change)
+	}
+	return true
+}
+
+private fun generateForConstWvJs(target: File, change: FileChange) {
+	// TODO Implement
+}
+
+private fun generateForTemplWvJs(target: File, change: FileChange) {
+	// TODO Implement
+}
+
+private fun generateForWvLst(target: File, change: FileChange) {
+	// TODO Implement
+}
+
+private fun E_DuplicateInput(change: FileChange) =
+	DuplicateFileCopyingException("Entry is present in multiple sources: ${change.normalizedPath}\nInput file: ${change.file}")
