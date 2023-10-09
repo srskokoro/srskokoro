173d1a065e4cce8e55a03a25ebebdc01b8a2c2b2
netaneru <netaneru3@gmail.com>
2023-10-09 22:29:46 +0800

    Better parameter name

--- gradle/build-logic/build-support/src/main/kotlin/conv/internal/support/io/UnsafeCharArrayWriter.kt
+++ gradle/build-logic/build-support/src/main/kotlin/conv/internal/support/io/UnsafeCharArrayWriter.kt
@@ -4,7 +4,7 @@ import java.io.CharArrayWriter
 
 // Inspiration:
 // - https://github.com/JetBrains/kotlin/blob/v1.8.0/libraries/stdlib/jvm/src/kotlin/io/FileReadWrite.kt#L98
-class UnsafeCharArrayWriter(size: Int = 4096) : CharArrayWriter(size) {
+class UnsafeCharArrayWriter(initialCapacity: Int = 4096) : CharArrayWriter(initialCapacity) {
 	val buffer: CharArray get() = buf
 	val size get() = count
 }
