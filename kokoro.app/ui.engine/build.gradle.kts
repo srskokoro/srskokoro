b970ef621564a879de6e124c33e489e18300ed43
netaneru <netaneru3@gmail.com>
2023-10-23 03:34:01 +0800

    Decoupled our UI engine from our redwood generator projects

--- kokoro.app/ui.engine/build.gradle.kts
+++ kokoro.app/ui.engine/build.gradle.kts
@@ -19,11 +19,9 @@ dependencies {
 
 	commonMainImplementation(project(":kokoro.lib.internal"))
 
-	commonMainImplementation(parent.project("redwood:compose"))
-	commonMainImplementation(parent.project("redwood:widget"))
-
 	desktopJvmMainApi(project("jcef"))
 
 	commonMainImplementation("com.soywiz.korlibs.kds:kds")
+	appMainImplementation("app.cash.redwood:redwood-widget")
 	appMainImplementation("cafe.adriel.voyager:voyager-core")
 }
