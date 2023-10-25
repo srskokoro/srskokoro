07539014fe91f107b166d495f9b278cdc0f2727b
netaneru <netaneru3@gmail.com>
2023-10-25 20:55:17 +0800

    Extracted `FrameClock` logic into a reusable utility

--- kokoro.app/src/desktopJvmMain/kotlin/kokoro/app/ui/redwood/RedwoodWindowFrame.kt
+++ kokoro.app/src/desktopJvmMain/kotlin/kokoro/app/ui/redwood/RedwoodWindowFrame.kt
@@ -6,14 +6,13 @@ import app.cash.redwood.ui.UiConfiguration
 import app.cash.redwood.widget.RedwoodView
 import app.cash.redwood.widget.Widget
 import kokoro.app.ui.BaseWindowFrame
+import kokoro.app.ui.compose.SimpleFrameClock
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.plus
-import kotlinx.coroutines.suspendCancellableCoroutine
 import java.awt.EventQueue
 import java.awt.Graphics
 import java.awt.GraphicsConfiguration
-import kotlin.coroutines.Continuation
 
 open class RedwoodWindowFrame : BaseWindowFrame {
 	constructor() : super()
@@ -70,54 +69,11 @@ open class RedwoodWindowFrame : BaseWindowFrame {
 		_frameClock.sendFrame(System.nanoTime() - nanoTimeStart)
 	}
 
-	/** @see androidx.compose.runtime.BroadcastFrameClock */
 	private class FrameClock(
 		private val onFrameNanosSwing: Runnable,
-	) : MonotonicFrameClock {
-
-		private val lock = Any()
-		private var awaiters = ArrayList<FrameAwaiter<*>>()
-		private var spareList = ArrayList<FrameAwaiter<*>>()
-
-		fun sendFrame(timeNanos: Long) {
-			synchronized(lock) {
-				// Rotate the lists so that if a resumed continuation on an
-				// immediate dispatcher bound to the thread calling `sendFrame()`
-				// immediately awaits again we don't disrupt iteration of
-				// resuming the rest.
-				val toResume = awaiters
-				awaiters = spareList
-				spareList = toResume
-
-				for (i in toResume.indices) {
-					toResume[i].resume(timeNanos)
-				}
-				toResume.clear()
-			}
-		}
-
-		override suspend fun <R> withFrameNanos(
-			onFrame: (frameTimeNanos: Long) -> R
-		): R = suspendCancellableCoroutine { co ->
-			val awaiter = FrameAwaiter(onFrame, co)
-			synchronized(lock) {
-				awaiters.add(awaiter)
-			}
-			co.invokeOnCancellation {
-				synchronized(lock) {
-					awaiters.remove(awaiter)
-				}
-			}
+	) : SimpleFrameClock() {
+		override fun onNewAwaiters() {
 			EventQueue.invokeLater(onFrameNanosSwing)
-		}
-	}
-
-	private class FrameAwaiter<R>(
-		val onFrame: (Long) -> R,
-		val continuation: Continuation<R>,
-	) {
-		fun resume(timeNanos: Long) {
-			continuation.resumeWith(runCatching { onFrame(timeNanos) })
 		}
 	}
 
