591c6e3dfee7d5380342d06fafa9477ba1c6b0af
netaneru <netaneru3@gmail.com>
2023-11-03 13:27:25 +0800

    Better

--- kokoro.app/src/desktopJvmMain/kotlin/kokoro/jcef/Jcef.kt
+++ kokoro.app/src/desktopJvmMain/kotlin/kokoro/jcef/Jcef.kt
@@ -20,7 +20,7 @@ import kotlin.jvm.optionals.getOrNull
 object Jcef {
 
 	val bundleDir = System.getProperty("jcef.bundle")?.let { File(it) } ?: File(
-		System.getenv("APP_HOME") ?: error(ERROR_JCEF_BUNDLE_DIR_NOT_SET),
+		System.getenv("APP_HOME") ?: throw E_JcefBundleDirNotSet(),
 		"jcef",
 	)
 
@@ -115,9 +115,11 @@ object Jcef {
 	}
 }
 
-private const val ERROR_JCEF_BUNDLE_DIR_NOT_SET = """
-JCEF bundle location not set.
-Must either set system property "jcef.bundle" (pointing to the JCEF install
-directory or bundle), or set up environment variable "APP_HOME" (where
-"APP_HOME/jcef" is the JCEF install directory).
-"""
+private fun E_JcefBundleDirNotSet() = IllegalStateException(
+	"""
+	JCEF bundle location not set.
+	Must either set system property "jcef.bundle" (pointing to the JCEF install
+	directory or bundle), or set up environment variable "APP_HOME" (where
+	"APP_HOME/jcef" is the JCEF install directory).
+	""".trimIndent()
+)
