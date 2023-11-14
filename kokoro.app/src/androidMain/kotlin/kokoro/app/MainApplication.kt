d91e7b6bc44b1aabda2a503b4129b2965a3c4e89
netaneru <netaneru3@gmail.com>
2023-11-14 17:48:14 +0800

    Moved `kokoro.app.i18n` package to "core.base" module

--- kokoro.app/src/androidMain/kotlin/kokoro/app/MainApplication.kt
+++ kokoro.app/src/androidMain/kotlin/kokoro/app/MainApplication.kt
@@ -1,29 +1,3 @@
 package kokoro.app
 
-import android.content.res.Configuration
-import android.os.Build
-import java.util.Locale
-
-class MainApplication : CoreApplication() {
-
-	override fun onConfigurationChanged(newConfig: Configuration) {
-		super.onConfigurationChanged(newConfig)
-
-		// Update the JVM default `Locale` manually as it may not automatically
-		// be updated for us, at least on some devices -- e.g., in the emulator,
-		// it seems to be automatically updated. See also,
-		// - https://medium.com/@hectorricardomendez/how-to-get-the-current-locale-in-android-fc12d8be6242
-		// - https://stackoverflow.com/a/21844639
-		// - https://developer.android.com/guide/topics/resources/app-languages
-		//
-		val locale = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-			newConfig.locales.get(0)
-		} else {
-			@Suppress("DEPRECATION")
-			newConfig.locale
-		}
-		if (Locale.getDefault() != locale) {
-			Locale.setDefault(locale)
-		}
-	}
-}
+class MainApplication : CoreApplication()
