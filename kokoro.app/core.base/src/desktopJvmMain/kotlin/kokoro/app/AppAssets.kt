5e6b6e9900c2da2b7606b8ba0c41adb508ae82fd
netaneru <netaneru3@gmail.com>
2023-11-14 19:05:51 +0800

    Better

--- kokoro.app/core.base/src/desktopJvmMain/kotlin/kokoro/app/AppAssets.kt
+++ kokoro.app/core.base/src/desktopJvmMain/kotlin/kokoro/app/AppAssets.kt
@@ -6,12 +6,10 @@ import okio.source
 import java.io.InputStream
 
 actual object AppAssets {
-	@JvmField val resourceLoader: ClassLoader = AppAssets::class.java.classLoader
+	@JvmField val resourceLoader = AppAssets::class.java.classLoader!!
 }
 
-actual fun AppAssets.open(path: String): Source =
-	(resourceLoader.getResourceAsStream(path) ?: throw E_MissingAsset(path))
-		.source()
+actual fun AppAssets.open(path: String): Source = openStream(path).source()
 
 actual fun AppAssets.openStream(path: String): InputStream =
 	resourceLoader.getResourceAsStream(path) ?: throw E_MissingAsset(path)
