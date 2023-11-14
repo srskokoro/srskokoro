62434ccc2c55b3d93898c23caee4688999afe41b
netaneru <netaneru3@gmail.com>
2023-11-15 07:36:36 +0800

    Simplified

--- kokoro.app/ui.engine/core.api/src/+appMain/kotlin/kokoro/app/ui/engine/web/WebRequest.kt
+++ kokoro.app/ui.engine/core.api/src/+appMain/kotlin/kokoro/app/ui/engine/web/WebRequest.kt
@@ -2,10 +2,6 @@ package kokoro.app.ui.engine.web
 
 interface WebRequest {
 
-	val isUiEngineRequest: Boolean
-
-	val referrer: WebUri
-
 	val method: String
 
 	val url: WebUri
@@ -13,6 +9,4 @@ interface WebRequest {
 	fun headers(): Map<String, String>
 
 	fun header(name: String): String?
-
-	var isSwitchingContext: Boolean
 }
