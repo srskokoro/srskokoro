08cb0743161c32cab9a5cf579716348d02760664
netaneru <netaneru3@gmail.com>
2023-11-24 00:07:18 +0800

    Unnecessary

--- gradle/conventions.build-plugin/src/main/kotlin/conv/attributes/TARGET_DISAMBIGUATOR_ATTRIBUTE.kt
+++ gradle/conventions.build-plugin/src/main/kotlin/conv/attributes/TARGET_DISAMBIGUATOR_ATTRIBUTE.kt
@@ -8,4 +8,4 @@ import org.gradle.api.attributes.Attribute
  * See, “[Distinguish several targets for one platform | Set up targets for Kotlin Multiplatform | Kotlin Documentation](https://kotlinlang.org/docs/multiplatform-set-up-targets.html#distinguish-several-targets-for-one-platform)”
  */
 val TARGET_DISAMBIGUATOR_ATTRIBUTE: Attribute<String> =
-	Attribute.of("convention:conv.kt.mpp.targetDisambiguator", String::class.java)
+	Attribute.of("conv.kt.mpp.targetDisambiguator", String::class.java)
