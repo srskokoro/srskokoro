72d15f6ca20ae13557e72c0d79398bebb5972302
netaneru <netaneru3@gmail.com>
2023-11-28 03:17:09 +0800

    Converted into secondary constructor for better IDE refactoring

--- kokoro.app/ui.engine/core.api/src/iosMain/kotlin/kokoro/app/ui/engine/web/WebResponse.kt
+++ kokoro.app/ui.engine/core.api/src/iosMain/kotlin/kokoro/app/ui/engine/web/WebResponse.kt
@@ -2,16 +2,29 @@ package kokoro.app.ui.engine.web
 
 import okio.Source
 
-actual class WebResponse actual constructor(
-	actual val status: Int,
-	actual val mimeType: String?,
-	actual val charset: String?,
-	actual val headers: Map<String, String>,
-	actual val contentLength: Long,
-	actual val content: Source,
-) {
-	init {
+actual class WebResponse {
+	actual val status: Int
+	actual val mimeType: String?
+	actual val charset: String?
+	actual val headers: Map<String, String>
+	actual val contentLength: Long
+	actual val content: Source
+
+	actual constructor(
+		status: Int,
+		mimeType: String?,
+		charset: String?,
+		headers: Map<String, String>,
+		contentLength: Long,
+		content: Source,
+	) {
 		WebResponse_checkStatus_nonAndroid(status)
+		this.status = status
+		this.mimeType = mimeType
+		this.charset = charset
+		this.headers = headers
+		this.contentLength = contentLength
+		this.content = content
 	}
 
 	actual constructor(
