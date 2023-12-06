22c6a17b09b3b4b2d5b593116f2031d4c4e50ee0
netaneru <netaneru3@gmail.com>
2023-12-07 04:17:56 +0800

    Fix: kludge to force the inclusion of `applicationDefaultJvmArgs`

--- gradle/conventions/conv.kt.jvm.app.gradle.kts
+++ gradle/conventions/conv.kt.jvm.app.gradle.kts
@@ -7,6 +7,20 @@ plugins {
 }
 
 tasks.withType<JavaExec>().configureEach {
+	// KLUDGE to force the inclusion of `application.applicationDefaultJvmArgs`,
+	//  since `Gradle` seems to set it up via `jvmArguments.convention()` at the
+	//  moment.
+	jvmArgs = jvmArgs
+	// NOTE: It seems that `application.applicationDefaultJvmArgs` is set up via
+	// `convention()` now, contrary to what we previously believed, or perhaps,
+	// this was introduced to `Gradle` by mistake when `jvmArguments` was
+	// introduced (as an alternative to `jvmArgs`). Moreover, the docs isn't
+	// even clear about `applicationDefaultJvmArgs` being a "convention" value.
+	// - See, https://github.com/gradle/gradle/pull/23924
+	// - See also,
+	//   - https://github.com/gradle/gradle/issues/15239
+	//   - https://github.com/gradle/gradle/issues/13463#issuecomment-1468710781
+
 	setUp(this)
 }
 
