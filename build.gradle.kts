ad76d9242d9f0474528ab6e9164eb480613bc77f
netaneru <netaneru3@gmail.com>
2023-12-09 02:36:17 +0800

    Easily run all tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,11 @@
 plugins {
 	id("build.root")
 }
+
+tasks {
+	fun Task.dependOnTasksFromIncludedBuilds() {
+		val name = name
+		dependsOn(project.gradle.includedBuilds.map { it.task(":$name") })
+	}
+	check.configure { dependOnTasksFromIncludedBuilds() }
+}
--- gradle/build.ground/src/main/kotlin/build.root.gradle.kts
+++ gradle/build.ground/src/main/kotlin/build.root.gradle.kts
@@ -1,3 +1,33 @@
 plugins {
 	`lifecycle-base`
 }
+
+afterEvaluate {
+	fun Task.dependOnTasksFromSubProjects() {
+		val project = project
+		val name = name
+		dependsOn(project.provider {
+			project.subprojects.mapNotNull { project ->
+				try {
+					project.tasks.named(name)
+				} catch (_: UnknownTaskException) {
+					null
+				}
+			}
+		})
+	}
+
+	fun TaskContainer.maybeRegister(name: String) = try {
+		// NOTE: Rather than check via `getNames()`, it's better to just let the
+		// following throw (and catch the exception), since `getNames()` seems
+		// to be not optimized for repeated access (as it may return a different
+		// instance every time).
+		named(name)
+	} catch (_: UnknownTaskException) {
+		register(name)
+	}
+
+	val tasks = tasks
+	tasks.maybeRegister("check").configure { dependOnTasksFromSubProjects() }
+	tasks.maybeRegister("test").configure { dependOnTasksFromSubProjects() }
+}
