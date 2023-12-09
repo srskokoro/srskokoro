5956785f4b3ecd2f3b2edf1f75e0bd5e49ebc595
netaneru <netaneru3@gmail.com>
2023-12-09 20:04:06 +0800

    Seems unnecessary with "org.gradle.kotlin.kotlin-dsl.base" applied

--- gradle/build.ground/src/main/kotlin/build.support.kt.dsl.gradle.kts
+++ gradle/build.ground/src/main/kotlin/build.support.kt.dsl.gradle.kts
@@ -2,11 +2,3 @@ plugins {
 	id("build.support.kt.jvm")
 	id("org.gradle.kotlin.kotlin-dsl.base")
 }
-
-dependencies {
-	compileOnly(gradleApi())
-	testImplementation(gradleApi())
-
-	compileOnly(gradleKotlinDsl())
-	testImplementation(gradleKotlinDsl())
-}
