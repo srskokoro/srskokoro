cf46eb08010ee15183d4e868193ee60b764c6dc8
netaneru <netaneru3@gmail.com>
2023-12-12 13:08:29 +0800

    Added initial (yet extensive) preset targets
    …since, strictly speaking, "build support" modules are supposedly
    libraries to be consumed by the build or other projects that need
    supporting.

--- gradle/build.base/src/main/kotlin/build.support.kt.mpp.gradle.kts
+++ gradle/build.base/src/main/kotlin/build.support.kt.mpp.gradle.kts
@@ -3,3 +3,21 @@ plugins {
 	id("build.support.kt.base")
 	id("build.kotest.mpp")
 }
+
+kotlin {
+	js(IR) {
+		nodejs()
+	}
+
+	jvm()
+
+	iosArm64()
+	iosSimulatorArm64()
+	iosX64()
+
+	linuxX64()
+	linuxArm64()
+	macosX64()
+	macosArm64()
+	mingwX64()
+}
