ebc5b854661c050982c3ac3d221ee30de1d15d4c
netaneru <netaneru3@gmail.com>
2023-12-12 22:50:39 +0800

    Renamed so that it's not among the subprojects
    …since we're planning to have subprojects whose directories are named
    like `build.*`, `dependencies` and `settings`.

--- gradle/build.logic/build.gradle.kts
+++ gradle/build.logic/build.gradle.kts
@@ -1,3 +1,12 @@
+import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin
+import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension
+
 plugins {
 	id("build.plugin.root")
 }
+
+rootProject.let(fun(rootProject) = rootProject.plugins.withType<YarnPlugin>(fun(_) {
+	with(rootProject.extensions.getByName(YarnRootExtension.YARN) as YarnRootExtension) {
+		lockFileDirectory = File(rootProject.projectDir, "#kotlin-js-store")
+	}
+}))
