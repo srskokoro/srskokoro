d63ef7953eb8874b2a500c4e54f0ebcabd4da516
netaneru <netaneru3@gmail.com>
2023-12-17 22:03:07 +0800

    Added `getOrNull()` counterpart

--- gradle/build.logic/src/main/kotlin/build/api/dsl/ExtensionContainer._.kt
+++ gradle/build.logic/src/main/kotlin/build/api/dsl/ExtensionContainer._.kt
@@ -3,11 +3,15 @@ package build.api.dsl
 import org.gradle.api.plugins.ExtensionContainer
 import org.gradle.kotlin.dsl.*
 
-inline fun <R> ExtensionContainer.getOrElse(name: String, defaultValue: ExtensionContainer.(name: String) -> R): R {
+@Suppress("NOTHING_TO_INLINE")
+inline fun <R> ExtensionContainer.getOrNull(name: String): R? {
 	@Suppress("UNCHECKED_CAST")
 	// NOTE: The cast below throws on non-null incompatible types (as intended).
-	return findByName(name) as R? ?: defaultValue(name)
+	return findByName(name) as R?
 }
+
+inline fun <R> ExtensionContainer.getOrElse(name: String, defaultValue: ExtensionContainer.(name: String) -> R): R =
+	getOrNull(name) ?: defaultValue(name)
 
 inline fun <reified R : Any> ExtensionContainer.getOrAdd(name: String, defaultValue: ExtensionContainer.(name: String) -> R): R = getOrElse(name) {
 	val extension = defaultValue(name)
