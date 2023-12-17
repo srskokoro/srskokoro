a162a37737002d278b6fc72dffd7a92e34e19222
netaneru <netaneru3@gmail.com>
2023-12-17 19:52:30 +0800

    Clarity

--- gradle/build.logic/src/main/kotlin/build/api/dsl/xs.kt
+++ gradle/build.logic/src/main/kotlin/build/api/dsl/xs.kt
@@ -8,12 +8,12 @@ inline fun Any.x() = (this as ExtensionAware).extensions.x()
 
 inline fun <reified T> Any.x(name: String): T = (this as ExtensionAware).extensions.x(name)
 
-inline fun <reified T> Any.x(name: String, noinline configure: T.() -> Unit) = (this as ExtensionAware).extensions.x(name, configure)
+inline fun <reified T> Any.x(name: String, noinline configure: T.() -> Unit): Unit = (this as ExtensionAware).extensions.x(name, configure)
 
 
 @Suppress("NOTHING_TO_INLINE")
 inline fun ExtensionContainer.x() = ExtensionContainerScope(this)
 
-inline fun <reified T> ExtensionContainer.x(name: String) = getByName(name) as T
+inline fun <reified T> ExtensionContainer.x(name: String): T = getByName(name) as T
 
-inline fun <reified T> ExtensionContainer.x(name: String, noinline configure: T.() -> Unit) = configure(name, configure)
+inline fun <reified T> ExtensionContainer.x(name: String, noinline configure: T.() -> Unit): Unit = configure(name, configure)
