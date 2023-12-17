e04a80d7925392da3e235865ff74e79851b8067e
netaneru <netaneru3@gmail.com>
2023-12-18 04:01:08 +0800

    Replaced by much better alternatives

---
+++ gradle/build.logic/src/main/kotlin/build/api/dsl/Gradle._.kt
@@ -0,0 +1,20 @@
+package build.api.dsl
+
+import org.gradle.api.UnknownDomainObjectException
+import org.gradle.api.initialization.IncludedBuild
+import org.gradle.api.invocation.Gradle
+
+@Suppress("NOTHING_TO_INLINE")
+inline fun Gradle.includedBuilds(vararg names: String) = includedBuilds(names.asList())
+
+fun Gradle.includedBuilds(names: Collection<String>) = mutableListOf<IncludedBuild>().also { out ->
+	@Suppress("NAME_SHADOWING") val names = LinkedHashSet(names)
+	for (build in includedBuilds) {
+		if (names.remove(build.name)) {
+			out.add(build)
+		}
+	}
+	if (names.isNotEmpty()) {
+		throw UnknownDomainObjectException("Included build '${names.first}' not found in $this")
+	}
+}
