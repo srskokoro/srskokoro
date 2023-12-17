db578d70424ceb445f75cd80f7f9d59a7d5711cc
netaneru <netaneru3@gmail.com>
2023-12-17 21:59:54 +0800

    Simplified

--- gradle/build.logic/src/main/kotlin/build/api/dsl/ExtraPropertiesExtension._.kt
+++ gradle/build.logic/src/main/kotlin/build/api/dsl/ExtraPropertiesExtension._.kt
@@ -12,11 +12,8 @@ fun <R> ExtraPropertiesExtension.getOrNull(name: String): R? {
 	} else null) as R? // NOTE: This cast throws on non-null incompatible types (as intended).
 }
 
-inline fun <R> ExtraPropertiesExtension.getOrElse(name: String, defaultValue: ExtraPropertiesExtension.(name: String) -> R): R {
-	@Suppress("UNCHECKED_CAST")
-	// NOTE: The cast below throws on non-null incompatible types (as intended).
-	return getOrNull(name) as R? ?: defaultValue(name)
-}
+inline fun <R> ExtraPropertiesExtension.getOrElse(name: String, defaultValue: ExtraPropertiesExtension.(name: String) -> R): R =
+	getOrNull(name) ?: defaultValue(name)
 
 inline fun <R> ExtraPropertiesExtension.getOrAdd(name: String, defaultValue: ExtraPropertiesExtension.(name: String) -> R): R = getOrElse(name) {
 	val value = defaultValue(name)
