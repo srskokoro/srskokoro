790f213c76a1f4b7f40f243e74bd0ef67ec5291d
netaneru <netaneru3@gmail.com>
2023-12-18 21:49:58 +0800

    Efficiently compute hash code of concatenated strings

---
+++ gradle/build.logic/src/test/kotlin/build/support/test_fastPow31.kt
@@ -0,0 +1,31 @@
+package build.support
+
+import kotlin.test.Test
+import kotlin.test.assertContentEquals
+
+class test_fastPow31 {
+
+	companion object {
+		private val TEST_RANGE = 0 until 256
+	}
+
+	private fun naivePow31Calc(n: Int): Int {
+		var x = 1
+		for (i in 0 until n) x *= 31
+		return x
+	}
+
+	@Test fun `fastPow31Calc(N) == naivePow31Calc(N)`() {
+		assertContentEquals(
+			TEST_RANGE.map { naivePow31Calc(it) },
+			TEST_RANGE.map { fastPow31Calc(it) },
+		)
+	}
+
+	@Test fun `fastPow31(N) == fastPow31Calc(N)`() {
+		assertContentEquals(
+			TEST_RANGE.map { fastPow31Calc(it) },
+			TEST_RANGE.map { fastPow31(it) },
+		)
+	}
+}
