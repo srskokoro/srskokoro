dd47420bac6b2fcb8b37fd62b9cec37fcedd4155
netaneru <netaneru3@gmail.com>
2023-12-19 02:11:03 +0800

    Looks better

--- gradle/conventions.plugins/settings.gradle.kts
+++ gradle/conventions.plugins/settings.gradle.kts
@@ -1,9 +1,10 @@
+@file:Suppress("UnstableApiUsage")
+
 pluginManagement {
 	includeBuild("../build.logic")
 	includeBuild("../conventions.base")
 }
 
-@Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
 	includeBuild("../build.logic")
 	includeBuild("../conventions.base")
