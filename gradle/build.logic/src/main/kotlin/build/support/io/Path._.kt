b6a676e6dc213012a5792ebad42c52321fa23c6d
netaneru <netaneru3@gmail.com>
2023-12-20 16:07:16 +0800

    Better error message in case of typos in the path when including builds

---
+++ gradle/build.logic/src/main/kotlin/build/support/io/Path._.kt
@@ -0,0 +1,13 @@
+package build.support.io
+
+import java.nio.file.Files
+import java.nio.file.Path
+
+@Suppress("NOTHING_TO_INLINE")
+inline fun Path.isEmptyDirectory() = !isNonEmptyDirectory()
+
+fun Path.isNonEmptyDirectory(): Boolean {
+	Files.newDirectoryStream(this).use {
+		return it.iterator().hasNext()
+	}
+}
