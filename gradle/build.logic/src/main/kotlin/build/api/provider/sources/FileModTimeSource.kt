56722acd2a29e2d79704e117d69f6bcf9cbc86cf
netaneru <netaneru3@gmail.com>
2023-12-20 23:27:24 +0800

    Modifications on the deps data should invalidate the configuration cache

---
+++ gradle/build.logic/src/main/kotlin/build/api/provider/sources/FileModTimeSource.kt
@@ -0,0 +1,28 @@
+package build.api.provider.sources
+
+import org.gradle.api.file.RegularFileProperty
+import org.gradle.api.provider.Provider
+import org.gradle.api.provider.ProviderFactory
+import org.gradle.api.provider.ValueSource
+import org.gradle.api.provider.ValueSourceParameters
+import java.io.File
+
+abstract class FileModTimeSource : ValueSource<Long, FileModTimeSource.Parameters> {
+
+	interface Parameters : ValueSourceParameters {
+		val file: RegularFileProperty
+	}
+
+	companion object {
+
+		fun get(file: File, providers: ProviderFactory) = of(file, providers).get()
+
+		fun of(file: File, providers: ProviderFactory): Provider<Long> {
+			return providers.of(FileModTimeSource::class.java) {
+				parameters.file.set(file)
+			}
+		}
+	}
+
+	override fun obtain(): Long = parameters.file.get().asFile.lastModified()
+}
