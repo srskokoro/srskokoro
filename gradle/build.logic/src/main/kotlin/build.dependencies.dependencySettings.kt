695e85f47e48b6ffa50c127bbf5feb3b1cac08b6
netaneru <netaneru3@gmail.com>
2023-12-21 21:58:50 +0800

    Fix: correct line numbers aren't reported on error

--- gradle/build.logic/src/main/kotlin/build.dependencies.dependencySettings.kt
+++ gradle/build.logic/src/main/kotlin/build.dependencies.dependencySettings.kt
@@ -12,5 +12,8 @@ internal const val dependencySettings__name = "dependencySettings"
 val Settings.dependencySettings: DependencySettings
 	get() = x(dependencySettings__name)
 
+// NOTE: Correct line numbers are reported only when `Action<T>` is used --
+// i.e., we can't even use `() -> Unit` here (`inline` or not).
+// - This restriction seem to apply only to calls made directly at the top-level
+// of the kotlin script (`*.kts`) file.
 fun Settings.dependencySettings(configure: Action<DependencySettings>) = configure.execute(dependencySettings)
-inline fun Settings.dependencySettings(configure: DependencySettings.() -> Unit) = configure.invoke(dependencySettings)
