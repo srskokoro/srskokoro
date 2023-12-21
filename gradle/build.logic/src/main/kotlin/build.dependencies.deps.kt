957bfb5efeb3a0e7d8f5cb1b3700bc8d7b4a24b2
netaneru <netaneru3@gmail.com>
2023-12-21 22:48:43 +0800

    Convenience

--- gradle/build.logic/src/main/kotlin/build.dependencies.deps.kt
+++ gradle/build.logic/src/main/kotlin/build.dependencies.deps.kt
@@ -1,5 +1,6 @@
 import build.api.dsl.*
 import build.dependencies.Deps
+import org.gradle.api.Action
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.*
 
@@ -8,6 +9,14 @@ private const val deps__key = "--deps--"
 
 val Project.deps: Deps?
 	get() = rootProject.xs().getOrNull(deps__key)
+
+// NOTE: Correct line numbers are reported only when `Action<T>` is used --
+// i.e., we can't even use `() -> Unit` here (`inline` or not).
+// - This restriction seem to apply only to calls made directly at the top-level
+// of the kotlin script (`*.kts`) file.
+fun Project.deps(configure: Action<Deps>) {
+	deps?.let { configure.execute(it) }
+}
 
 internal fun setUpDeps(rootProject: Project, deps: Deps) {
 	rootProject.xs().add(typeOf(), deps__key, deps)
--- gradle/build.logic/src/main/kotlin/build/dependencies/DepsProps.kt
+++ gradle/build.logic/src/main/kotlin/build/dependencies/DepsProps.kt
@@ -1,5 +1,19 @@
 package build.dependencies
 
+import org.gradle.api.UnknownDomainObjectException
+
 class DepsProps(
 	val map: Map<String, String>,
+) {
+	/**
+	 * @see DepsProps.map
+	 * @see Deps.prop
+	 */
+	@Suppress("NOTHING_TO_INLINE")
+	inline operator fun get(key: String) = map[key] ?: throw E_UnknownPropKey(key)
+}
+
+@PublishedApi
+internal fun E_UnknownPropKey(key: String) = UnknownDomainObjectException(
+	"Could not find property with key: $key"
 )
