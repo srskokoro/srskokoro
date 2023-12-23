def29a981adf419696597ce91a2a98f7d6f3ab36
netaneru <netaneru3@gmail.com>
2023-12-24 01:13:26 +0800

    Setup unnecessary now that it lives alone in its own file

--- gradle/build.logic/src/main/kotlin/build/api/testing/gradleUserHome.kt
+++ gradle/build.logic/src/main/kotlin/build/api/testing/gradleUserHome.kt
@@ -2,18 +2,16 @@ package build.api.testing
 
 import java.io.File
 
-val gradleUserHome: File get() = _gradleUserHome.value
-
-private object _gradleUserHome {
-	/** @see org.gradle.testkit.runner.GradleRunner.withGradleVersion */
-	val value = run(fun(): File {
-		var path = System.getProperty("gradle.user.home")
+val gradleUserHome = run(fun(): File {
+	/**
+	 * @see org.gradle.testkit.runner.GradleRunner.withGradleVersion
+	 */
+	var path = System.getProperty("gradle.user.home")
+	if (path == null) {
+		path = System.getenv("GRADLE_USER_HOME")
 		if (path == null) {
-			path = System.getenv("GRADLE_USER_HOME")
-			if (path == null) {
-				return File(System.getProperty("user.home"), ".gradle")
-			}
+			return File(System.getProperty("user.home"), ".gradle")
 		}
-		return File(path)
-	})
-}
+	}
+	return File(path)
+})
