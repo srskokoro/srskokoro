68d2aa4aebfa7d1d603c2db0651ff5700b3f6754
netaneru <netaneru3@gmail.com>
2023-12-25 01:08:25 +0800

    Should provide only the most essential base plugins for the build

--- gradle/build.base/build.gradle.kts
+++ gradle/build.base/build.gradle.kts
@@ -14,16 +14,8 @@ group = "build"
 
 gradlePlugin {
 	plugins {
-		//addPlugin("build.kt.base")
-
 		//addPlugin("build.plugins.base")
 		//addPlugin("build.root.base")
-
-		//addPlugin("build.support.kt.base")
-		//addPlugin("build.support.kt.jvm")
-		//addPlugin("build.support.kt.mpp")
-
-		//addPlugin("build.testing.kt.mpp")
 	}
 }
 
