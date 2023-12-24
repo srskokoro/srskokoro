0135435ef4e5e9f6b747bfb145c05167ac25585f
netaneru <netaneru3@gmail.com>
2023-12-25 01:56:50 +0800

    Reasoning

--- gradle/build.base/src/main/kotlin/build/api/dsl/Task._.kt
+++ gradle/build.base/src/main/kotlin/build/api/dsl/Task._.kt
@@ -11,6 +11,10 @@ fun Task.dependOnSameTaskFromSubProjects() {
 	dependsOn(project.provider {
 		project.subprojects.mapNotNull { project ->
 			try {
+				// NOTE: Rather than check via `getNames()`, it's better to just
+				// let the following throw (and catch the exception), since
+				// `getNames()` seems to be not optimized for repeated access
+				// (as it may return a different instance every time).
 				project.tasks.named(taskName)
 			} catch (_: UnknownTaskException) {
 				null
