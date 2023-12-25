73a6e6b3384db26e92f4c1296c7aad4de113af72
netaneru <netaneru3@gmail.com>
2023-12-26 05:47:32 +0800

    Easily hook any task to run automatically on IDE "gradle sync"

---
+++ gradle/build.base/src/main/kotlin/build/api/dsl/ideSyncTask.kt
@@ -0,0 +1,21 @@
+package build.api.dsl
+
+import org.gradle.api.Project
+import org.gradle.api.Task
+import org.gradle.api.tasks.TaskContainer
+
+@Suppress("NOTHING_TO_INLINE")
+inline fun Task.runOnIdeSync() = project.runOnIdeSync(this)
+
+@Suppress("NOTHING_TO_INLINE")
+inline fun Project.runOnIdeSync(task: Task) = tasks.runOnIdeSync(task)
+
+@Suppress("NOTHING_TO_INLINE")
+inline fun TaskContainer.runOnIdeSync(task: Task) {
+	ideSyncTask.dependsOn(task)
+}
+
+val TaskContainer.ideSyncTask: Task
+	// NOTE: Automatically runs on "gradle sync" (via IntelliJ IDEA or Android
+	// Studio) -- https://twitter.com/Sellmair/status/1619308362881187840
+	get() = maybeCreate("prepareKotlinIdeaImport")
