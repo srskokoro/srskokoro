9a30bbd45dc65d3d216aaf04b28eaef00e96db51
netaneru <netaneru3@gmail.com>
2023-12-26 10:36:34 +0800

    Removed as no longer needed

--- gradle/build.logic (old)/src/main/kotlin/build/api/ProjectPlugin.kt
+++
@@ -1,8 +0,0 @@
-package build.api
-
-import org.gradle.api.Project
-
-/**
- * @see org.gradle.api.Plugin
- */
-interface ProjectPlugin : Plugin<Project>
--- gradle/build.logic (old)/src/main/kotlin/build/api/SettingsPlugin.kt
+++
@@ -1,8 +0,0 @@
-package build.api
-
-import org.gradle.api.initialization.Settings
-
-/**
- * @see org.gradle.api.Plugin
- */
-interface SettingsPlugin : Plugin<Settings>
