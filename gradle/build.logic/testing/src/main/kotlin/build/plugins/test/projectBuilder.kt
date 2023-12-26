7b4654dc74c62b6c9ff7858579a7ef5161318303
netaneru <netaneru3@gmail.com>
2023-12-26 09:19:12 +0800

    Better

--- gradle/build.logic/testing/src/main/kotlin/build/plugins/test/projectBuilder.kt
+++ gradle/build.logic/testing/src/main/kotlin/build/plugins/test/projectBuilder.kt
@@ -1,14 +1,15 @@
 package build.plugins.test
 
+import org.gradle.api.Project
 import org.gradle.testfixtures.ProjectBuilder
 import java.io.File
 
-fun projectBuilder() = ProjectBuilder.builder().withGradleUserHomeDir(gradleUserHome)
+fun projectBuilder(): ProjectBuilder = ProjectBuilder.builder().withGradleUserHomeDir(gradleUserHome)
 
-fun projectBuilder(projectDir: File?) = projectBuilder().withProjectDir(projectDir)
+fun projectBuilder(projectDir: File?): ProjectBuilder = projectBuilder().withProjectDir(projectDir)
 
-inline fun buildProject(configure: ProjectBuilder.() -> Unit = {}) =
+inline fun buildProject(configure: ProjectBuilder.() -> Unit = {}): Project =
 	projectBuilder().apply(configure).build()
 
-inline fun buildProject(projectDir: File?, configure: ProjectBuilder.() -> Unit = {}) =
+inline fun buildProject(projectDir: File?, configure: ProjectBuilder.() -> Unit = {}): Project =
 	projectBuilder(projectDir).apply(configure).build()
