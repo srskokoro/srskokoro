b5fcb0d5fffec96c340ec763bf8a5b352a78e40a
netaneru <netaneru3@gmail.com>
2023-12-28 01:02:07 +0800

    Declared some repositories in the root `settings.gradle.kts` file

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,14 @@
 @Suppress("ComplexRedundantLet")
 pluginManagement {
 	apply(from = "settings.init.gradle.kts")
+
 	val autoGradleProperties: (String) -> String by extra
 	autoGradleProperties("gradle/build.base")
 	autoGradleProperties("gradle/build.logic").let { includeBuild(it) }
 	autoGradleProperties("gradle/conventions").let { includeBuild(it) }
 	autoGradleProperties("gradle/plugins").let { includeBuild(it) }
+
+	repositories.gradlePluginPortal()
 }
 plugins {
 	id("build.dependencies")
@@ -17,6 +20,11 @@ dependencySettings {
 }
 dependencyResolutionManagement {
 	includeBuild(autoGradleProperties("gradle/support"))
+
+	@Suppress("UnstableApiUsage")
+	repositories {
+		mavenCentral()
+	}
 }
 
 rootProject.name = "srskokoro"
