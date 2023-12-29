e3c24e25ddc06c1f37efc13bf4b3aa40fc6e8b17
netaneru <netaneru3@gmail.com>
2023-12-29 23:04:58 +0800

    Moved to "build.base" module instead

--- gradle/build.logic/src/main/kotlin/build/support/io/File._.kt
+++ gradle/build.logic/src/main/kotlin/build/support/io/File._.kt
@@ -3,23 +3,6 @@ package build.support.io
 import java.io.File
 import java.nio.file.Files
 
-/**
- * Just like Kotlin's [File.resolve], except that we trust this more :P
- */
-fun File.safeResolve(relative: String): File {
-	val r = File(relative)
-	return if (r.isAbsolute) r
-	else File(this, relative)
-}
-
-/** @see safeResolve */
-fun File.safeResolve(relative: File): File {
-	@Suppress("UnnecessaryVariable")
-	val r = relative
-	return if (r.isAbsolute) r
-	else File(this, r.path)
-}
-
 @Suppress("NOTHING_TO_INLINE")
 inline fun File.isEmptyDirectory() = toPath().isEmptyDirectory()
 
