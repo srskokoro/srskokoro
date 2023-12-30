0f49b660e06ccb91dfa30a6004b60c4928fa9d40
netaneru <netaneru3@gmail.com>
2023-12-31 04:58:59 +0800

    Set up "build.base"

---
+++ gradle/build.base/build.gradle.kts
@@ -0,0 +1,51 @@
+import org.gradle.kotlin.dsl.support.expectedKotlinDslPluginsVersion
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion as KotlinCompileVersion
+
+plugins {
+	`java-gradle-plugin`
+	`kotlin-dsl-base`
+}
+
+gradlePlugin {
+	plugins {
+	}
+}
+
+//#region Complex build setup
+
+private object Build {
+	const val PLUGIN_CLASS = "_plugin"
+
+	const val JAVA_RELEASE_TARGET = 8
+	inline val KOTLIN_JVM_TARGET get() = JvmTarget.JVM_1_8
+}
+
+tasks {
+	compileJava.configure {
+		options.release = Build.JAVA_RELEASE_TARGET
+	}
+	compileKotlin.configure {
+		compilerOptions {
+			jvmTarget = Build.KOTLIN_JVM_TARGET
+			apiVersion.set(KotlinCompileVersion.DEFAULT)
+			languageVersion.set(KotlinCompileVersion.DEFAULT)
+		}
+	}
+}
+
+fun NamedDomainObjectContainer<PluginDeclaration>.addPlugin(name: String) {
+	create(name) {
+		id = name
+		implementationClass = "$name.${Build.PLUGIN_CLASS}"
+	}
+}
+
+//#endregion
+
+dependencies {
+	compileOnly(embeddedKotlin("gradle-plugin"))
+	compileOnly("org.gradle.kotlin", "gradle-kotlin-dsl-plugins", expectedKotlinDslPluginsVersion)
+	compileOnly(embeddedKotlin("sam-with-receiver"))
+	compileOnly(embeddedKotlin("assignment"))
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,8 @@ pluginManagement {
 	apply(from = "settings.init.gradle.kts")
 	val autoGradleProperties: (String) -> String by extra
 
+	includeBuild(autoGradleProperties("gradle/build.base")) // TODO Should not include this build directly
+
 	repositories.gradlePluginPortal()
 }
 
