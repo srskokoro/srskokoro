c38fd06b37b3e8a24b3fd3969619b0ff84130d0d
netaneru <netaneru3@gmail.com>
2023-12-31 06:12:38 +0800

    Allowed referring by module ID

--- gradle/build.support/settings.gradle.kts
+++ gradle/build.support/settings.gradle.kts
@@ -6,6 +6,7 @@ pluginManagement {
 }
 
 dependencyResolutionManagement {
+	includeBuild(".") // -- https://docs.gradle.org/8.5/userguide/composite_builds.html#included_build_declaring_substitutions
 	repositories.gradlePluginPortal()
 }
 
