5de656eb545306755dac268fbcfbc3d75c530233
netaneru <netaneru3@gmail.com>
2024-01-01 01:11:22 +0800

    Reorganized

--- dependencies/settings.gradle.kts
+++ dependencies/settings.gradle.kts
@@ -15,12 +15,15 @@ dependencySettings {
 	}
 
 	val kotlin = "1.9.21" // https://kotlinlang.org/docs/releases.html
+
 	pluginKotlin("android", kotlin)
 	pluginKotlin("jvm", kotlin)
 	pluginKotlin("multiplatform", kotlin)
-	moduleKotlin("gradle-plugin", kotlin)
+
 	// See also, https://youtrack.jetbrains.com/issue/KT-54691#focus=Comments-27-6852272.0-0
 	moduleKotlin("gradle-plugins-bom", kotlin)
+	moduleKotlin("gradle-plugin", kotlin)
+
 	// See, https://kotlinlang.org/docs/gradle-configure-project.html#versions-alignment-of-transitive-dependencies
 	moduleKotlin("bom", kotlin)
 	moduleKotlin("stdlib", kotlin)
