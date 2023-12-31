55fb335185fb4adf8216e34d1fb4637c6cb7e48a
netaneru <netaneru3@gmail.com>
2023-12-31 10:38:13 +0800

    Made it convenient to override

--- gradle/build.support/support/src/main/kotlin/build/api/UtilityPlugin.kt
+++ gradle/build.support/support/src/main/kotlin/build/api/UtilityPlugin.kt
@@ -2,10 +2,13 @@ package build.api
 
 import org.gradle.api.Plugin
 
-abstract class UtilityPlugin : Plugin<Any> {
-
+abstract class UtilityPlugin(
+	private val apply: Any.() -> Unit = {
+		// The primary purpose of this plugin is to simply bring in some useful
+		// utilities to the buildscript classpath. Thus, by default, do nothing.
+	},
+) : Plugin<Any> {
 	override fun apply(target: Any) {
-		// Do nothing. The purpose of this plugin is to simply bring in some
-		// useful utilities to the buildscript classpath.
+		apply.invoke(target)
 	}
 }
