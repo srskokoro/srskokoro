9bdc159620737c1e9377574c587890a70ced0160
netaneru <netaneru3@gmail.com>
2023-12-31 10:30:27 +0800

    Allowed customization

--- gradle/build.support/support/src/main/kotlin/build/api/UtilityOnly.kt
+++ gradle/build.support/support/src/main/kotlin/build/api/UtilityOnly.kt
@@ -4,7 +4,7 @@ import org.gradle.api.Plugin
 
 abstract class UtilityOnly : Plugin<Any> {
 
-	final override fun apply(target: Any) {
+	override fun apply(target: Any) {
 		// Do nothing. The purpose of this plugin is to simply bring in some
 		// useful utilities to the buildscript classpath.
 	}
