08870926610d0c7280a1b6ef2c879fa3f5acc243
netaneru <netaneru3@gmail.com>
2023-12-31 23:43:03 +0800

    Added `base` convention plugin

--- gradle/conventions/basic/src/main/plugins/build/base/_plugin.kt
+++ gradle/conventions/basic/src/main/plugins/build/base/_plugin.kt
@@ -4,32 +4,34 @@ import build.api.ProjectPlugin
 import build.api.dsl.*
 import build.api.dsl.accessors.base
 import build.api.dsl.accessors.baseOrNull
+import build.conventions.internal.InternalConventions
+import build.conventions.internal.InternalConventionsApi
+import build.conventions.internal.ensureReproducibleBuild
 import build.support.from
 import org.gradle.api.Project
-import org.gradle.kotlin.dsl.*
 
 class _plugin : ProjectPlugin({
 	apply {
-		if (isRoot) plugin<build.root._plugin>()
 		plugin("base")
-		plugin<build.base.internal._plugin>()
 	}
+
+	@OptIn(InternalConventionsApi::class)
+	InternalConventions.ensureReproducibleBuild(this)
 
 	prioritizedAfterEvaluate(fun Project.() {
 		// Don't proceed if we're either a direct child of the root project or
 		// the root project itself.
 		if (depth <= 1) return
-		val parent = parent ?: return
 
 		// -=-
 		// Prevents conflicts when generating the archives, e.g., for tasks that
 		// output an installation distribution where all the archives are placed
 		// under the same directory. See also, https://github.com/gradle/gradle/issues/847#issuecomment-1205001575
 		val archivesNameProvider = provider {
-			val parentBase = parent.baseOrNull
-			if (parentBase != null) {
-				parentBase.archivesName.map { "$it[.]$name" }
-			} else provider {
+			parent?.baseOrNull?.let { parentBase ->
+				return@provider parentBase.archivesName.map { "$it[.]$name" }
+			}
+			provider {
 				val path = path
 				(if (path.startsWith(':')) path.from(1) else path)
 					.replace(":", "[.]")
@@ -37,14 +39,14 @@ class _plugin : ProjectPlugin({
 		}.flatMap { it }
 		// ^ NOTE: Prior to the "[.]" string being used above, single characters
 		// were also considered, such as the '!' and '$' character. The '!'
-		// character seemed problematic, as it seems to confuse the IDE (i.e.,
-		// when a path with a '!' character is displayed in the build output's
-		// console, auto-linking may not work properly), since the '!' character
-		// is often used for denoting the inside of an archive.
+		// character is probably problematic, since the '!' character is often
+		// used for denoting the inside of an archive. Still, `JarURLConnection`
+		// uses the "!/" sequence instead for referring to an entry in a JAR --
+		// see, https://docs.oracle.com/en/java/javase/21/docs/api/java.base/java/net/JarURLConnection.html
 		//
 		// There are other problematic characters as well, simply because the
-		// IDE (or perhaps the kotlin compiler?) fails to escape them whenever
-		// they're displayed via file URIs:
+		// Kotlin Gradle Plugin (or perhaps the Kotlin compiler?) fails to
+		// escape them whenever they're printed to the console via file URIs:
 		// - '#' -- interpreted as a URI hash fragment.
 		// - '%' -- special escape character used in URIs.
 		// - '+' -- interpreted as a space character in URIs.
