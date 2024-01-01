34b0da5531153b748cea36cc183d7b4b0ab42cf0
netaneru <netaneru3@gmail.com>
2024-01-02 00:15:58 +0800

    Removed as it's not performant anyway

--- gradle/build.support/support/src/main/kotlin/build/api/dsl/xs.kt
+++ gradle/build.support/support/src/main/kotlin/build/api/dsl/xs.kt
@@ -2,7 +2,6 @@ package build.api.dsl
 
 import org.gradle.api.plugins.ExtensionAware
 import org.gradle.api.plugins.ExtensionContainer
-import org.gradle.kotlin.dsl.*
 
 @Suppress("NOTHING_TO_INLINE")
 inline fun Any.xs() = (this as ExtensionAware).xs()
@@ -23,8 +22,6 @@ inline fun <reified T> Any.x(name: String): T = xs().x(name)
 
 inline fun <reified T> Any.x(name: String, noinline configure: T.() -> Unit): Unit = xs().x(name, configure)
 
-inline fun <reified T> Any.x(noinline configure: T.() -> Unit): Unit = xs().x(configure)
-
 
 @Suppress("NOTHING_TO_INLINE")
 inline fun ExtensionContainer.x() = ExtensionsDelegate<Any>(this)
@@ -33,13 +30,9 @@ inline fun <reified T> ExtensionContainer.x(name: String): T = getOrThrow(name)
 
 inline fun <reified T> ExtensionContainer.x(name: String, noinline configure: T.() -> Unit): Unit = configure(name, configure)
 
-inline fun <reified T> ExtensionContainer.x(noinline configure: T.() -> Unit): Unit = configure(configure)
-
 
 inline fun <reified O> ExtensionsDelegate<O>.x() = this
 
 inline fun <reified T> ExtensionsDelegate<*>.x(name: String): T = xs().x(name)
 
 inline fun <reified T> ExtensionsDelegate<*>.x(name: String, noinline configure: T.() -> Unit): Unit = xs().x(name, configure)
-
-inline fun <reified T> ExtensionsDelegate<*>.x(noinline configure: T.() -> Unit): Unit = xs().x(configure)
