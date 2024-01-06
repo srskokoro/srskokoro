36792690b03e7ce948ee82360e853ec8fed7261f
netaneru <netaneru3@gmail.com>
2024-01-07 04:50:07 +0800

    Better

--- gradle/conventions/testing/build.gradle.kts
+++ gradle/conventions/testing/build.gradle.kts
@@ -1,4 +1,6 @@
+import build.api.dsl.*
 import org.gradle.api.internal.tasks.testing.junitplatform.JUnitPlatformTestFramework
+import org.jetbrains.kotlin.gradle.targets.jvm.tasks.KotlinJvmTest
 
 plugins {
 	id("build.foundation.kt.mpp.inclusive")
@@ -16,16 +18,21 @@ private object Build {
 	fun excludeKotestAssertions(dependency: ExternalModuleDependency) {
 		dependency.exclude("io.kotest", "kotest-assertions-shared")
 	}
+
+	fun assert_JUnit5(jvmTest: TaskProvider<KotlinJvmTest>): Unit = jvmTest.configure {
+		val tf = testFramework
+		// Ensure that we're really using `JUnit5` (failing otherwise).
+		check(tf is JUnitPlatformTestFramework) {
+			"Expected `JUnitPlatformTestFramework` as test framework but was `${tf::class.qualifiedName}`"
+		}
+	}
 }
 
 dependencies {
 	commonMainRuntimeOnly("io.kotest:kotest-assertions-shared")
 	commonMainApi("io.kotest:kotest-framework-engine", Build::excludeKotestAssertions)
-	jvmMainApi("io.kotest:kotest-runner-junit5") {
-		Build.excludeKotestAssertions(this)
-		// Ensure that we're really using `JUnit5` (failing otherwise).
-		tasks.jvmTest { check(testFramework is JUnitPlatformTestFramework) }
-	}
+	jvmMainApi("io.kotest:kotest-runner-junit5", Build::excludeKotestAssertions)
+	projectThis.tasks.run { Build.assert_JUnit5(jvmTest) }
 	commonMainApi("io.kotest:kotest-property", Build::excludeKotestAssertions)
 	commonMainApi("com.willowtreeapps.assertk:assertk")
 }
