6d127da0fe8a8ee9a247d6ea9b40ba46f679bfef
netaneru <netaneru3@gmail.com>
2024-01-07 04:17:27 +0800

    Prevented Kotest Assertions (e.g., `shouldBe`) from being referenced
    - Should really only use `AssertK`.

--- dependencies/settings.gradle.kts
+++ dependencies/settings.gradle.kts
@@ -44,6 +44,7 @@ dependencySettings {
 
 	val kotest = "5.8.0" // https://github.com/kotest/kotest/releases
 	plugin("io.kotest.multiplatform", kotest)
+	module("io.kotest:kotest-assertions-shared", kotest)
 	module("io.kotest:kotest-framework-engine", kotest)
 	module("io.kotest:kotest-framework-multiplatform-plugin-gradle", kotest)
 	module("io.kotest:kotest-runner-junit5", kotest)
--- gradle/conventions/testing/build.gradle.kts
+++ gradle/conventions/testing/build.gradle.kts
@@ -4,9 +4,25 @@ plugins {
 	id("build.foundation.kt.mpp.inclusive")
 }
 
+private object Build {
+	/**
+	 * Use this utility to exclude `kotest-assertions-shared` from a dependency.
+	 *
+	 * NOTE: At the moment, both Kotest framework and its property testing
+	 * library brings in `kotest-assertions-shared` (which brings in `shouldBe`,
+	 * `shouldNotBe`, etc.) when we just want the test framework and the
+	 * property testing library (as we want a different assertion library).
+	 */
+	fun excludeKotestAssertions(dependency: ExternalModuleDependency) {
+		dependency.exclude("io.kotest", "kotest-assertions-shared")
+	}
+}
+
 dependencies {
-	commonMainApi("io.kotest:kotest-framework-engine")
+	commonMainRuntimeOnly("io.kotest:kotest-assertions-shared")
+	commonMainApi("io.kotest:kotest-framework-engine", Build::excludeKotestAssertions)
 	jvmMainApi("io.kotest:kotest-runner-junit5") {
+		Build.excludeKotestAssertions(this)
 		// Ensure that we're really using `JUnit5` (failing otherwise).
 		tasks.jvmTest { check(testFramework is JUnitPlatformTestFramework) }
 	}
