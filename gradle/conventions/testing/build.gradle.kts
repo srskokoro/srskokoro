d05e31b56f4a6e54d7acc3149adcb6dde6239d79
netaneru <netaneru3@gmail.com>
2024-01-07 04:14:10 +0800

    Organized better

--- gradle/conventions/testing/build.gradle.kts
+++ gradle/conventions/testing/build.gradle.kts
@@ -6,9 +6,10 @@ plugins {
 
 dependencies {
 	commonMainApi("io.kotest:kotest-framework-engine")
-	jvmMainApi("io.kotest:kotest-runner-junit5")
-	// Ensure that we're really using `JUnit5` (failing otherwise).
-	tasks.jvmTest { check(testFramework is JUnitPlatformTestFramework) }
+	jvmMainApi("io.kotest:kotest-runner-junit5") {
+		// Ensure that we're really using `JUnit5` (failing otherwise).
+		tasks.jvmTest { check(testFramework is JUnitPlatformTestFramework) }
+	}
 
 	commonMainApi("com.willowtreeapps.assertk:assertk")
 }
