b344bcd413fd0f0ad19b1e4ace69b1bf24a7f96b
netaneru <netaneru3@gmail.com>
2024-01-07 11:24:30 +0800

    Better

--- gradle/build.foundation/core/src/main/kotlin/build/foundation/PropertiesSource.kt
+++ gradle/build.foundation/core/src/main/kotlin/build/foundation/PropertiesSource.kt
@@ -1,16 +1,19 @@
 package build.foundation
 
+import org.gradle.api.Describable
 import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.provider.ValueSource
 import org.gradle.api.provider.ValueSourceParameters
 import java.util.Properties
 
 // See also, https://github.com/JetBrains/kotlin/blob/v1.9.22/libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/internal/CustomPropertiesFileValueSource.kt
-internal abstract class PropertiesSource : ValueSource<Properties, PropertiesSource.Parameters> {
+internal abstract class PropertiesSource : ValueSource<Properties, PropertiesSource.Parameters>, Describable {
 
 	interface Parameters : ValueSourceParameters {
 		val from: RegularFileProperty
 	}
+
+	override fun getDisplayName(): String = "properties file '${parameters.from.get().asFile}'"
 
 	override fun obtain() = Properties().also { out ->
 		val from = parameters.from.get().asFile
