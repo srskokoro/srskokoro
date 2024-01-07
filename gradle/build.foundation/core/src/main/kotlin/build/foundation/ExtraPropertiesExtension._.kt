ea771041d167a7379933ae345d3bae7262038210
netaneru <netaneru3@gmail.com>
2024-01-08 05:02:07 +0800

    Better implementation

--- gradle/build.foundation/core/src/main/kotlin/build/foundation/ExtraPropertiesExtension._.kt
+++ gradle/build.foundation/core/src/main/kotlin/build/foundation/ExtraPropertiesExtension._.kt
@@ -4,9 +4,7 @@ import org.gradle.api.plugins.ExtraPropertiesExtension
 import org.gradle.internal.extensibility.DefaultExtraPropertiesExtension
 
 internal fun ExtraPropertiesExtension.parseBoolean(name: String, default: Boolean): Boolean {
-	// NOTE: Similar to `java.lang.Boolean.parseBoolean(String)` in converting
-	// the string value into a boolean.
-	return "true".equals((getOrNull<Any>(name) ?: return default).toString(), ignoreCase = true)
+	return java.lang.Boolean.parseBoolean((getOrNull<Any>(name) ?: return default).toString())
 }
 
 internal fun <R> ExtraPropertiesExtension.getOrNull(name: String): R? {
