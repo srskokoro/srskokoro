6213455e72cbc2f1df7f5c981d4be3faad5d93e2
netaneru <netaneru3@gmail.com>
2024-01-09 23:57:53 +0800

    Should be consistent with `afterEvaluate()`

--- gradle/build.foundation/support/src/main/kotlin/build/api/dsl/Project.prioritizedAfterEvaluate.kt
+++ gradle/build.foundation/support/src/main/kotlin/build/api/dsl/Project.prioritizedAfterEvaluate.kt
@@ -2,9 +2,16 @@ package build.api.dsl
 
 import build.api.ensurePrioritizedEvaluationListeners
 import org.gradle.api.Action
+import org.gradle.api.InvalidUserCodeException
 import org.gradle.api.Project
+import org.gradle.api.internal.project.ProjectStateInternal
 
 fun Project.prioritizedAfterEvaluate(action: Action<in Project>) {
+	// Just like `afterEvaluate()`, fail when project is already evaluated.
+	val state = state
+	if (state is ProjectStateInternal && state.hasCompleted()) throw InvalidUserCodeException(
+		"Cannot run `Project.prioritizedAfterEvaluate(Action)` when the project is already evaluated."
+	)
 	// NOTE: Prioritizes the added action against all previously added actions.
 	ensurePrioritizedEvaluationListeners().addFirst(action) // NOT `addLast()`
 }
