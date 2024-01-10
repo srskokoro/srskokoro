76145a6deaa51752473bf527f7e4ac87f2f3042c
netaneru <netaneru3@gmail.com>
2024-01-10 20:32:33 +0800

    Oops: should not be from import

--- gradle/conventions/core/src/main/plugins/build/root/inclusive/_plugin.kt
+++ gradle/conventions/core/src/main/plugins/build/root/inclusive/_plugin.kt
@@ -1,7 +1,6 @@
 package build.root.inclusive
 
 import build.api.ProjectPlugin
-import build.base.inclusive._plugin
 import org.gradle.kotlin.dsl.*
 
 class _plugin : ProjectPlugin({
@@ -11,7 +10,7 @@ class _plugin : ProjectPlugin({
 
 	allprojects(fun(project): Unit = project.afterEvaluate(fun(project) {
 		val plugins = project.plugins
-		if (plugins.hasPlugin(_plugin::class.java)) return
+		if (plugins.hasPlugin(build.base.inclusive._plugin::class.java)) return
 		if (plugins.hasPlugin(build.base._plugin::class.java)) error(
 			"""
 			Each project of this build should have "build.base.inclusive" plugin applied or
