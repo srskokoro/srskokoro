3659464948c33e55457e869af1193b0b944c4645
netaneru <netaneru3@gmail.com>
2024-01-14 00:14:31 +0800

    Depend on the plugin marker instead
    …in case the coordinates change, so as to avoid potential issues like,
    https://github.com/gmazzo/gradle-buildconfig-plugin/pull/88#issuecomment-1819603462

--- dependencies/settings.gradle.kts
+++ dependencies/settings.gradle.kts
@@ -51,9 +51,9 @@ dependencySettings {
 
 	val kotest = "5.8.0" // https://github.com/kotest/kotest/releases
 	plugin("io.kotest.multiplatform", kotest)
+	module("io.kotest.multiplatform:io.kotest.multiplatform.gradle.plugin", kotest)
 	module("io.kotest:kotest-assertions-shared", kotest)
 	module("io.kotest:kotest-framework-engine", kotest)
-	module("io.kotest:kotest-framework-multiplatform-plugin-gradle", kotest)
 	module("io.kotest:kotest-runner-junit5", kotest)
 	module("io.kotest:kotest-property", kotest)
 
--- gradle/conventions/core/build.gradle.kts
+++ gradle/conventions/core/build.gradle.kts
@@ -9,7 +9,7 @@ dependencies {
 	implementation(project(":support.kotlin-gradle-plugin"))
 	implementation(kotlin("sam-with-receiver"))
 	implementation(kotlin("assignment"))
-	implementation("io.kotest:kotest-framework-multiplatform-plugin-gradle")
+	implementation("io.kotest.multiplatform:io.kotest.multiplatform.gradle.plugin")
 
 	testImplementation(project(":testing.conventions"))
 }
