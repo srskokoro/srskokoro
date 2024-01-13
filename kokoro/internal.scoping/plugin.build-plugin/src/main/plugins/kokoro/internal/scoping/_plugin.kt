665703bf451955e988a2b87c206b2896cd33e686
netaneru <netaneru3@gmail.com>
2024-01-13 22:38:50 +0800

    Added necessary opt-in for consumers of the plugin

--- kokoro/internal.scoping/plugin.build-plugin/src/main/plugins/kokoro/internal/scoping/_plugin.kt
+++ kokoro/internal.scoping/plugin.build-plugin/src/main/plugins/kokoro/internal/scoping/_plugin.kt
@@ -1,12 +1,20 @@
 package kokoro.internal.scoping
 
+import org.gradle.api.Project
 import org.gradle.api.provider.Provider
+import org.gradle.kotlin.dsl.*
 import org.jetbrains.kotlin.gradle.plugin.KotlinCompilation
 import org.jetbrains.kotlin.gradle.plugin.KotlinCompilerPluginSupportPlugin
 import org.jetbrains.kotlin.gradle.plugin.SubpluginArtifact
 import org.jetbrains.kotlin.gradle.plugin.SubpluginOption
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
+
+private const val REQUIRED_OPT_IN_CLASS = "kokoro.internal.RequiresCompilerPlugin"
 
 class _plugin : KotlinCompilerPluginSupportPlugin {
+	override fun apply(target: Project) {
+		target.apply_()
+	}
 
 	override fun isApplicable(kotlinCompilation: KotlinCompilation<*>): Boolean = true
 
@@ -22,4 +30,12 @@ class _plugin : KotlinCompilerPluginSupportPlugin {
 		COMPILER_ARTIFACT_GROUP,
 		COMPILER_ARTIFACT_NAME,
 	)
+}
+
+private fun Project.apply_() {
+	tasks.withType<KotlinCompilationTask<*>>().configureEach {
+		compilerOptions.freeCompilerArgs.run {
+			add("-opt-in=$REQUIRED_OPT_IN_CLASS")
+		}
+	}
 }
