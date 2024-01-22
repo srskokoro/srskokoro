1a0ddf11e3dc5acc7b2266897db65500d3aec199
netaneru <netaneru3@gmail.com>
2024-01-23 03:27:35 +0800

    Used K2 compiler for non-"test" source sets

--- gradle/conventions/core/src/main/kotlin/build/setUp(Project).kt
+++ gradle/conventions/core/src/main/kotlin/build/setUp(Project).kt
@@ -1,6 +1,7 @@
 package build
 
 import build.api.dsl.accessors.kotlin
+import build.api.dsl.accessors.kotlinSourceSets
 import build.foundation.BuildFoundation
 import build.foundation.InternalApi
 import build.foundation.setUpTestTasks
@@ -12,4 +13,15 @@ internal fun setUp(project: Project): Unit = with(project) {
 
 	@OptIn(InternalApi::class)
 	BuildFoundation.setUpTestTasks(this)
+
+	kotlinSourceSets.configureEach {
+		name.let {
+			// TODO! Enable K2 for tests (or upgrade KGP to 2.X) once Kotest finally supports it
+			if (it.endsWith("Test") || it == "test") {
+				return@configureEach // Early exit. Skip for test source sets.
+			}
+		}
+		// Enable the K2 compiler -- https://kotlinlang.org/docs/whatsnew1920.html#enable-k2-in-gradle
+		languageSettings.languageVersion = "2.0"
+	}
 }
