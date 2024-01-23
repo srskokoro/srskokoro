9b946c230f9f66c0a6951ed63a55f6f54806b7ce
netaneru <netaneru3@gmail.com>
2024-01-23 12:54:06 +0800

    Unnecessary convention?

--- kokoro/internal/src/commonMain/kotlin/kokoro/internal/NOOK.kt
+++ kokoro/internal/src/commonMain/kotlin/kokoro/internal/NOOK.kt
@@ -13,17 +13,5 @@ package kokoro.internal
  * visibility: declarations annotated with an `@nook` annotation should only
  * really be accessed within the same package as the `@nook` annotation or
  * subpackages of the said package.
- *
- * Also, as a convention, when opting in with an `@nook` annotation and the
- * target is either the entire file or the top-most declaration (such as a
- * class), specify the fully qualified name of the `@nook` annotation. Example:
- *
- * ```
- * @file:OptIn(com.example.nook::class)
- *
- * package com.example.foo.bar
- *
- * // ...
- * ```
  */
 const val NOOK = "Not meant to be accessed other than within the same package as the `@nook` annotation or subpackages of that package."
