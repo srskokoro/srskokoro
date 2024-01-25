26912cb204c986ffd2e3160b8b275b8c14bcbe33
netaneru <netaneru3@gmail.com>
2024-01-25 17:19:05 +0800

    Utilities for reliably accessing the parent project
    - Code resurrected from 1feb9eee

--- gradle/build.foundation/support/src/main/kotlin/build/api/dsl/Project._.kt
+++ gradle/build.foundation/support/src/main/kotlin/build/api/dsl/Project._.kt
@@ -3,3 +3,23 @@ package build.api.dsl
 import org.gradle.api.Project
 
 inline val Project.isRoot get() = parent == null
+
+/**
+ * @see unsafeParent
+ */
+val Project.evaluatedParent: Project
+	get() {
+		val parent = parent!!
+		if (!parent.state.executed) {
+			// NOTE: Blocks until the parent project is evaluated.
+			evaluationDependsOn(parent.path)
+		}
+		return parent
+	}
+
+/**
+ * Returns the parent project, but unlike [evaluatedParent], it is NOT
+ * guaranteed that the parent project is already evaluated.
+ */
+val Project.unsafeParent: Project
+	get() = parent!!
