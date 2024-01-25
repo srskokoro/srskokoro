024eedb7ad5efd82aed5e72e260cbdb260244989
netaneru <netaneru3@gmail.com>
2024-01-25 17:35:27 +0800

    Necessary?

--- gradle/conventions/core/src/main/plugins/build/kt/android/app/_plugin.kt
+++ gradle/conventions/core/src/main/plugins/build/kt/android/app/_plugin.kt
@@ -1,11 +1,20 @@
 package build.kt.android.app
 
 import build.api.ProjectPlugin
+import build.api.dsl.accessors.androidApp
 import org.gradle.kotlin.dsl.*
 
 class _plugin : ProjectPlugin({
 	apply {
 		plugin("com.android.application")
 		plugin<build.kt.android.internal._plugin>()
+	}
+
+	androidApp.run {
+		packaging {
+			resources {
+				excludes += "/META-INF/{AL2.0,LGPL2.1}"
+			}
+		}
 	}
 })
