b8a9f2bfa448d67735db26a48fa43619cc413114
netaneru <netaneru3@gmail.com>
2024-01-25 13:19:37 +0800

    Exposed for further extensions

---
+++ gradle/build.foundation/core/src/main/kotlin/build/foundation/BuildFoundation.mppHierarchy.kt
@@ -0,0 +1,36 @@
+package build.foundation
+
+import build.foundation.BuildFoundation.MPP
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.plugin.KotlinHierarchyTemplate
+import org.jetbrains.kotlin.gradle.plugin.extend
+
+val BuildFoundation.mppHierarchy get() = mppHierarchy_
+
+@OptIn(ExperimentalKotlinGradlePluginApi::class)
+private val mppHierarchy_ = KotlinHierarchyTemplate.default.extend {
+	// Extend the default hierarchy with our own custom setup
+	common {
+		group(MPP.jvmish) {
+			withAndroidTarget()
+			withJvm()
+		}
+		group("native") {
+			group(MPP.unix) {
+				group("apple")
+				group("linux")
+				group("androidNative")
+			}
+		}
+		group(MPP.desktop) {
+			withJvm()
+			group("linux")
+			group("macos")
+			group("mingw")
+		}
+		group(MPP.mobile) {
+			withAndroidTarget()
+			group("ios")
+		}
+	}
+}
--- gradle/build.foundation/core/src/main/kotlin/build/foundation/BuildFoundation.setUpMppHierarchy.kt
+++ gradle/build.foundation/core/src/main/kotlin/build/foundation/BuildFoundation.setUpMppHierarchy.kt
@@ -1,6 +1,5 @@
 package build.foundation
 
-import build.foundation.BuildFoundation.MPP
 import org.gradle.api.Project
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
@@ -8,31 +7,6 @@ import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 fun BuildFoundation.setUpMppHierarchy(project: Project): Unit = with(project) {
 	with(extensions.getByName("kotlin") as KotlinMultiplatformExtension) {
 		@OptIn(ExperimentalKotlinGradlePluginApi::class)
-		applyDefaultHierarchyTemplate {
-			// Extend the default hierarchy with our own custom setup
-			common {
-				group(MPP.jvmish) {
-					withAndroidTarget()
-					withJvm()
-				}
-				group("native") {
-					group(MPP.unix) {
-						group("apple")
-						group("linux")
-						group("androidNative")
-					}
-				}
-				group(MPP.desktop) {
-					withJvm()
-					group("linux")
-					group("macos")
-					group("mingw")
-				}
-				group(MPP.mobile) {
-					withAndroidTarget()
-					group("ios")
-				}
-			}
-		}
+		applyHierarchyTemplate(mppHierarchy)
 	}
 }
