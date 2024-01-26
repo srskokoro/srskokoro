4daf093a28fd0ef32b09c6de537e7673b24f7353
netaneru <netaneru3@gmail.com>
2024-01-26 15:17:42 +0800

    This one seems to work

--- gradle/reloadOnIdeaInitialSync.settings.gradle.kts
+++ gradle/reloadOnIdeaInitialSync.settings.gradle.kts
@@ -16,18 +16,29 @@ run<Unit> {
 	if (Build.ideaInitialSyncSeen) return@run
 	Build.ideaInitialSyncSeen = true
 
-	// NOTE: The following prevents Android Studio from loading projects on
-	// initial project sync. This exists so as to prevent Android Studio from
+	// NOTE: The following forces Android Studio to load the entire build fully
+	// on initial project sync. This exists so as to prevent Android Studio from
 	// analyzing source code right away, since it also has the consequence of
 	// treating Android source code as if they were for the current JDK running
 	// Gradle (that isn't affected by Gradle's JVM toolchain support), which can
 	// cause issues in the IDE's code analysis, as it would fail for when a
 	// referenced class is not present in Android targets but is present in the
 	// current JDK (e.g., `java.lang.constant.Constable`).
-	error(
-		"""
-		Due to issues with Android Studio's project import mechanism (that we would
-		rather not deal with), a manual IDE sync has been required on first load.
-		""".trimIndent()
-	)
+	// --
+
+	val settings = settings
+	val workDir = File(settings.settingsDir, ".build/reloadOnIdeaInitialSync")
+	workDir.deleteRecursively()
+	workDir.mkdirs()
+
+	val dummy = java.util.UUID.randomUUID().run {
+		File(workDir, "dummy_${
+			java.lang.Long.toHexString(mostSignificantBits)
+		}_${
+			java.lang.Long.toHexString(leastSignificantBits)
+		}.settings.gradle.kts")
+	}
+	dummy.writeText("println(\"IDE forced to reload all projects\")\n")
+
+	settings.apply(from = dummy)
 }
