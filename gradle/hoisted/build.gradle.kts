100a025d84bd2dd8a38d05a38afc339ac9e4e586
netaneru <netaneru3@gmail.com>
2024-01-26 12:16:59 +0800

    Better if hidden under "hoisted"

--- gradle/hoisted/build.gradle.kts
+++ gradle/hoisted/build.gradle.kts
@@ -1,3 +1,17 @@
 plugins {
 	id("build.root")
 }
+
+// The following is expected to be a valid Android-only project, which we
+// evaluate first in order for Android Studio to see that the build contains
+// Android sources. Without this, Android Studio will analyze sources for
+// Android targets as if they were for the current JDK, which can cause an issue
+// in the IDE's code analysis, as it would fail for when a class is referenced
+// that's not present in Android targets but is present in the current JDK
+// (e.g., `java.lang.constant.Constable`).
+//
+// Note that, the "current JDK" said above is the "Gradle JDK" used by Android
+// Studio for running Gradle and syncing projects. It's not the same as the JDK
+// set up via Gradle's JVM toolchain support. (At least, that's how things work
+// at the time of writing.)
+evaluationDependsOn(":init")
--- gradle/hoisted/settings.gradle.kts
+++ gradle/hoisted/settings.gradle.kts
@@ -35,3 +35,5 @@ dependencyResolutionManagement {
 		google()
 	}
 }
+
+include("init")
