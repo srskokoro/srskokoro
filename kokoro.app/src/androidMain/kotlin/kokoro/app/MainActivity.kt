2891c07acd6a62c029c2f989101bbe1c20a3dd89
netaneru <netaneru3@gmail.com>
2024-02-24 00:05:17 +0800

    Initial implementation of splash screen

--- kokoro.app/src/androidMain/kotlin/kokoro/app/MainActivity.kt
+++ kokoro.app/src/androidMain/kotlin/kokoro/app/MainActivity.kt
@@ -2,13 +2,26 @@ package kokoro.app
 
 import android.app.Activity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : Activity() {
+
 	override fun onCreate(savedInstanceState: Bundle?) {
+		// See, https://developer.android.com/develop/ui/views/launch/splash-screen/migrate
+		// - See also https://developer.android.com/reference/kotlin/androidx/core/splashscreen/SplashScreen
+		val splashScreen = installSplashScreen()
+
 		super.onCreate(savedInstanceState)
 
-		// TODO Launch main window here!
+		// Keep the splash screen visible for this Activity.
+		// - https://developer.android.com/develop/ui/views/launch/splash-screen/migrate#prevent
+		splashScreen.setKeepOnScreenCondition { true }
 
+		startMainWindow()
 		finish() // Done!
+	}
+
+	private fun startMainWindow() {
+		// TODO Implement!
 	}
 }
