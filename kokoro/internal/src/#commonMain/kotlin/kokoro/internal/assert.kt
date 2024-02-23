53373b47e9e07dc59bc89301f022a9e17eee2bf4
netaneru <netaneru3@gmail.com>
2024-02-23 15:12:25 +0800

    More runtime assertion utilities

--- kokoro/internal/src/#commonMain/kotlin/kokoro/internal/assert.kt
+++ kokoro/internal/src/#commonMain/kotlin/kokoro/internal/assert.kt
@@ -15,3 +15,19 @@ inline fun assertUnreachable(orFailWith: () -> Any = { "Should be unreachable" }
 		throw AssertionError(message)
 	}
 }
+
+inline fun assertUnreachable(cause: Throwable?, orFailWith: () -> Any = { "Should be unreachable" }) {
+	if (ASSERTIONS_ENABLED) {
+		val message = orFailWith().toString()
+		throw AssertionError(message, cause)
+	}
+}
+
+inline fun assertSucceeds(statement: () -> Unit, orFailWith: () -> Any = { "Should be unreachable" }) {
+	if (ASSERTIONS_ENABLED) try {
+		statement()
+	} catch (ex: Throwable) {
+		val message = orFailWith().toString()
+		throw AssertionError(message, ex)
+	}
+}
