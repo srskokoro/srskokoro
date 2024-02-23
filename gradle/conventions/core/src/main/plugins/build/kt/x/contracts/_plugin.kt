7eb69b468d7909c4c4c2bf1ee18b5567138638dd
netaneru <netaneru3@gmail.com>
2024-02-23 15:35:11 +0800

    Plugin to easily opt-in to kotlin contracts for the entire project

---
+++ gradle/conventions/core/src/main/plugins/build/kt/x/contracts/_plugin.kt
@@ -0,0 +1,28 @@
+package build.kt.x.contracts
+
+import build.api.ProjectPlugin
+import build.api.dsl.*
+import build.api.dsl.accessors.kotlinSourceSets
+import org.gradle.kotlin.dsl.*
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
+
+private const val REQUIRED_OPT_IN_CLASS = "kotlin.contracts.ExperimentalContracts"
+
+class _plugin : ProjectPlugin({
+	tasks.withType<KotlinCompilationTask<*>>().configureEach {
+		compilerOptions.freeCompilerArgs.run {
+			// NOTE: Android Studio honors `languageSettings.optIn()` (from
+			// `KotlinSourceSet`) but only for KMP :P
+			add("-opt-in=$REQUIRED_OPT_IN_CLASS")
+		}
+	}
+	// NOTE: We're using `prioritizedAfterEvaluate()` here since KGP may not
+	// have been applied yet.
+	prioritizedAfterEvaluate {
+		kotlinSourceSets.configureEach {
+			// NOTE: Android Studio doesn't honor `-opt-in` compiler option when
+			// in KMP :P
+			languageSettings.optIn(REQUIRED_OPT_IN_CLASS)
+		}
+	}
+})
