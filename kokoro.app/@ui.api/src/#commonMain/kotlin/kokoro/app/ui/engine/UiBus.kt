f4bf7f669d6734797b6c837dafab6c02269909c4
netaneru <netaneru3@gmail.com>
2024-02-27 23:25:31 +0800

    Made it consistent with `WvWindowFactory`

--- kokoro.app/@ui.api/src/#commonMain/kotlin/kokoro/app/ui/engine/UiBus.kt
+++ kokoro.app/@ui.api/src/#commonMain/kotlin/kokoro/app/ui/engine/UiBus.kt
@@ -7,13 +7,18 @@ import kotlin.jvm.JvmField
 import kotlin.reflect.typeOf
 
 inline fun <reified T> UiBus(
-	tag: String? = null,
 	noinline serialization: SerializersModule.() -> KSerializer<T> = UiBus.defaultSerialization<T>(),
 ) = UiBus(
 	serialization,
-	typeOf<T>().toString().let { type ->
-		if (tag == null) type else "$type#$tag"
-	},
+	UiBus.id<T>(),
+)
+
+inline fun <reified T> UiBus(
+	tag: String?,
+	noinline serialization: SerializersModule.() -> KSerializer<T> = UiBus.defaultSerialization<T>(),
+) = UiBus(
+	serialization,
+	UiBus.id<T>(tag),
 )
 
 data class UiBus<T>(
@@ -22,5 +27,11 @@ data class UiBus<T>(
 ) {
 	companion object {
 		inline fun <reified T> defaultSerialization(): SerializersModule.() -> KSerializer<T> = { serializer() }
+
+		inline fun <reified T> id(tag: String? = null): String {
+			return typeOf<T>().toString().let { type ->
+				if (tag == null) type else "$type#$tag"
+			}
+		}
 	}
 }
