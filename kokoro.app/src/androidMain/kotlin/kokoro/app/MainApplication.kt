61809d04fc3c30d83513c1682b7cf3318c5e7e28
netaneru <netaneru3@gmail.com>
2024-02-29 14:42:34 +0800

    Initial setup so that we can run the app in Android

--- kokoro.app/src/androidMain/kotlin/kokoro/app/MainApplication.kt
+++ kokoro.app/src/androidMain/kotlin/kokoro/app/MainApplication.kt
@@ -3,12 +3,16 @@ package kokoro.app
 import kokoro.app.ui.engine.window.WvWindowFactory
 import kokoro.app.ui.engine.window.WvWindowHandle_globalInit
 import kokoro.internal.annotation.MainThread
+import kokoro.internal.os.SerializationEncoded
+import kotlinx.serialization.modules.EmptySerializersModule
 
 class MainApplication : CoreApplication() {
 
 	@MainThread
 	override fun onCreate() {
 		super.onCreate()
+
+		SerializationEncoded.init(EmptySerializersModule()) // TODO!
 
 		@Suppress("DEPRECATION_ERROR") WvWindowHandle_globalInit()
 		@Suppress("DEPRECATION_ERROR") WvWindowFactory.globalInit()
