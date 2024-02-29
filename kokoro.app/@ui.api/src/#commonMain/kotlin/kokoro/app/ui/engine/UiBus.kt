c3337d0ec9f01bf8ffb33651840fcd0c2ccde183
netaneru <netaneru3@gmail.com>
2024-02-29 16:45:28 +0800

    Better?

--- kokoro.app/@ui.api/src/#commonMain/kotlin/kokoro/app/ui/engine/UiBus.kt
+++ kokoro.app/@ui.api/src/#commonMain/kotlin/kokoro/app/ui/engine/UiBus.kt
@@ -6,26 +6,42 @@ import kotlinx.serialization.serializer
 import kotlin.jvm.JvmField
 import kotlin.reflect.typeOf
 
-inline fun <reified T> UiBus(
-	noinline serialization: SerializersModule.() -> KSerializer<T> = UiBus.defaultSerialization<T>(),
-) = UiBus(serialization, UiBus.id<T>())
-
-inline fun <reified T> UiBus(
-	tag: String?,
-	noinline serialization: SerializersModule.() -> KSerializer<T> = UiBus.defaultSerialization<T>(),
-) = UiBus(serialization, UiBus.id<T>(tag))
-
-data class UiBus<T>(
-	@JvmField val serialization: SerializersModule.() -> KSerializer<T>,
+@Suppress("DataClassPrivateConstructor")
+data class UiBus<T> private constructor(
 	@JvmField val id: String,
+	@JvmField val serialization: SerializersModule.() -> KSerializer<T>,
 ) {
 	companion object {
 		inline fun <reified T> defaultSerialization(): SerializersModule.() -> KSerializer<T> = { serializer() }
 
+		/**
+		 * @see UiBus.of
+		 */
 		inline fun <reified T> id(tag: String? = null): String {
 			return typeOf<T>().toString().let { type ->
 				if (tag == null) type else "$type#$tag"
 			}
 		}
+
+		inline fun <reified T> of(
+			tag: String? = null,
+			noinline serialization: SerializersModule.() -> KSerializer<T> = defaultSerialization<T>(),
+		) = wrap(id<T>(tag), serialization)
+
+		/**
+		 * @see UiBus.of
+		 * @see UiBus.id
+		 */
+		fun <T> wrap(
+			id: String,
+			serialization: SerializersModule.() -> KSerializer<T>,
+		) = UiBus(id, serialization)
+
+		/**
+		 * @see UiBus.of
+		 * @see UiBus.id
+		 * @see UiBus.wrap
+		 */
+		inline fun <reified T> wrap(id: String) = wrap(id, defaultSerialization<T>())
 	}
 }
