c4fb1c8a65934292b7bb535e314e49f694b87e8d
netaneru <netaneru3@gmail.com>
2024-03-09 03:00:07 +0800

    Convenience + Less verbose

--- gradle/conventions/testing/src/commonMain/kotlin/build/test/assertResult.kt
+++ gradle/conventions/testing/src/commonMain/kotlin/build/test/assertResult.kt
@@ -1,8 +1,13 @@
 package build.test
 
+import assertk.Assert
 import assertk.assertThat
+import assertk.assertions.isFailure
+import assertk.assertions.isInstanceOf
 
 /**
  * @see assertWith
  */
 inline fun <R> assertResult(block: () -> R) = assertThat(runCatching(block))
+
+inline fun <reified T : Throwable> Assert<Result<*>>.isFailure() = isFailure().isInstanceOf<T>()
