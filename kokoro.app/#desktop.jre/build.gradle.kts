3f564ee7279ac9da5b328a205fa6689e58ed2c9b
netaneru <netaneru3@gmail.com>
2024-03-15 15:17:00 +0800

    Preferred the "start scripts" template provided by Gradle

--- kokoro.app/#desktop.jre/build.gradle.kts
+++ kokoro.app/#desktop.jre/build.gradle.kts
@@ -1,4 +1,5 @@
 import build.api.dsl.*
+import build.support.cast
 
 plugins {
 	id("build.kt.jvm.app")
@@ -10,6 +11,15 @@ group = evaluatedParent.group
 application {
 	mainClass.set("main.MainKt")
 	applicationName = extra["kokoro.app.exe.name"] as String
+}
+
+tasks.startShadowScripts {
+	// Prefer the "start scripts" template provided by Gradle.
+	val startScripts = project.tasks.startScripts.get()
+	unixStartScriptGenerator.cast<TemplateBasedScriptGenerator>().template =
+		startScripts.unixStartScriptGenerator.cast<TemplateBasedScriptGenerator>().template
+	windowsStartScriptGenerator.cast<TemplateBasedScriptGenerator>().template =
+		startScripts.windowsStartScriptGenerator.cast<TemplateBasedScriptGenerator>().template
 }
 
 dependencies {
