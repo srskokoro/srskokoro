1353727edc744406ca6bde94085b3f250ad3078e
netaneru <netaneru3@gmail.com>
2024-03-22 12:44:28 +0800

    Oops: the check was only correct for Windows

--- kokoro.app/src/jreMain/kotlin/main/appHomeExe.kt
+++ kokoro.app/src/jreMain/kotlin/main/appHomeExe.kt
@@ -6,9 +6,20 @@ import kotlin.jvm.optionals.getOrNull
 @JvmField val appHomeExe = run(fun(): String? {
 	ProcessHandle.current().info().command().getOrNull()?.let { exePath ->
 		val exeFile = File(exePath).absoluteFile
-		if (exeFile.parentFile == appHomeDir) {
-			return exeFile.name
+		// TODO! Verify that the following checks are correct for all platforms
+		run check@{
+			// See, “Generated Application Image | Packaging Overview | Packaging Tool User's Guide”
+			// -- https://docs.oracle.com/en/java/javase/21/jpackage/packaging-overview.html#GUID-DAE6A497-6E6F-4895-90CA-3C71AF052271
+			val exeDir = exeFile.parentFile
+			val appHomeDir = appHomeDir
+			if (appHomeDir == exeDir) return@check
+			exeDir.parentFile.let {
+				if (appHomeDir == it) return@check
+				if (appHomeDir == File(it, "lib")) return@check
+			}
+			return@let // All checks failed!
 		}
+		return exeFile.name
 	}
 	return System.getenv("APP_BASE_NAME")
 })
