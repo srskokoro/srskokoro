6b46abac05ea6561d4033dc7f352830a9cdc4c34
netaneru <netaneru3@gmail.com>
2024-03-25 06:16:08 +0800

    Fix: SLF4J warning in unit tests

--- gradle/conventions/core/build.gradle.kts
+++ gradle/conventions/core/build.gradle.kts
@@ -12,7 +12,12 @@ dependencies {
 	implementation(kotlin("sam-with-receiver"))
 	implementation(kotlin("assignment"))
 
-	implementation("org.jetbrains.kotlinx:atomicfu-gradle-plugin")
+	implementation("org.jetbrains.kotlinx:atomicfu-gradle-plugin") {
+		// Needed because Gradle API already has an SLF4J binding and having
+		// more than one causes a warning to be reported (particularly, in unit
+		// tests). See, http://www.slf4j.org/codes.html#multiple_bindings
+		exclude("org.slf4j", "slf4j-simple")
+	}
 	implementation("com.android.tools.build:gradle")
 	implementation(plugin("io.kotest.multiplatform"))
 	implementation(plugin("com.github.gmazzo.buildconfig"))
