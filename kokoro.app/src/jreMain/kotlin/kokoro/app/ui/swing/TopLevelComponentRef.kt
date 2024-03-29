de41cfef3ebd5d5408812c5115115044184a1427
netaneru <netaneru3@gmail.com>
2024-03-29 18:17:46 +0800

    Clarity

--- kokoro.app/src/jreMain/kotlin/kokoro/app/ui/swing/TopLevelComponentRef.kt
+++ kokoro.app/src/jreMain/kotlin/kokoro/app/ui/swing/TopLevelComponentRef.kt
@@ -4,6 +4,9 @@ import java.awt.Component
 import java.lang.ref.WeakReference
 import kotlin.coroutines.CoroutineContext
 
+/**
+ * Coroutine context element for inferring top-level Swing components.
+ */
 class TopLevelComponentRef<C : Component?>(component: C) : WeakReference<C>(component), CoroutineContext.Element {
 
 	companion object Key : CoroutineContext.Key<TopLevelComponentRef<*>>
