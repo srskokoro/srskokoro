3c8ea3b0abd22973102ad663983a4d4c2f12f0ba
netaneru <netaneru3@gmail.com>
2024-04-06 05:01:51 +0800

    A better demo code

--- kokoro.app/src/jreMain/kotlin/main/main.kt
+++ kokoro.app/src/jreMain/kotlin/main/main.kt
@@ -1,9 +1,27 @@
 package main
 
+import com.formdev.flatlaf.FlatDarkLaf
 import com.formdev.flatlaf.FlatSystemProperties
+import kokoro.app.ui.swing.BaseWindowFrame
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.swing.Swing
+import java.awt.Dimension
 import java.io.File
+import javax.swing.JLabel
 
-fun main() {
+fun main(): Unit = runBlocking(Dispatchers.Swing) {
 	System.setProperty(FlatSystemProperties.NATIVE_LIBRARY_PATH, File(appHomeDir, "flatlaf").path)
+
 	println("Hello World!")
+
+	FlatDarkLaf.setup()
+	BaseWindowFrame(appHomeExe.toString()).apply {
+		contentPane.add(JLabel("Hello World!", JLabel.CENTER))
+	}.apply {
+		pack()
+		minimumSize = Dimension(250, 250)
+		isLocationByPlatform = true
+		isVisible = true
+	}
 }
