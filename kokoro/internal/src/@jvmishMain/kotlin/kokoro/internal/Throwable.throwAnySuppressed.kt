2076daf18546cb35a308d8dcb0aed12cd33f3f85
netaneru <netaneru3@gmail.com>
2024-04-11 06:15:47 +0800

    Better

--- kokoro/internal/src/@jvmishMain/kotlin/kokoro/internal/Throwable.throwAnySuppressed.kt
+++ kokoro/internal/src/@jvmishMain/kotlin/kokoro/internal/Throwable.throwAnySuppressed.kt
@@ -6,7 +6,8 @@ fun Throwable.throwAnySuppressed() {
 
 	val ex = suppressed[0]
 	for (i in 1..<suppressed.size) {
-		ex.addSuppressed(suppressed[i])
+		@Suppress("PLATFORM_CLASS_MAPPED_TO_KOTLIN")
+		(ex as java.lang.Throwable).addSuppressed(suppressed[i])
 	}
 	throw ex
 }
