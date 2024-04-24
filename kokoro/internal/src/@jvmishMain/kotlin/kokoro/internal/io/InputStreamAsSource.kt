b960136330035e5b47d12892169fe357b419a007
netaneru <netaneru3@gmail.com>
2024-04-24 10:06:31 +0800

    Bidirectional conversion between Okio interfaces & Java IO streams
    - Code resurrected from 1feb9eee with some modifications.

---
+++ kokoro/internal/src/@jvmishMain/kotlin/kokoro/internal/io/InputStreamAsSource.kt
@@ -0,0 +1,28 @@
+package kokoro.internal.io
+
+import okio.BufferedSource
+import okio.Source
+import okio.buffer
+import okio.source
+import java.io.InputStream
+
+fun InputStream.asSource() = InputStreamAsSource(this)
+
+class InputStreamAsSource(
+	@PublishedApi @JvmField
+	internal val inputStream: InputStream,
+) : Source by inputStream.source() {
+
+	@Suppress("NOTHING_TO_INLINE")
+	inline fun asInputStream() = inputStream
+
+	override fun toString() = "asSource($inputStream)"
+}
+
+fun Source.asInputStream(): InputStream {
+	if (this is InputStreamAsSource) {
+		return asInputStream()
+	} else {
+		return (if (this !is BufferedSource) buffer() else this).inputStream()
+	}
+}
---
+++ kokoro/internal/src/@jvmishMain/kotlin/kokoro/internal/io/OutputStreamAsSink.kt
@@ -0,0 +1,28 @@
+package kokoro.internal.io
+
+import okio.BufferedSink
+import okio.Sink
+import okio.buffer
+import okio.sink
+import java.io.OutputStream
+
+fun OutputStream.asSink() = OutputStreamAsSink(this)
+
+class OutputStreamAsSink(
+	@PublishedApi @JvmField
+	internal val outputStream: OutputStream,
+) : Sink by outputStream.sink() {
+
+	@Suppress("NOTHING_TO_INLINE")
+	inline fun asOutputStream() = outputStream
+
+	override fun toString() = "asSink($outputStream)"
+}
+
+fun Sink.asOutputStream(): OutputStream {
+	if (this is OutputStreamAsSink) {
+		return asOutputStream()
+	} else {
+		return (if (this !is BufferedSink) buffer() else this).outputStream()
+	}
+}
