64ba2e420676dc3ecdc3619c772dcdec2a9205a7
netaneru <netaneru3@gmail.com>
2024-05-03 21:37:45 +0800

    Seems unnecessary

--- kokoro.app/src/#commonMain/kotlin/kokoro/app/ui/engine/web/WebRequest.kt
+++ kokoro.app/src/#commonMain/kotlin/kokoro/app/ui/engine/web/WebRequest.kt
@@ -9,10 +9,4 @@ interface WebRequest {
 	fun headers(): Map<String, String>
 
 	fun header(name: String): String?
-
-	/**
-	 * Indicates whether or not this object is still valid. When `false`, the
-	 * methods and properties of this object may throw.
-	 */
-	val isValid: Boolean
 }
