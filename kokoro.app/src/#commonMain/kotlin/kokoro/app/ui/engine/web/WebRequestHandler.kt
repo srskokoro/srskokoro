0fe0b2c381c9b02b22f11430d519d2123a991e9e
netaneru <netaneru3@gmail.com>
2024-05-03 21:30:33 +0800

    Now forced to always respond instead

--- kokoro.app/src/#commonMain/kotlin/kokoro/app/ui/engine/web/WebRequestHandler.kt
+++ kokoro.app/src/#commonMain/kotlin/kokoro/app/ui/engine/web/WebRequestHandler.kt
@@ -1,12 +1,6 @@
 package kokoro.app.ui.engine.web
 
-import kotlin.jvm.JvmField
-
 fun interface WebRequestHandler {
 
-	suspend fun onWebRequest(request: WebRequest): WebResponse?
-
-	companion object {
-		@JvmField val NULL = WebRequestHandler { null }
-	}
+	suspend fun handle(request: WebRequest): WebResponse
 }
