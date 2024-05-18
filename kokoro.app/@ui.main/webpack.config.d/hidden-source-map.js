fe77ac88fdb95993b7d67a7c3ac2aba9287f98ab
netaneru <netaneru3@gmail.com>
2024-05-19 01:09:35 +0800

    Removed `sourceMappingURL` comment from the output JS

---
+++ kokoro.app/@ui.main/webpack.config.d/hidden-source-map.js
@@ -0,0 +1,3 @@
+// Generates source maps without adding the `sourceMappingURL` comment.
+// - See also HTTP header `SourceMap` -- https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/SourceMap
+config.devtool = 'hidden-source-map'
