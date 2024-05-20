214277840876f36ab6a0965be3919cd3769277b4
netaneru <netaneru3@gmail.com>
2024-05-21 07:00:40 +0800

    Made more secure

--- kokoro.app/src/jreMain/kotlin/kokoro/app/LibAssets.jre.kt
+++ kokoro.app/src/jreMain/kotlin/kokoro/app/LibAssets.jre.kt
@@ -1,17 +1,33 @@
 package kokoro.app
 
 import okio.FileNotFoundException
+import java.io.File
+import java.io.IOException
 import java.io.InputStream
 
 actual object LibAssets {
 	@JvmField val loader = LibAssets::class.java.classLoader!!
 }
 
-actual fun LibAssets.openStreamOrNull(path: String): InputStream? =
-	loader.getResourceAsStream(path)
+actual fun LibAssets.openStreamOrNull(path: String): InputStream? {
+	val url = loader.getResource(path)
+	if (url != null) try {
+		if (url.protocol != "file" || !File(url.path).isDirectory) {
+			return url.openStream()
+		} else {
+			// NOTE: Necessary as the default implementation is insecure -- it
+			// would return a list of directory entries instead.
+			// - See also, https://stackoverflow.com/a/20107785
+			return InputStream.nullInputStream()
+		}
+	} catch (_: IOException) {
+		// Ignore.
+	}
+	return null
+}
 
 @Throws(FileNotFoundException::class)
 actual fun LibAssets.openStream(path: String): InputStream =
-	loader.getResourceAsStream(path) ?: throw E_MissingAsset(path)
+	openStreamOrNull(path) ?: throw E_MissingAsset(path)
 
 private fun E_MissingAsset(path: String) = FileNotFoundException("Asset file: $path")
--- kokoro.app/test/#commonTest/kotlin/kokoro/app/test_LibAssets.kt
+++ kokoro.app/test/#commonTest/kotlin/kokoro/app/test_LibAssets.kt
@@ -1,12 +1,16 @@
 package kokoro.app
 
+import assertk.assertAll
 import assertk.assertions.isEqualTo
 import assertk.assertions.isSuccess
+import assertk.assertions.matchesPredicate
 import build.test.assertResult
+import build.test.assertWith
 import build.test.isFailure
 import io.kotest.core.spec.style.FreeSpec
 import okio.FileNotFoundException
 import okio.buffer
+import okio.use
 
 /** @see LibAssets */
 class test_LibAssets : FreeSpec({
@@ -20,6 +24,15 @@ class test_LibAssets : FreeSpec({
 		assertResult {
 			LibAssets.open("kokoro/app/missing.txt")
 		}.isFailure<FileNotFoundException>()
+	}
+	"Asset directories cannot be streamed" {
+		assertAll {
+			for (path in listOf("kokoro/app", "kokoro/app/")) assertWith {
+				LibAssets.openOrNull(path)
+			}.matchesPredicate { source ->
+				source == null || source.buffer().use { it.readUtf8().trim() } == ""
+			}
+		}
 	}
 	test_LibAssets_platformTest()
 })
