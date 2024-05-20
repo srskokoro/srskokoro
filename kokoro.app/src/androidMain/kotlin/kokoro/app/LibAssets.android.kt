e56fd0403b9dc9adc77553e03a56464ce4ba69b0
netaneru <netaneru3@gmail.com>
2024-05-21 03:40:53 +0800

    Optimized

--- kokoro.app/src/androidMain/kotlin/kokoro/app/LibAssets.android.kt
+++ kokoro.app/src/androidMain/kotlin/kokoro/app/LibAssets.android.kt
@@ -1,6 +1,8 @@
 package kokoro.app
 
 import android.content.res.AssetManager
+import android.util.LruCache
+import kokoro.internal.DEBUG
 import okio.FileNotFoundException
 import java.io.IOException
 import java.io.InputStream
@@ -11,10 +13,19 @@ actual object LibAssets {
 	@JvmField val manager: AssetManager = CoreApplication.get().assets
 }
 
-actual fun LibAssets.openStreamOrNull(path: String): InputStream? = try {
-	manager.open(path)
-} catch (ex: IOException) {
-	null
+private val LibAssets_MISSES = object : LruCache<String, Boolean>(32768) {
+	override fun sizeOf(key: String, value: Boolean): Int =
+		key.length * Char.SIZE_BYTES + /* Estimated object size per entry */ 64
+}
+
+actual fun LibAssets.openStreamOrNull(path: String): InputStream? {
+	if (LibAssets_MISSES.get(path) == null) try {
+		manager.open(path)
+	} catch (ex: IOException) {
+		if (DEBUG && ex !is FileNotFoundException) throw ex
+		LibAssets_MISSES.put(path, true)
+	}
+	return null
 }
 
 @Throws(FileNotFoundException::class)
