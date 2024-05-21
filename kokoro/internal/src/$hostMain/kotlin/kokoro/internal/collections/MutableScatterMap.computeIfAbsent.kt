3f20f8e569e0557983299fcb5d46568585b80ec1
netaneru <netaneru3@gmail.com>
2024-05-22 03:46:08 +0800

    Ability to take an extra action when the value exists

--- kokoro/internal/src/$hostMain/kotlin/kokoro/internal/collections/MutableScatterMap.computeIfAbsent.kt
+++ kokoro/internal/src/$hostMain/kotlin/kokoro/internal/collections/MutableScatterMap.computeIfAbsent.kt
@@ -2,9 +2,14 @@ package kokoro.internal.collections
 
 import androidx.collection.MutableScatterMap
 
-inline fun <K, V : Any> MutableScatterMap<K, V>.computeIfAbsent(key: K, computeBlock: (key: K) -> V): V {
-	return compute(key) { _, prev ->
-		if (prev != null) return@computeIfAbsent prev
-		computeBlock.invoke(key)
+inline fun <K, V : Any> MutableScatterMap<K, V>.computeIfAbsent(
+	key: K,
+	onExisting: (key: K, value: V) -> Unit = { _, _ -> },
+	computeBlock: (key: K) -> V,
+): V = compute(key) { _, prev ->
+	if (prev != null) {
+		onExisting(key, prev)
+		return@computeIfAbsent prev
 	}
+	computeBlock.invoke(key)
 }
