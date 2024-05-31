ea61d43502dbadca6f459b41f4c4fc76550b73fd
netaneru <netaneru3@gmail.com>
2024-06-01 00:01:19 +0800

    Ability to iterate JS map entries

--- kokoro/internal/src/jsMain/kotlin/kokoro/internal/collections/JsMap.kt
+++ kokoro/internal/src/jsMain/kotlin/kokoro/internal/collections/JsMap.kt
@@ -1,7 +1,7 @@
 package kokoro.internal.collections
 
 @JsName("Map")
-open external class JsMap<K, V> {
+open external class JsMap<K, V> : JsIterable<JsMapEntry<K, V>> {
 
 	val size: Int
 
@@ -15,6 +15,12 @@ open external class JsMap<K, V> {
 
 	operator fun get(key: K): V?
 
+	fun keys(): JsIterable<K>
+
+	fun values(): JsIterable<V>
+
+	fun entries(): JsIterable<JsMapEntry<K, V>>
+
 	operator fun set(key: K, value: V)
 
 	@JsName("set")
@@ -25,3 +31,21 @@ open external class JsMap<K, V> {
 
 	fun clear()
 }
+
+val <K> JsMap<K, *>.keys get() = keys()
+
+val <V> JsMap<*, V>.values get() = values()
+
+val <K, V> JsMap<K, V>.entries get() = entries()
+
+external interface JsMapEntry<@Suppress("unused") out K, @Suppress("unused") out V>
+
+val <K> JsMapEntry<K, *>.key: K
+	inline get() = @Suppress("UnsafeCastFromDynamic") asDynamic()[0]
+
+val <V> JsMapEntry<*, V>.value: V
+	inline get() = @Suppress("UnsafeCastFromDynamic") asDynamic()[1]
+
+inline operator fun <K> JsMapEntry<K, *>.component1() = key
+
+inline operator fun <V> JsMapEntry<*, V>.component2() = value
