d6e1cebb1b039dcee5f0072649a86169b751cda7
netaneru <netaneru3@gmail.com>
2024-06-02 15:30:34 +0800

    More constants for consistency

--- kokoro.app/@ui.api/src/#commonMain/kotlin/kokoro/app/ui/engine/web/HTTPX_.kt
+++ kokoro.app/@ui.api/src/#commonMain/kotlin/kokoro/app/ui/engine/web/HTTPX_.kt
@@ -8,3 +8,7 @@ val HTTPX_RES_X = "$HTTPX://$HOST_RES_X"
 
 /** @see HOST_RES_U */
 val HTTPX_RES_U = "$HTTPX://$HOST_RES_U"
+
+// --
+
+val HTTPX_RES_X_UI_JS = "$HTTPX_RES_X/ui.js"
--- kokoro.app/src/#commonMain/kotlin/kokoro/app/ui/engine/web/WvLibAssetResolver.kt
+++ kokoro.app/src/#commonMain/kotlin/kokoro/app/ui/engine/web/WvLibAssetResolver.kt
@@ -106,6 +106,8 @@ data class WvLibAssetResolver(
 
 		@JvmField val HTTPX_RES_U = httpx(HOST_RES_U)
 
+		@JvmField val PRESET = HTTPX_UI_X + HTTPX_RES_X + HTTPX_RES_U
+
 		@Suppress("NOTHING_TO_INLINE")
 		inline fun httpx(host: String) = WvLibAssetResolver("$HTTPX://$host", "wv/{$host}")
 
