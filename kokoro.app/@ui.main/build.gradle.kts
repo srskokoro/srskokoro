5741abbfc9c090c71a00efd87652d86f84a79da9
netaneru <netaneru3@gmail.com>
2024-06-02 14:07:16 +0800

    Explicitly used `window` as the module target

--- kokoro.app/@ui.main/build.gradle.kts
+++ kokoro.app/@ui.main/build.gradle.kts
@@ -1,8 +1,18 @@
+import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackOutput
+
 plugins {
 	id("kokoro.build.kt.js.packed")
 }
 
 base.archivesName = "ui"
+
+kotlin.js {
+	browser {
+		webpackTask {
+			output.libraryTarget = KotlinWebpackOutput.Target.WINDOW
+		}
+	}
+}
 
 dependencies {
 	jsMainImplementation(project(":kokoro.app:ui.api"))
