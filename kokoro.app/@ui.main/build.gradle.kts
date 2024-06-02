7ec5a03be989479d727c1c049e8fd901e8a10290
netaneru <netaneru3@gmail.com>
2024-06-02 14:10:00 +0800

    A different module name + Constant for the module name

--- kokoro.app/@ui.main/build.gradle.kts
+++ kokoro.app/@ui.main/build.gradle.kts
@@ -2,16 +2,29 @@ import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackOutput
 
 plugins {
 	id("kokoro.build.kt.js.packed")
+	id("build.gmazzo.buildconfig")
 }
 
-base.archivesName = "ui"
+private object Build {
+	const val NAMESPACE = "kokoro.app.ui.engine"
+	const val ARCHIVE_NAME = "ui"
+	const val UI_MODULE_NAME = "uiJs"
+}
+
+base.archivesName = Build.ARCHIVE_NAME
 
 kotlin.js {
 	browser {
 		webpackTask {
+			output.library = Build.UI_MODULE_NAME
 			output.libraryTarget = KotlinWebpackOutput.Target.WINDOW
 		}
 	}
+}
+
+buildConfig {
+	asPublicTopLevel() inPackage Build.NAMESPACE
+	buildConfigField("String", "UI_MODULE", "\"${Build.UI_MODULE_NAME}\"")
 }
 
 dependencies {
