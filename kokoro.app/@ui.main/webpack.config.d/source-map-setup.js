883f95dd02f4bd1c257e8c60cd5bff2863c558a6
netaneru <netaneru3@gmail.com>
2024-06-02 08:03:02 +0800

    Do not include sources content

--- kokoro.app/@ui.main/webpack.config.d/source-map-setup.js
+++ kokoro.app/@ui.main/webpack.config.d/source-map-setup.js
@@ -2,4 +2,4 @@
 //
 // NOTE: `hidden-*` generates source maps without adding the `sourceMappingURL`
 // comment. See also HTTP header `SourceMap` -- https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/SourceMap
-config.devtool = 'hidden-source-map'
+config.devtool = 'hidden-nosources-source-map'
