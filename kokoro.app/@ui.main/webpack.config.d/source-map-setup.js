f3f9162c1cf8f11e352707c8aaf32fcf72e3f22d
netaneru <netaneru3@gmail.com>
2024-06-02 08:09:38 +0800

    Fix: weird source map URLs

--- kokoro.app/@ui.main/webpack.config.d/source-map-setup.js
+++ kokoro.app/@ui.main/webpack.config.d/source-map-setup.js
@@ -3,3 +3,12 @@
 // NOTE: `hidden-*` generates source maps without adding the `sourceMappingURL`
 // comment. See also HTTP header `SourceMap` -- https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/SourceMap
 config.devtool = 'hidden-nosources-source-map'
+
+// See, https://webpack.js.org/configuration/output/#outputdevtoolmodulefilenametemplate
+//
+// The setup below achieves the following:
+// - Strips away any leading "../" or "./" from each source's resource path.
+// - Ensures that each source entry is not an absolute URL, which also prevents
+// the "webpack:" scheme (or similar) from being included.
+config.output.devtoolModuleFilenameTemplate =
+	info => info.resourcePath.replace(/^(?:\.\.?\/)+/, "")
