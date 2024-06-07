6d687d5750d80f8788a0ba6016e1dc6952de9f9c
netaneru <netaneru3@gmail.com>
2024-06-08 04:31:50 +0800

    Better for maintenance + Docs

--- kokoro.app/@ui.api/src/#commonMain/kotlin/kokoro/app/ui/engine/web/HOST_.kt
+++ kokoro.app/@ui.api/src/#commonMain/kotlin/kokoro/app/ui/engine/web/HOST_.kt
@@ -1,10 +1,31 @@
 package kokoro.app.ui.engine.web
 
+/**
+ * A custom TLD for app-level resources.
+ *
+ * App-level resources should only be accessible to other app-level resources.
+ *
+ * @see HOST_U
+ */
+const val HOST_X = "x"
+
+/**
+ * A custom TLD for user-level resources.
+ *
+ * User-level resources should only be accessible to other user-level resources
+ * or to app-level resources.
+ *
+ * @see HOST_X
+ */
+const val HOST_U = "u"
+
+// --
+
 /** @see HTTPX_UI_X */
-const val HOST_UI_X = "ui.x"
+const val HOST_UI_X = "ui.$HOST_X"
 
 /** @see HTTPX_RES_X */
-const val HOST_RES_X = "res.x"
+const val HOST_RES_X = "res.$HOST_X"
 
 /** @see HTTPX_RES_U */
-const val HOST_RES_U = "res.u"
+const val HOST_RES_U = "res.$HOST_U"
