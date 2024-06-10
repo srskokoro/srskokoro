98c5c3e9101e9ecbdbfe5359c390e8b45d938cb3
netaneru <netaneru3@gmail.com>
2024-06-10 08:27:20 +0800

    Added "secret" for `platform.js` on Android

--- kokoro.app/src/androidMain/kotlin/kokoro/app/ui/engine/web/PlatformJsResource.kt
+++ kokoro.app/src/androidMain/kotlin/kokoro/app/ui/engine/web/PlatformJsResource.kt
@@ -1,10 +1,29 @@
 package kokoro.app.ui.engine.web
 
 import okio.Buffer
+import java.util.UUID
 
 class PlatformJsResource : BasePlatformJsResource() {
+
 	override fun feed(out: Buffer, request: WebRequest) {
 		super.feed(out, request)
 		// TODO Saved states handling
+	}
+
+	companion object {
+
+		val SECRET = buildString(26) {
+			val uuid = UUID.randomUUID()
+
+			var s = java.lang.Long.toUnsignedString(uuid.mostSignificantBits, 32)
+			var n = 13 - s.length
+			while (--n >= 0) append('0')
+			append(s)
+
+			s = java.lang.Long.toUnsignedString(uuid.leastSignificantBits, 32)
+			n = 13 - s.length
+			while (--n >= 0) append('0')
+			append(s)
+		}
 	}
 }
